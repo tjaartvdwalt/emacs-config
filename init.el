ecef30783975efe0223a21f8b8a2d915653935a7
Tjaart van der Walt <github@tjaart.co.za>
2013-05-15 19:13:14 -0500

    removed toolbar for more vertical space

--- init.el
+++ init.el
@@ -13,7 +13,8 @@
  '(keep-end-mode t)
  '(rails-ws:default-server-type "webrick")
  '(show-paren-mode t)
- '(size-indication-mode t))
+ '(size-indication-mode t)
+ '(tool-bar-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
