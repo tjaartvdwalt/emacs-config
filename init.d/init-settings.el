96e9c352c66bb3b4c4f22e40b6a35dfcc40d29c1
Tjaart van der Walt <github@tjaart.co.za>
2014-08-18 12:44:35 -0500

    problem with auto save

--- init.d/init-settings.el
+++ init.d/init-settings.el
@@ -25,7 +25,8 @@
  '(rails-ws:default-server-type "webrick")
  '(show-paren-mode t)
  '(size-indication-mode t)
- '(tool-bar-mode nil))
+ '(tool-bar-mode nil)
+ '(uniquify-buffer-name-style (quote forward) nil (uniquify)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
