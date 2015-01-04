0b6d21e421ab377faa1500cc69bae2d58737b5bc
Tjaart van der Walt <github@tjaart.co.za>
2015-01-04 17:17:17 -0600

    init.d/init-swoop.el

--- init.d/init-swoop.el
+++ init.d/init-swoop.el
@@ -6,9 +6,9 @@
   :config(progn
            ;; (setq swoop-font-size-change: nil)
            ;; (setq swoop-use-target-magnifier: nil)
-           ;; (define-key isearch-mode-map (kbd "C-o") 'swoop-from-isearch)
-           ;; (define-key swoop-map (kbd "C-o") 'swoop-multi-from-swoop)
-           ;; (setq swoop-window-split-direction: 'split-window-horizontally)
-           ;; (setq swoop-window-split-current-window: t)
+           (define-key isearch-mode-map (kbd "C-o") 'swoop-from-isearch)
+           (define-key swoop-map (kbd "C-o") 'swoop-multi-from-swoop)
+           (setq swoop-window-split-direction: 'split-window-horizontally)
+           (setq swoop-window-split-current-window: t)
            ))
 
