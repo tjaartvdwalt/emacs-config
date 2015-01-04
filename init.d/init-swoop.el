fba208dc0f5547c252df89f4fab88d0bae069fd6
Tjaart van der Walt <github@tjaart.co.za>
2015-01-04 17:09:19 -0600

    init.d/init-swoop.el

--- init.d/init-swoop.el
+++ init.d/init-swoop.el
@@ -4,11 +4,11 @@
   ("C-c s r" . swoop-pcre-regexp)
   ("C-c s l" . swoop-back-to-last-position)
   :config(progn
-           (setq swoop-font-size-change: nil)
-           (setq swoop-use-target-magnifier: nil)
-           (define-key isearch-mode-map (kbd "C-o") 'swoop-from-isearch)
-           (define-key swoop-map (kbd "C-o") 'swoop-multi-from-swoop)
-           (setq swoop-window-split-direction: 'split-window-horizontally)
-           (setq swoop-window-split-current-window: t)
+           ;; (setq swoop-font-size-change: nil)
+           ;; (setq swoop-use-target-magnifier: nil)
+           ;; (define-key isearch-mode-map (kbd "C-o") 'swoop-from-isearch)
+           ;; (define-key swoop-map (kbd "C-o") 'swoop-multi-from-swoop)
+           ;; (setq swoop-window-split-direction: 'split-window-horizontally)
+           ;; (setq swoop-window-split-current-window: t)
            ))
 
