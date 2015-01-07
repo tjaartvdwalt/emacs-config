9c8cc29cf8b7ea5cafffd1b108e17390230ee048
Tjaart van der Walt <github@tjaart.co.za>
2015-01-07 16:24:13 -0600

    init.d/init-multiple-cursors.el

--- init.d/init-multiple-cursors.el
+++ init.d/init-multiple-cursors.el
@@ -1,2 +1,5 @@
 (req-package multiple-cursors
-    :bind ("C-c c e" . mc/edit-lines))
+  :bind (("C-c c e" . mc/edit-lines)
+         ("C-c c n" . mc/mark-next-like-this)
+         ("C-c c p" . mc/mark-previous-like-this)
+         ("C-c c a" . mc/mark-all-like-this)))
