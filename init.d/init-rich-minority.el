323b695657fc73fc48b5f934cc06595d0722c50f
Tjaart van der Walt <github@tjaart.co.za>
2015-01-08 10:23:43 -0600

    init.d/init-rich-minority.el

--- init.d/init-rich-minority.el
+++ init.d/init-rich-minority.el
@@ -1,5 +1,5 @@
 (req-package rich-minority
   :config (progn
           (rich-minority-mode 1)
-          (setq rm-blacklist '(" Undo-Tree" " 80col" " MRev" " company" "FIC" "ga" "Guide" " Par-"
+          (setq rm-blacklist '(" Undo-Tree" " 80col" " MRev" " company" " FIC" " ga" " Guide" " Par-"
                                " b-loc" " $" " Abbrev"))))
