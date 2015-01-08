5c106cefd9b4adfe54fe008879dc62e250baa251
Tjaart van der Walt <github@tjaart.co.za>
2015-01-08 10:09:06 -0600

    init.d/init-smart-mode-line.el

--- init.d/init-smart-mode-line.el
+++ init.d/init-smart-mode-line.el
@@ -2,5 +2,5 @@
   :config (progn
             (sml/setup)
             (setq sml/name-width 8)
-            (sml/apply-theme 'respectful) 
+            ;; (sml/apply-theme 'respectful) 
             (setq sml/mode-width "full")))
