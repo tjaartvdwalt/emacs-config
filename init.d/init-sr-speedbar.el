4338d9409911b4939dac17d06c778a32b118d427
Tjaart van der Walt <github@tjaart.co.za>
2015-01-09 17:55:21 -0600

    init.d/init-sr-speedbar.el

--- init.d/init-sr-speedbar.el
+++ init.d/init-sr-speedbar.el
@@ -2,4 +2,4 @@
   :bind (("C-c l l" . sr-speedbar-toggle)
          ("C-c l r" . sr-speedbar-refresh-toggle)
          ("C-c l s" . sr-speedbar-select-window))
-  :config((setq speedbar-use-images nil)))
+  :config(progn (setq speedbar-use-images nil)))
