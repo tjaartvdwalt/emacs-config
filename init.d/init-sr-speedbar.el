b832e05c79d6217255abb61a9f3d20abc61cc067
Tjaart van der Walt <github@tjaart.co.za>
2015-01-09 17:48:00 -0600

    init.d/init-sr-speedbar.el

--- init.d/init-sr-speedbar.el
+++ init.d/init-sr-speedbar.el
@@ -1,3 +1,4 @@
 (req-package sr-speedbar
   :bind (("C-c l l" . sr-speedbar-toggle)
-   ("C-c l r" . sr-speedbar-refresh-toggle)))
+         ("C-c l r" . sr-speedbar-refresh-toggle)
+         ("C-c d s" . sr-speedbar-select-window)))
