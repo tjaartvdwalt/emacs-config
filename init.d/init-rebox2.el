609adb0795653225995b9c1371908c993f5b0b7a
Tjaart van der Walt <github@tjaart.co.za>
2015-01-09 19:42:49 -0600

    init.d/init-rebox2.el

--- init.d/init-rebox2.el
+++ init.d/init-rebox2.el
@@ -1,7 +1,5 @@
 (req-package rebox2
   :config(progn (add-hook 'emacs-lisp-mode-hook (lambda ()
- (set (make-local-variable 'rebox-style-loop) '(25 17 21))
  (rebox-mode 1)))
 (add-hook 'c++-mode-hook (lambda ()
- (set (make-local-variable 'rebox-style-loop) '(25 17 21))
  (rebox-mode 1)))))
