8c7c3481e0f248ac4a6d45afd8bb99c18218a206
Tjaart van der Walt <github@tjaart.co.za>
2015-01-10 17:59:49 -0600

    init.d/init-skype.el

--- init.d/init-skype.el
+++ init.d/init-skype.el
@@ -1,4 +1,4 @@
 (req-package skype
-  :config(progn(setq skype--my-user-handle "rkwtavdw")
+  :config((setq skype--my-user-handle "rkwtavdw")
                 (defvar skype--com-handlers 'skype--com-handlers-py)
                 (skype--init)))
