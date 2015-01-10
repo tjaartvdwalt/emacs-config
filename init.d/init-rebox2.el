f104d2b3a898262464678caac000cc11bac1c038
Tjaart van der Walt <github@tjaart.co.za>
2015-01-09 19:27:22 -0600

    init.d/init-rebox2.el

--- init.d/init-rebox2.el
+++ init.d/init-rebox2.el
@@ -1,4 +1,4 @@
 (req-package rebox2
   :bind(("C-c x x" . rebox-dwim)
         ("C-c x c" . rebox-cycle))
-  :config(setq rebox-style-loop '(12 21 23 25 27)))
+  :config(setq rebox-style-loop '(11 12 21 23 25 27)))
