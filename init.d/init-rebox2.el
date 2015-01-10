75d632780b471427f2a33339deb25ff3e37cf232
Tjaart van der Walt <github@tjaart.co.za>
2015-01-09 19:22:34 -0600

    init.d/init-rebox2.el

--- init.d/init-rebox2.el
+++ init.d/init-rebox2.el
@@ -1,4 +1,4 @@
 (req-package rebox2
   :bind(("C-c x x" . rebox-dwim)
         ("C-c x c" . rebox-cycle))
-  :config(progn((setq rebox-style-loop '(21 25 27)))))
+  :config(setq rebox-style-loop '(21 25 27)))
