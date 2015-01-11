7aeedd6eb124e62f1e760ba2e1832ee52dc4d718
Tjaart van der Walt <github@tjaart.co.za>
2015-01-10 18:48:24 -0600

    init.d/init-guide-key.el

--- init.d/init-guide-key.el
+++ init.d/init-guide-key.el
@@ -5,7 +5,7 @@
     ;;(setq guide-key/text-scale-amount -1)
     (setq guide-key/recursive-key-sequence-flag t)
     (setq guide-key/guide-key-sequence '("C-c"))
-    (setq guide-key/popup-window-position 'bottom)
+    (setq guide-key/popup-window-position 'right)
     ;; (setq guide-key/guide-key-sequence '("C-x" "C-c"))
     (guide-key-mode 1))
 
