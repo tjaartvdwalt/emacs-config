a071697b1fc7747fdd3bb3893637cf17c595077f
Tjaart van der Walt <github@tjaart.co.za>
2015-01-12 16:12:32 -0600

    functions/dired-get-size.el

--- functions/dired-get-size.el
+++ functions/dired-get-size.el
@@ -8,6 +8,6 @@
                  (re-search-backward "\\(^[0-9.,]+[A-Za-z]+\\).*total$")
                   (match-string 1))))))
  
- (define-key dired-mode-map (kbd "\") 'dired-get-size)
+ (define-key dired-mode-map (kbd "\\") 'dired-get-size)
 
 That was the easy way … more fun would it be if you can get the file s
