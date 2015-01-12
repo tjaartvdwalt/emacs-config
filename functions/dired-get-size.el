ee04d0cd2bd72a3c8fa29b10dcb2b0a6c4d7b95e
Tjaart van der Walt <github@tjaart.co.za>
2015-01-12 11:22:04 -0600

    functions/dired-get-size.el

---
+++ functions/dired-get-size.el
@@ -0,0 +1,13 @@
+ (defun dired-get-size ()
+  (interactive)
+  (let ((files (dired-get-marked-files)))
+    (with-temp-buffer
+      (apply 'call-process "/usr/bin/du" nil t nil "-sch" files)
+      (message "Size of all marked files: %s"
+               (progn 
+                 (re-search-backward "\\(^[0-9.,]+[A-Za-z]+\\).*total$")
+                  (match-string 1))))))
+ 
+ (define-key dired-mode-map (kbd "\") 'dired-get-size)
+
+That was the easy way … more fun would it be if you can get the file s
