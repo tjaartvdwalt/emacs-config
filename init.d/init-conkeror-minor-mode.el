1ce080b561f5d9343f300fdeba4a1055a42e9066
Tjaart van der Walt <github@tjaart.co.za>
2015-01-12 19:18:00 -0600

    init.d/init-conkeror-minor-mode.el

--- init.d/init-conkeror-minor-mode.el
+++ init.d/init-conkeror-minor-mode.el
@@ -1,5 +1,5 @@
 (req-package conkeror-minor-mode
-  :config(prog (add-hook 'js-mode-hook (lambda ()
+  :config(progn (add-hook 'js-mode-hook (lambda ()
                           (when (string-match "conkeror" (buffer-file-name))
                             (conkeror-minor-mode 1))))
 ))
