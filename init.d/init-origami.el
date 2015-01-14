4a4f1a91c69c629b7f79cacf0ea1665069cfd919
Tjaart van der Walt <github@tjaart.co.za>
2015-01-14 13:19:42 -0600

    init.d/init-origami.el

--- init.d/init-origami.el
+++ init.d/init-origami.el
@@ -1,5 +1,5 @@
-(req-package origami
-  ;; :bind (("C-c o c" . origami-close-node)
-  ;;        ("C-c o o" . origami-open-node))
+;; (req-package origami
+;;   ;; :bind (("C-c o c" . origami-close-node)
+;;   ;;        ("C-c o o" . origami-open-node))
 
-  :config (progn (    (add-hook 'prog-mode-hook 'origami-mode))))
+;;   :config (progn (    (add-hook 'prog-mode-hook 'origami-mode))))
