84adfd5829e25b8c331628e829232c26a13021f2
Tjaart van der Walt <github@tjaart.co.za>
2015-01-15 16:56:28 -0600

    init.d/init-menu-bar-mode.el

--- init.d/init-menu-bar-mode.el
+++ init.d/init-menu-bar-mode.el
@@ -1,5 +1,4 @@
 ;; Disable menu-bar by default, but provide keybinding to quickly enable
 (req-package menu-bar-mode
   :bind(("C-c m m" . menu-bar-mode))
-
-  :init((menu-bar-mode -1)))
+  :config((menu-bar-mode -1)))
