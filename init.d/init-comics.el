57fae5427b7a675e5cc775c44f46c53b18332d1c
Tjaart van der Walt <github@tjaart.co.za>
2015-01-15 14:09:37 -0600

    init.d/init-comics.el

--- init.d/init-comics.el
+++ init.d/init-comics.el
@@ -1,4 +1,4 @@
 (req-package comics
   :bind(("C-c k l" . comics-favorites-list-comics)
-        ("C-c k l" . comics-fetch-unread-favorites))
+        ("C-c k f" . comics-fetch-unread-favorites))
   :config (progn (setq comics-use-separate-comic-directories t)))
