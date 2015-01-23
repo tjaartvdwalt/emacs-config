4c282ea01ec3078a49bc41e2282018a153034982
Tjaart van der Walt <github@tjaart.co.za>
2015-01-23 16:56:14 -0600

    functions/dired-decrypt-open-xdg.el

--- functions/dired-decrypt-open-xdg.el
+++ functions/dired-decrypt-open-xdg.el
@@ -1,4 +1,4 @@
-(defun tmp-filename (filename)
+(defun tvdw/tmp-filename (filename)
   "Returns the path where the temp file should be saved. It also creates the parent directory.
    The pattern is:
   /tmp/emacs-epa/filename.ext (removing the .gpg at the end)."
@@ -7,12 +7,12 @@
   (set-file-modes dir #o700)
  (concat dir (file-name-nondirectory (file-name-sans-extension filename)))))
 
-(defun decrypt-to-temp (filename)
+(defun tvdw/decrypt-to-temp (filename)
   "Decrypts the file into the temp file."
   (epa-decrypt-file filename (tmp-filename filename))
   (tmp-filename filename))
 
-(defun dired-decrypt-open-xdg ()
+(defun tvdw/dired-decrypt-open-xdg ()
   "Puts everything together"
   (interactive)
   (if (equal (file-name-extension (dired-filename-at-point)) "gpg")
