204aad72cadf4261c49af5bf41315f9920b5a7f3
Tjaart van der Walt <github@tjaart.co.za>
2015-01-24 23:34:20 -0600

    init.d/init-pymacs.el

--- init.d/init-pymacs.el
+++ init.d/init-pymacs.el
@@ -1,5 +1,5 @@
 ;; Because we have rope set up for our Python2
 ;; environment, which is no longer default in arch, we have to
 ;; have the following line in our .bashrc:
-;; PYMACS_PYTHON=python2
+;; export PYMACS_PYTHON=python2
 (req-package pymacs)
