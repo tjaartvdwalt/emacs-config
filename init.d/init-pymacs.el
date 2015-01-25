a33b2c7b741294d624d173e5cb2d56973a7b52b9
Tjaart van der Walt <github@tjaart.co.za>
2015-01-24 23:37:51 -0600

    init.d/init-pymacs.el

--- init.d/init-pymacs.el
+++ init.d/init-pymacs.el
@@ -2,4 +2,9 @@
 ;; environment, which is no longer default in arch, we have to
 ;; have the following line in our .bashrc:
 ;; export PYMACS_PYTHON=python2
-(req-package pymacs)
+(req-package pymacs
+  :config(progn
+           (autoload 'pymacs-apply "pymacs")
+(autoload 'pymacs-call "pymacs")
+(autoload 'pymacs-eval "pymacs" nil t)
+(autoload 'pymacs-exec "pymacs" nil t)))
