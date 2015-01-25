76633ccc338d41879d33e9c2461295d4b4d229ef
Tjaart van der Walt <github@tjaart.co.za>
2015-01-24 23:40:04 -0600

    init.d/init-pymacs.el

--- init.d/init-pymacs.el
+++ init.d/init-pymacs.el
@@ -7,4 +7,5 @@
            (autoload 'pymacs-apply "pymacs")
            (autoload 'pymacs-call "pymacs")
            (autoload 'pymacs-eval "pymacs" nil t)
-           (autoload 'pymacs-exec "pymacs" nil t)))
+           (autoload 'pymacs-exec "pymacs" nil t)
+           (autoload 'pymacs-load "pymacs" nil t)))
