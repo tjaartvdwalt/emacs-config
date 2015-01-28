0740510843da40139cb53dcc7fa2f0939dd68446
Tjaart van der Walt <github@tjaart.co.za>
2015-01-28 12:26:19 -0600

    init.d/init-org-toc.el

--- init.d/init-org-toc.el
+++ init.d/init-org-toc.el
@@ -1,1 +1,6 @@
-(req-package org-toc)
+(req-package org-toc
+  :config((eval-after-load "org-toc-autoloads"
+  '(progn
+     (if (require 'org-toc nil t)
+         (add-hook 'org-mode-hook 'org-toc-enable)
+       (warn "org-toc not found"))))))
