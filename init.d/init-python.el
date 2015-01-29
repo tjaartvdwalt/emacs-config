7f43e3aab5cfc7f6e09904f4733ae304e87a3a60
Tjaart van der Walt <github@tjaart.co.za>
2015-01-29 15:37:19 -0600

    init.d/init-python.el

--- init.d/init-python.el
+++ init.d/init-python.el
@@ -2,13 +2,13 @@
 ;;; Commentary:
 ;;; Code:
 (req-package python
-  :require (anaconda-mode company company-anaconda)
+  ;; :require (anaconda-mode company company-anaconda)
   :interpreter ("python" . python-mode)
-  :config
-  (progn
-    (add-hook 'python-mode-hook 'anaconda-mode)
-    (add-hook 'python-mode-hook 'anaconda-eldoc)
-    (add-to-list 'company-backends 'company-anaconda)))
+  ;; :config
+  ;; (progn
+  ;;   (add-hook 'python-mode-hook 'anaconda-mode)
+  ;;   (add-hook 'python-mode-hook 'anaconda-eldoc)
+  ;;   (add-to-list 'company-backends 'company-anaconda)))
 
 (provide 'init-python)
 ;;; init-python ends here
