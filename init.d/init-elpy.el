0d90de9cc9a03a23539aa176279a7afcc434c26d
Tjaart van der Walt <github@tjaart.co.za>
2015-01-29 15:45:25 -0600

    init.d/init-elpy.el

--- init.d/init-elpy.el
+++ init.d/init-elpy.el
@@ -1,2 +1,8 @@
+;; On the python side you need the following installed:
+;; pip install flake8
+;; And either rope or jedi
+;; pip install rope (Python3 rope_py3k)
+;; pip install jedi
+
 (req-package elpy
   :config(progn  (elpy-enable)))
