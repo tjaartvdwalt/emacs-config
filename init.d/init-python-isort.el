377d1a4cbd3d67419f73aa2ace65e892488901c0
Tjaart van der Walt <github@tjaart.co.za>
2015-02-27 16:09:00 -0600

    init.d/init-python-isort.el

--- init.d/init-python-isort.el
+++ init.d/init-python-isort.el
@@ -1,3 +1,7 @@
+;; This package requires the isort package to be installed from pypi
+;; You can install it using:
+;;
+;; sudo pip install isort
 (req-package py-isort
   :config(progn (add-hook 'before-save-hook 'py-isort-before-save)))
 
