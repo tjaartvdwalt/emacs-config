56cae38a97ec65fa12ab1eacc7c67a9875825bc9
Tjaart van der Walt <github@tjaart.co.za>
2015-03-16 07:42:44 -0500

    init.el

--- init.el
+++ init.el
@@ -44,8 +44,9 @@
 (use-package load-dir
   :ensure load-dir
   :init (progn
-          (load-dir-one "~/.emacs.d/functions")
-          (load-dir-one "~/.emacs.d/init.d")
+          (setq load-dirs '("~/.emacs.d/functions" "~/.emacs.d/init.d"))
+          ;; (load-dir-one "~/.emacs.d/functions")
+          ;; (load-dir-one "~/.emacs.d/init.d")
           ))
 
 ;; show details in *Messages* buffer
