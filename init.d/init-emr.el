9af2746dd00357d81db30086847edfd2383bebed
Tjaart van der Walt <github@tjaart.co.za>
2015-03-15 22:35:53 -0500

    init.d/init-emr.el

--- init.d/init-emr.el
+++ init.d/init-emr.el
@@ -1,4 +1,4 @@
 (req-package emr
   :config(progn
            (define-key prog-mode-map (kbd "M-RET") 'emr-show-refactor-menu)
-                (add-hook 'prog-mode-hook 'emr-initialize)))
+           (add-hook 'prog-mode-hook 'emr-initialize)))
