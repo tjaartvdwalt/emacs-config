aec67e127795093701a92cbe3cd5c63f8bab4059
Tjaart van der Walt <github@tjaart.co.za>
2015-03-17 22:46:21 -0500

    init.d/init-persp-mode.el

--- init.d/init-persp-mode.el
+++ init.d/init-persp-mode.el
@@ -1,5 +1,4 @@
 (req-package persp-mode
   :config(
           progn
-           (with-eval-after-load "persp-mode-autoloads"
-             (add-hook 'after-init-hook #'(lambda () (persp-mode 1))))))
+             (add-hook 'after-init-hook '(lambda () (persp-mode 1)))))
