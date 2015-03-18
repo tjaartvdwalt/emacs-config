4b461fd064f40151ee49cff866b039c06d2c84b9
Tjaart van der Walt <github@tjaart.co.za>
2015-03-17 22:43:24 -0500

    init.d/init-persp-mode.el

--- init.d/init-persp-mode.el
+++ init.d/init-persp-mode.el
@@ -1,6 +1,10 @@
 (req-package persp-mode
-  :init(
+  :config(
           progn
+           (unless (fboundp 'with-eval-after-load)
+             (defmacro with-eval-after-load (file &rest body)
+               (declare (indent 1) (debug t))
+               `(eval-after-load ,file '(progn ,@body))))
            (with-eval-after-load "persp-mode-autoloads"
              (setq wg-morph-on nil) ;; switch off animation
              (add-hook 'after-init-hook #'(lambda () (persp-mode 1))))))
