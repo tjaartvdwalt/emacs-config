29bb0ec469db636869b6567ddcb16be303645ed2
Tjaart van der Walt <github@tjaart.co.za>
2015-03-17 22:08:41 -0500

    init.d/init-ibuffer-projectile.el

---
+++ init.d/init-ibuffer-projectile.el
@@ -0,0 +1,7 @@
+(req-package
+  :config (progn
+           (add-hook 'ibuffer-hook
+    (lambda ()
+      (ibuffer-projectile-set-filter-groups)
+      (unless (eq ibuffer-sorting-mode 'alphabetic)
+        (ibuffer-do-sort-by-alphabetic)))) ))
