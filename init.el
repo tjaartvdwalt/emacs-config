43a99dafb653360ea1024c2bc736bb5edcdf4c07
Tjaart van der Walt <github@tjaart.co.za>
2015-03-18 23:05:45 -0500

    init.el

--- init.el
+++ init.el
@@ -18,7 +18,7 @@
                 company-oddmuse company-files company-dabbrev)))
  '(custom-safe-themes
    (quote
-    ("a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" default)))
+    ("c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" default)))
  '(gac-automatically-push-p t)
  '(global-auto-complete-mode t)
  '(keep-end-mode t)
@@ -66,3 +66,9 @@
 ;; actually load the packages
 ;; (put 'upcase-region 'disabled nil)
 ;; (req-package-finish)
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
