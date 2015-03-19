48adeac5047b0372e9b12939beebc36583324999
Tjaart van der Walt <github@tjaart.co.za>
2015-03-18 22:41:21 -0500

    init.d/init-flycheck-color-mode-line.el

--- init.d/init-flycheck-color-mode-line.el
+++ init.d/init-flycheck-color-mode-line.el
@@ -1,2 +1,3 @@
 (req-package flycheck-color-mode-line-mode
-  :config((add-hook 'flycheck-mode-hook 'flycheck-color-mode-line-mode)))
+  :config(progn
+          (add-hook 'flycheck-mode-hook 'flycheck-color-mode-line-mode)))
