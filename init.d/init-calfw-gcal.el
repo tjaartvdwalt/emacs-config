dd7e629de5e760f6362cad5907ad1bef18c6fe18
Tjaart van der Walt <github@tjaart.co.za>
2015-03-29 23:05:08 -0500

    init.d/init-calfw-gcal.el

--- init.d/init-calfw-gcal.el
+++ init.d/init-calfw-gcal.el
@@ -1,4 +1,4 @@
 (req-package calfw-gcal
   :config(progn
            (setq cfw:gcal-user "tjaart@tjaart.co.za")
-           (setq cfw:gcal-user "shgpwsmtkfwecskk")))
+           (setq cfw:gcal-pass "shgpwsmtkfwecskk")))
