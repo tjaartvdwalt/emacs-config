a89df9f221da168afe886508e3dc29c6509fc219
Tjaart van der Walt <github@tjaart.co.za>
2015-03-29 22:44:24 -0500

    init.d/init-calfw.el

--- init.d/init-calfw.el
+++ init.d/init-calfw.el
@@ -1,2 +1,4 @@
 (req-package calfw
-  require(calfw-org calfw-ical))
+  :require (calfw-org calfw-ical)
+  :config (progn
+            (cfw:open-ical-calendar "https://www.google.com/calendar/ical/tjaart%40tjaart.co.za/private-578554fb506f63442f2f53cc4ee2c144/basic.ics")))
