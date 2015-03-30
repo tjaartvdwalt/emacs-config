2d235934b7a124f46afdaed16389e7852f3a7e0d
Tjaart van der Walt <github@tjaart.co.za>
2015-03-30 08:34:31 -0500

    init.d/init-calfw-gcal.el

--- init.d/init-calfw-gcal.el
+++ init.d/init-calfw-gcal.el
@@ -1,4 +1,5 @@
 (req-package calfw-gcal
   :config(progn
+           (setq cfw:gcal-py-dir "/usr/bin/python2")
            (setq cfw:gcal-user "tjaart@tjaart.co.za")
            (setq cfw:gcal-pass "shgpwsmtkfwecskk")))
