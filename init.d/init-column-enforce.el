314e84a2dbf3e47fd008634bf29d06d9dd548d8d
Tjaart van der Walt <github@tjaart.co.za>
2015-04-04 18:53:40 -0500

    init.d/init-column-enforce.el

--- init.d/init-column-enforce.el
+++ init.d/init-column-enforce.el
@@ -5,7 +5,7 @@
 (req-package column-enforce-mode
   :defer 1
   :config (progn (add-hook 'prog-mode-hook 'column-enforce-mode)
-               (global-set-key "\C-ce" 'column-enforce-mode)
+               ;; (global-set-key "\C-ce" 'column-enforce-mode)
                ))
 (provide 'init-column-enforce)
 ;;; init-column-enforce ends here
