d3ba0ae5827c5179e28c9849e7fa8b6cf509310f
Tjaart van der Walt <github@tjaart.co.za>
2015-04-09 18:30:46 -0500

    elpa.el

--- elpa.el
+++ elpa.el
@@ -9,6 +9,9 @@
 (add-to-list 'package-archives
     '("melpa" .
       "http://melpa.milkbox.net/packages/"))
+(add-to-list 'package-archives
+    '("melpa-stable" .
+      "http://stable.melpa.org/packages/")
 ;; (add-to-list 'package-archives
 ;;    '("tromney" .
 ;;      "http://tromey.com/elpa"))
