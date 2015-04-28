4f35a55e71083d30f89546cecec8ca83dbfd6485
Tjaart van der Walt <github@tjaart.co.za>
2015-04-28 14:31:46 -0500

    init.d/init-org.el

--- init.d/init-org.el
+++ init.d/init-org.el
@@ -34,6 +34,16 @@
                ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
                ("\\paragraph{%s}" . "\\paragraph*{%s}")
                ("\\subparagraph{%s}" . "\\subparagraph*{%s}")))
+
+    (add-to-list 'org-latex-classes
+             '("scrartcl"
+               "\\documentclass{moderncv}"
+               ("\\section{%s}" . "\\section*{%s}")
+               ("\\subsection{%s}" . "\\subsection*{%s}")
+               ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
+               ("\\paragraph{%s}" . "\\paragraph*{%s}")
+               ("\\subparagraph{%s}" . "\\subparagraph*{%s}")))
+
     
     ;; ************* Misc *************
     (setq org-src-fontify-natively t)
