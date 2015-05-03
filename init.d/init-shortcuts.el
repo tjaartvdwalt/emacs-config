2b762df70089d5255d4ab3fb255d37835619d727
Tjaart van der Walt <github@tjaart.co.za>
2015-05-02 21:17:40 -0500

    init.d/init-shortcuts.el

--- init.d/init-shortcuts.el
+++ init.d/init-shortcuts.el
@@ -16,7 +16,7 @@
 (define-key global-map (kbd "RET") 'newline-and-indent)
 
 ;; Convenient mark functions
-(global-set-key   (kbd "\C-. p") 'mark-paragraph)
+(global-set-key   (kbd "\C-\. p") 'mark-paragraph)
 ;; (global-set-key (kbd "\C-\\. p") )
 ;; (global-set-key (kbd "\C-\\. f") 'mark-function)
 ;; (global-set-key (kbd "\C-\\. w") 'mark-word)
