d8e07f570c9f2491012c6318b031215b3bba43a4
Tjaart van der Walt <github@tjaart.co.za>
2015-05-18 12:16:09 -0500

    init.d/init-magit.el

--- init.d/init-magit.el
+++ init.d/init-magit.el
@@ -7,10 +7,10 @@
   :config
   (progn
     (setq magit-last-seen-setup-instructions "1.4.0")
-     (set-face-foreground 'magit-diff-add "green3")
-     (set-face-foreground 'magit-diff-del "red3")
-     (when (not window-system)
-       (set-face-background 'magit-item-highlight "black"))
-  (add-hook 'magit-status-mode-hook (lambda () (magit-filenotify-mode t)))))
+    (set-face-foreground 'magit-diff-add "green3")
+    (set-face-foreground 'magit-diff-del "red3")
+    (when (not window-system)
+      (set-face-background 'magit-item-highlight "black"))
+    (add-hook 'magit-status-mode-hook (lambda () (magit-filenotify-mode t)))))
 (provide 'init-magit)
 ;;; init-magit ends here
