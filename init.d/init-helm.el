f20d6a77dff2bb016e1ffed2e05ef8bd4ae56870
Tjaart van der Walt <github@tjaart.co.za>
2015-05-22 19:21:00 -0500

    init.d/init-helm.el

--- init.d/init-helm.el
+++ init.d/init-helm.el
@@ -4,5 +4,6 @@
  :config (progn
            (require 'helm-config)
            (helm-mode 1)
+           (helm-autoresize-mode 1)
            (global-set-key (kbd "C-x C-f") 'helm-find-files)
            (add-to-list 'helm-completing-read-handlers-alist (find-file . ido))))
