5299293ba118ab15b362c974a4b77fb1385c99d6
Tjaart van der Walt <github@tjaart.co.za>
2015-05-22 22:18:12 -0500

    init.d/init-ace-jump-helm-line.el

--- init.d/init-ace-jump-helm-line.el
+++ init.d/init-ace-jump-helm-line.el
@@ -2,4 +2,4 @@
   :require(helm)
   :config(progn
            (eval-after-load "helm"
-            '(define-key helm-map (kbd "C-p") 'ace-jump-helm-line)) ))
+            '(define-key helm-map (kbd "C-,") 'ace-jump-helm-line)) ))
