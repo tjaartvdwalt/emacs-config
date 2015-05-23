16da9432621928c79b9d1b82467ad55c2ca5157d
Tjaart van der Walt <github@tjaart.co.za>
2015-05-23 09:36:16 -0500

    init.d/init-calfw.el

--- init.d/init-calfw.el
+++ init.d/init-calfw.el
@@ -2,5 +2,5 @@
   :require (calfw-org calfw-ical)
   :bind ("C-c m k" . cfw:open-calender-buffer)
   ;; :config (progn
-  ;;           (cfw:open-ical-calendar "https://www.google.com/calendar/ical/tjaart%40tjaart.co.za/private-578554fb506f63442f2f53cc4ee2c144/basic.ics")
-            ))
+  ;;           (cfw:open-ical-calendar "https://www.google.com/calendar/ical/tjaart%40tjaart.co.za/private-578554fb506f63442f2f53cc4ee2c144/basic.ics"))
+)
