632a89e51eecc5aa10b2b73b3f718736daa93f2c
Tjaart van der Walt <github@tjaart.co.za>
2015-06-01 13:40:27 -0500

    init.d/init-org-page.el

--- init.d/init-org-page.el
+++ init.d/init-org-page.el
@@ -5,7 +5,7 @@
             (setq op/repository-org-branch "source")
             (setq op/repository-html-branch "master")
             (setq op/personal-github-link "https://github.com/tjaartvdwalt")
-            (setq op/personal-avatar "http://www.gravatar.com/avatar/802e5786235d6cfa93279d29c6626c4c?s=80")
+            (setq op/personal-avatar "http://www.gravatar.com/avatar/802e5786235d6cfa93279d29c6626c4c?s=200")
             (setq op/personal-google-analytics-id "UA-63610941-1")
             (setq op/theme 'mdo)
             (setq op/site-main-title "Tjaart's blog")
