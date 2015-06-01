9c9dddaf380b5ffab14f8d490ce0cff243ff3086
Tjaart van der Walt <github@tjaart.co.za>
2015-06-01 11:19:40 -0500

    init.d/init-org-page.el

--- init.d/init-org-page.el
+++ init.d/init-org-page.el
@@ -2,4 +2,5 @@
   :config (progn
             (setq op/repository-directory "~/org-page")
             (setq op/site-domain "https://github.com/tjaartvdwalt/org-page-test")
-            (setq op/personal-github-link "tjaartvdwalt")))
+            (setq op/personal-github-link "tjaartvdwalt")
+            (setq op/personal-avatar "http://www.gravatar.com/avatar/802e5786235d6cfa93279d29c6626c4c")))
