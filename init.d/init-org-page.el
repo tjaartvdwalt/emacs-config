d575799aef6cfe9bcdcc87894729add43a4c7ea1
Tjaart van der Walt <github@tjaart.co.za>
2015-06-01 12:30:18 -0500

    init.d/init-org-page.el

--- init.d/init-org-page.el
+++ init.d/init-org-page.el
@@ -5,7 +5,6 @@
             (setq op/personal-github-link "https://github.com/tjaartvdwalt")
             (setq op/personal-avatar "http://www.gravatar.com/avatar/802e5786235d6cfa93279d29c6626c4c?s=100")
             (setq op/personal-google-analytics-id "UA-63610941-1")
-            (setq op/theme-root-directory "~/.emacs.d/elpa/org-page-20150428.525/themes/")
             (setq op/theme 'mdo)
             (setq op/site-main-title "Tjaart's blog")
             (setq op/site-sub-title "A blog about bloggerol")
