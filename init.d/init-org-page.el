4011cdfc7e07dc210cc96ceaff1314cdfcd30b68
Tjaart van der Walt <github@tjaart.co.za>
2015-06-01 11:37:46 -0500

    init.d/init-org-page.el

--- init.d/init-org-page.el
+++ init.d/init-org-page.el
@@ -5,4 +5,5 @@
             (setq op/personal-github-link "https://github.com/tjaartvdwalt")
             (setq op/personal-avatar "http://www.gravatar.com/avatar/802e5786235d6cfa93279d29c6626c4c?s=200")
             (setq op/personal-google-analytics-id "UA-63610941-1")
-            (setq op/theme 'emacs_love)))
+            (setq op/theme 'emacs_love)
+            (setq op/theme-root-directory "~/.emacs.d/elpa/org-page-20150428.525/themes/emacs_love")))
