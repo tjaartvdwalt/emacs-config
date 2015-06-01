36123ef96724e629ccc5be01ae870e97f48ffcd8
Tjaart van der Walt <github@tjaart.co.za>
2015-06-01 10:52:41 -0500

    init.d/init-org-page.el

--- init.d/init-org-page.el
+++ init.d/init-org-page.el
@@ -1,3 +1,4 @@
 (req-package org-page
   :config (progn
-           (setq op/repository-directory "~/org-page") ))
+            (setq op/repository-directory "~/org-page")
+            (setq op/site-domain "https://github.com/tjaartvdwalt/org-page-test")))
