c42e83215c48fb6c20429bc708d71b4606aed3da
Tjaart van der Walt <github@tjaart.co.za>
2015-06-07 17:53:14 -0500

    init.d/init-helm-mode-manager.el

--- init.d/init-helm-mode-manager.el
+++ init.d/init-helm-mode-manager.el
@@ -1,2 +1,4 @@
 (req-package helm-mode-manager
-  )
+    :bind (("C-c h o s" . helm-switch-major-mode)
+           ("C-c h o e" . helm-enable-minor-mode)
+           ("C-c h o d" . helm-disable-minor-mode))
