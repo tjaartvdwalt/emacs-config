f18373826e74f40708cd4eb50afecaec16408f48
Tjaart van der Walt <github@tjaart.co.za>
2015-06-07 16:34:27 -0500

    init.d/init-golden-ratio.el

--- init.d/init-golden-ratio.el
+++ init.d/init-golden-ratio.el
@@ -3,6 +3,12 @@
            (golden-ratio-mode 1)
            (setq golden-ratio-auto-scale t)
 
+           (defcustom golden-ratio-extra-commands
+             '(windmove-left windmove-right windmove-down windmove-up ace-window)
+             "List of extra commands used to jump to other window."
+             :group 'golden-ratio
+             :type '(repeat symbol))
+           
            ;; exclude golden-ratio from helm
            (defun pl/helm-alive-p ()
              (if (boundp 'helm-alive-p)
