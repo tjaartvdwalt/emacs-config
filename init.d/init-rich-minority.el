787d3cc97f851dcb2213df13107cc52559c44cfd
Tjaart van der Walt <github@tjaart.co.za>
2015-06-07 18:23:04 -0500

    init.d/init-rich-minority.el

--- init.d/init-rich-minority.el
+++ init.d/init-rich-minority.el
@@ -1,4 +1,4 @@
 (req-package rich-minority
   :config (progn
           (rich-minority-mode 1)
-          (setq rm-blacklist '(" Anzu" " Undo-Tree" " 80col" " back" " MRev" " company" " FIC" " Fill" " ga" " Gradle" " Guide" " Par-" " b-loc" " rebox" " $" " Abbrev" " Projectile" " Golden" " Helm" " #none"))))
+          (setq rm-blacklist '(" Anzu" " Undo-Tree" " 80col" " back" " MRev" " company" " FIC" " Fill" " ga" " Gradle" " Guide" " Par-" " b-loc" " rebox" " $" " Abbrev" " Projectile" " Golden" " Helm" " #none" " Omit"))))
