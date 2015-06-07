57b049a0f257ad8ba94bf26daa9b325a73c81e01
Tjaart van der Walt <github@tjaart.co.za>
2015-06-07 17:30:17 -0500

    init.d/init-helm.el

--- init.d/init-helm.el
+++ init.d/init-helm.el
@@ -6,6 +6,7 @@
          ("C-c h a" . helm-ag)
           ("C-c h c" . helm-calcul-expression)
          ("C-c h g" . helm-google-suggest)
+         ("C-c h f" . helm-find-files)
          ("C-c h k" . helm-show-kill-ring)
          ("C-c h m" . helm-mini)
          ("C-c h i" . helm-all-mark-rings)
