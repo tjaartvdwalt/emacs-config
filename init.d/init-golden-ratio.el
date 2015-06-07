449de06a0b081a1947d065b7d5bdd2be81dbd740
Tjaart van der Walt <github@tjaart.co.za>
2015-06-07 15:26:02 -0500

    init.d/init-golden-ratio.el

--- init.d/init-golden-ratio.el
+++ init.d/init-golden-ratio.el
@@ -2,6 +2,9 @@
   :config(progn
            (golden-ratio-mode 1)
            (setq golden-ratio-auto-scale t)
+
+           (add-hook 'buffer-list-update-hook (lambda ()
+                                                golden-ratio))
            
            ;; exclude golden-ratio from helm
            (defun pl/helm-alive-p ()
