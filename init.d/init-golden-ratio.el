9ccd89edc13c1d9fc65cee0ed467bcd71162a934
Tjaart van der Walt <github@tjaart.co.za>
2015-06-07 09:04:05 -0500

    init.d/init-golden-ratio.el

--- init.d/init-golden-ratio.el
+++ init.d/init-golden-ratio.el
@@ -2,6 +2,7 @@
   :config(progn
            (golden-ratio-mode 1)
 
+           ;; exclude golden-ratio from helm
            (defun pl/helm-alive-p ()
              (if (boundp 'helm-alive-p)
                  (symbol-value 'helm-alive-p)))
