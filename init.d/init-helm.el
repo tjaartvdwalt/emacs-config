96b20dad3c6dadfdabf97cd3b567f8ab830bf3e2
Tjaart van der Walt <github@tjaart.co.za>
2015-06-07 23:02:39 -0500

    init.d/init-helm.el

--- init.d/init-helm.el
+++ init.d/init-helm.el
@@ -10,11 +10,10 @@
           ("C-c h c" . helm-calcul-expression)
          ("C-c h g" . helm-google-suggest)
          ("C-c h f" . helm-find)
+         ("C-c h l" . helm-locate)
          ("C-c h k" . helm-show-kill-ring)
          ("C-c h m" . helm-mini)
          ("C-c h i" . helm-all-mark-rings)
-         ("C-c h s" . helm-swoop)
-         ("C-c h l" . helm-locate)
          ("C-c h r" . helm-regexp))
 
  :config (progn
