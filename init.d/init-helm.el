0e0c999599d15715291cfaa247e7927dac91c6ba
Tjaart van der Walt <github@tjaart.co.za>
2015-06-10 13:51:40 -0500

    init.d/init-helm.el

--- init.d/init-helm.el
+++ init.d/init-helm.el
@@ -30,8 +30,6 @@
 
            (setq helm-buffers-fuzzy-matching t)
            (setq helm-recentf-fuzzy-match    t)
-
-
            (setq helm-M-x-fuzzy-match t)
            (setq helm-imenu-fuzzy-match t)
            (setq helm-apropos-fuzzy-match t)
