03e3326ebcad2e453f187a8d44548203606ce1d8
Tjaart van der Walt <github@tjaart.co.za>
2015-06-09 21:39:02 -0500

    init.d/init-helm-open-github.el

--- init.d/init-helm-open-github.el
+++ init.d/init-helm-open-github.el
@@ -2,7 +2,7 @@
       :bind (("C-c h h f" . helm-open-github-from-file)
              ("C-c h h c" . helm-open-github-from-commit)
              ("C-c h h i" . helm-open-github-from-issues)
-             ("C-c h h p" . helm-open-github-from-pull-requests))
+             ("C-c h h p" . helm-open-github-from-pull-requests)))
 
 
 
