fc2fd0e5d3bea6252dd9da4ba69240cfa00a924b
Tjaart van der Walt <github@tjaart.co.za>
2015-06-09 21:38:39 -0500

    init.d/init-helm-open-github.el

---
+++ init.d/init-helm-open-github.el
@@ -0,0 +1,8 @@
+(req-package helm-open-github
+      :bind (("C-c h h f" . helm-open-github-from-file)
+             ("C-c h h c" . helm-open-github-from-commit)
+             ("C-c h h i" . helm-open-github-from-issues)
+             ("C-c h h p" . helm-open-github-from-pull-requests))
+
+
+
