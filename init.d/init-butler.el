0fee9940d0dc8180419c8dcafe7deb6cda4fa2af
Tjaart van der Walt <github@tjaart.co.za>
2015-06-16 23:05:08 -0500

    init.d/init-butler.el

--- init.d/init-butler.el
+++ init.d/init-butler.el
@@ -1,5 +1,5 @@
 (req-package butler
-  :config ( progn
+  :config (progn
             add-to-list 'butler-server-list
              '(jenkins "tjaart.mooo.com"
                        (server-address . "https://tjaart.mooo.com:8090")
