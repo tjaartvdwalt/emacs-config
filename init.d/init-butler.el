f8a0c1bc91b44818169be4fdf9f1e975e5751c71
Tjaart van der Walt <github@tjaart.co.za>
2015-06-16 23:11:26 -0500

    init.d/init-butler.el

--- init.d/init-butler.el
+++ init.d/init-butler.el
@@ -3,4 +3,4 @@
             (add-to-list 'butler-server-list
              '(jenkins "tjaart.mooo.com"
                        (server-address . "https://tjaart.mooo.com:8090")
-                       (auth-file . "~/.authinfo")))))
+                       (auth-file . "~/.authinfo.gpg")))))
