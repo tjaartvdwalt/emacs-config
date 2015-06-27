a33d06b53e1627024808bfeb67cd3c7a6cea5343
Tjaart van der Walt <github@tjaart.co.za>
2015-06-27 15:21:24 -0500

    init.d/init-js2-mode.el

--- init.d/init-js2-mode.el
+++ init.d/init-js2-mode.el
@@ -13,13 +13,6 @@
     ;; (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
     
     (add-hook 'js2-mode-hook 'jquery-doc-setup)
-    (add-to-list 'load-path "/usr/lib/node_modules/tern/emacs/")
-    (add-hook 'js2-mode-hook (lambda () (tern-mode t)))
-    ;; (add-to-list 'company-backends 'company-tern)
-    ;; tern must be installed seperately
-    ;; on arch linux `yaourt -S nodejs-tern`
-    (autoload 'tern-mode "tern.el" nil t)))
-
 
 (provide 'init-js2-mode)
 ;;; init-js ends here
