f507f14d3aaf54938c9d0437b2759cef3136ec03
Tjaart van der Walt <github@tjaart.co.za>
2015-06-27 11:18:40 -0500

    init.d/init-yasnippet.el

--- init.d/init-yasnippet.el
+++ init.d/init-yasnippet.el
@@ -1,11 +1,12 @@
 ;; custom snippets go in ~/.emac.d/snippets
 
 (req-package yasnippet
-  :require (auto-yasnippet)
+  :require (company company-backends auto-yasnippet)
 
   ;; :init
   ;; (progn
   ;;   (setq yas-prompt-functions '(yas-ido-prompt)))
   :config
   (progn
-    (yas-global-mode 1)))
+    (yas-global-mode 1)
+    (add-to-list 'company-backends 'company-yasnippet)))
