d2face9d8a75d1e924a09cee5ae94683030a293d
Tjaart van der Walt <github@tjaart.co.za>
2015-06-27 11:05:23 -0500

    init.d/init-company-web.el

--- init.d/init-company-web.el
+++ init.d/init-company-web.el
@@ -9,6 +9,6 @@
           ;;                            (set (make-local-variable 'company-backends) '(company-web-html company-files))
           ;;                            (company-mode t)))
         (add-to-list 'company-backends 'company-web-html)
-        (add-to-list 'company-backends 'company-web-jade)
-        (add-to-list 'company-backends 'company-web-slim)
+        ;; (add-to-list 'company-backends 'company-web-jade)
+        ;; (add-to-list 'company-backends 'company-web-slim)
         ))
