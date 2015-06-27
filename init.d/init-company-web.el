99d8efb475cbbcdb502928092c799636174207d5
Tjaart van der Walt <github@tjaart.co.za>
2015-06-27 10:43:32 -0500

    init.d/init-company-web.el

--- init.d/init-company-web.el
+++ init.d/init-company-web.el
@@ -2,12 +2,12 @@
   :require (company company-backends web-mode)
   
   :config(
-          (define-key web-mode-map (kbd "M-,") 'company-web-html)
+          ;; (define-key web-mode-map (kbd "M-,") 'company-web-html)
 
-          (add-hook 'web-mode-hook (lambda ()
-                                     (set (make-local-variable 'company-backends) '(company-web-html company-files))
-                                     (company-mode t)))
-        ;; (add-to-list 'company-backends 'company-web-html)
-        ;; (add-to-list 'company-backends 'company-web-jade)
-        ;; (add-to-list 'company-backends 'company-web-slim)
+          ;; (add-hook 'web-mode-hook (lambda ()
+          ;;                            (set (make-local-variable 'company-backends) '(company-web-html company-files))
+          ;;                            (company-mode t)))
+        (add-to-list 'company-backends 'company-web-html)
+        (add-to-list 'company-backends 'company-web-jade)
+        (add-to-list 'company-backends 'company-web-slim)
         ))
