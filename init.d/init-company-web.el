d1f726e70d9f4ecc75989ec8c1988a4fa49707a4
Tjaart van der Walt <github@tjaart.co.za>
2015-06-27 10:35:33 -0500

    init.d/init-company-web.el

--- init.d/init-company-web.el
+++ init.d/init-company-web.el
@@ -1,7 +1,7 @@
 (req-package company-web
   :require (company)
   
-  :init(
+  :config(
         (add-to-list 'company-backends 'company-web-html)
         (add-to-list 'company-backends 'company-web-jade)
         (add-to-list 'company-backends 'company-web-slim)))
