75d961c546c31d319a365e502573d1fced33d59a
Tjaart van der Walt <github@tjaart.co.za>
2015-06-27 10:32:13 -0500

    init.d/init-company-web.el

--- init.d/init-company-web.el
+++ init.d/init-company-web.el
@@ -1,7 +1,7 @@
 (req-package company-web
   :require (company)
   
-  :config(
+  :init(
           (add-hook 'web-mode-hook (lambda ()
                                      (set (make-local-variable 'company-backends)
                                           '(company-web-html company-files))
