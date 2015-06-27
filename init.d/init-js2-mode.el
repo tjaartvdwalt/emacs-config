a2cf39172dae9134931639f7627b2c31f93bedc7
Tjaart van der Walt <github@tjaart.co.za>
2015-06-27 15:29:04 -0500

    init.d/init-js2-mode.el

--- init.d/init-js2-mode.el
+++ init.d/init-js2-mode.el
@@ -10,7 +10,7 @@
   (progn
     (autoload 'js2-mode "js2" nil t)
     ;; (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
-    (add-hook 'js2-mode-hook 'jquery-doc-setup))
+    (add-hook 'js2-mode-hook 'jquery-doc-setup)))
 
 (provide 'init-js2-mode)
 ;;; init-js ends here
