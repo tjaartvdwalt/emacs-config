54ab0405d4652eb71b694de1ac8b8252e02b9704
Tjaart van der Walt <github@tjaart.co.za>
2015-07-16 09:03:26 -0500

    init.d/init-tern.el

--- init.d/init-tern.el
+++ init.d/init-tern.el
@@ -3,5 +3,10 @@
 
 (req-package tern
   :config (progn
-            (autoload 'tern-mode "tern.el" nil t))
-  (add-hook 'js-mode-hook (lambda () (tern-mode t))))
+            ;; (autoload 'tern-mode "tern.el" nil t))
+  (add-hook 'js-mode-hook (lambda () (tern-mode t)))
+  (eval-after-load 'tern
+    '(progn
+       (require 'tern-auto-complete)
+       (tern-ac-setup)))
+  )
