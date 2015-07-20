171776cd6fe524b67d073e164aa3a17313792bbb
Tjaart van der Walt <github@tjaart.co.za>
2015-07-20 14:40:57 -0500

    init.d/init-web-beautify.el

---
+++ init.d/init-web-beautify.el
@@ -0,0 +1,24 @@
+(req-package web-beautify
+
+  :config(
+          (eval-after-load 'js2-mode
+            '(add-hook 'js2-mode-hook
+                       (lambda ()
+                         (add-hook 'before-save-hook 'web-beautify-js-buffer t t))))
+
+          (eval-after-load 'json-mode
+            '(add-hook 'json-mode-hook
+                       (lambda ()
+                         (add-hook 'before-save-hook 'web-beautify-js-buffer t t))))
+
+          (eval-after-load 'sgml-mode
+            '(add-hook 'html-mode-hook
+                       (lambda ()
+                         (add-hook 'before-save-hook 'web-beautify-html-buffer t t))))
+
+          (eval-after-load 'css-mode
+            '(add-hook 'css-mode-hook
+                       (lambda ()
+                         (add-hook 'before-save-hook 'web-beautify-css-buffer t t)))))
+
+  )
