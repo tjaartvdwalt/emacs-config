cc3f804ee5e62839981a1ef84f6784c1d0595362
Tjaart van der Walt <github@tjaart.co.za>
2015-07-20 15:01:23 -0500

    init.d/init-web-beautify.el

--- init.d/init-web-beautify.el
+++ init.d/init-web-beautify.el
@@ -13,14 +13,4 @@
            (add-hook 'json-mode-hook
                      (lambda ()
                        (add-hook 'before-save-hook 'web-beautify-js)))
-
-           ;; (eval-after-load 'web-mode
-           ;;   '(add-hook 'web-mode-hook
-           ;;              (lambda ()
-           ;;                (add-hook 'before-save-hook 'web-beautify-html t t))))
-
-           ;; (eval-after-load 'css-mode
-           ;;   '(add-hook 'css-mode-hook
-           ;;              (lambda ()
-           ;;                (add-hook 'before-save-hook 'web-beautify-css t t))))
            ))
