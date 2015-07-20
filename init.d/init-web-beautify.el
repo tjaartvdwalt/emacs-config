cef7e5865e101114e10af0377795f7e743a0b59c
Tjaart van der Walt <github@tjaart.co.za>
2015-07-20 15:01:11 -0500

    init.d/init-web-beautify.el

--- init.d/init-web-beautify.el
+++ init.d/init-web-beautify.el
@@ -1,22 +1,26 @@
 (req-package web-beautify
   :require(js-mode json-mode sgml-mode css-mode)
-  :config((eval-after-load 'js-mode
-            '(add-hook 'js-mode-hook
-                       (lambda ()
-                         (add-hook 'before-save-hook 'web-beautify-js))))
 
-          (eval-after-load 'json-mode
-            '(add-hook 'json-mode-hook
-                       (lambda ()
-                         (add-hook 'before-save-hook 'web-beautify-js))))
+  (add-hook 'c-mode-hook
+            (lambda ()
+              (add-hook 'before-save-hook 'astyle-buffer nil 'buffer-local)))
 
-          ;; (eval-after-load 'web-mode
-          ;;   '(add-hook 'web-mode-hook
-          ;;              (lambda ()
-          ;;                (add-hook 'before-save-hook 'web-beautify-html t t))))
+  :config(progn
+           (add-hook 'js-mode-hook
+                     (lambda ()
+                       (add-hook 'before-save-hook 'web-beautify-js)))
 
-          ;; (eval-after-load 'css-mode
-          ;;   '(add-hook 'css-mode-hook
-          ;;              (lambda ()
-          ;;                (add-hook 'before-save-hook 'web-beautify-css t t))))
-          ))
+           (add-hook 'json-mode-hook
+                     (lambda ()
+                       (add-hook 'before-save-hook 'web-beautify-js)))
+
+           ;; (eval-after-load 'web-mode
+           ;;   '(add-hook 'web-mode-hook
+           ;;              (lambda ()
+           ;;                (add-hook 'before-save-hook 'web-beautify-html t t))))
+
+           ;; (eval-after-load 'css-mode
+           ;;   '(add-hook 'css-mode-hook
+           ;;              (lambda ()
+           ;;                (add-hook 'before-save-hook 'web-beautify-css t t))))
+           ))
