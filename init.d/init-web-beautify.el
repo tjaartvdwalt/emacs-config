4a6da640643240f581e84bb96950c98a3b4805c3
Tjaart van der Walt <github@tjaart.co.za>
2015-07-20 15:14:12 -0500

    init.d/init-web-beautify.el

--- init.d/init-web-beautify.el
+++ init.d/init-web-beautify.el
@@ -1,13 +1,13 @@
 (req-package web-beautify
   :require(js-mode json-mode)
 
-  :init
+  :config
   (progn
-           (add-hook 'js-mode-hook
-                     (lambda ()
-                       (add-hook 'before-save-hook 'web-beautify-js-buffer nil t)))
+    (add-hook 'js-mode-hook
+              (lambda ()
+                (add-hook 'before-save-hook 'web-beautify-js-buffer nil t)))
 
-           (add-hook 'json-mode-hook
-                     (lambda ()
-                       (add-hook 'before-save-hook 'web-beautify-js-buffer nil t)))
-           ))
+    (add-hook 'json-mode-hook
+              (lambda ()
+                (add-hook 'before-save-hook 'web-beautify-js-buffer nil t)))
+    ))
