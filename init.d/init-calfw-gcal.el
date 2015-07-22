7bb85eb58aed1bb9f890aa306d5cb830ee0c5550
Tjaart van der Walt <github@tjaart.co.za>
2015-07-22 11:33:00 -0500

    deleted some files

--- init.d/init-calfw-gcal.el
+++ init.d/init-calfw-gcal.el
@@ -1,5 +1,6 @@
 (req-package calfw-gcal
-  :config(progn
+    :requires(calfw)
+    :config(progn
            (setq cfw:gcal-py-dir "/usr/bin/python2")
            (setq cfw:gcal-user "tjaart@tjaart.co.za")
            (setq cfw:gcal-pass "shgpwsmtkfwecskk")))
--- init.d/init-helm-descbinds.el
+++
@@ -1,4 +0,0 @@
-(req-package helm-descbinds
-  :bind (("C-c h z" . helm-descbinds)))
-  :config (progn
-            (helm-descbinds-mode))
