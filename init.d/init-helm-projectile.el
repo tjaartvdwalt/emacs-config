62b8a9efa6e78dc65fc53f4ab920abb6ca76a7e6
Tjaart van der Walt <github@tjaart.co.za>
2015-07-22 15:55:08 -0500

    init.d/init-helm-projectile.el

---
+++ init.d/init-helm-projectile.el
@@ -0,0 +1,7 @@
+(req-package  helm-projectile
+  :require(projectile)
+
+  :config (progn
+            (setq projectile-completion-system 'helm)
+            (helm-projectile-on)
+            (setq projectile-switch-project-action 'helm-projectile)))
