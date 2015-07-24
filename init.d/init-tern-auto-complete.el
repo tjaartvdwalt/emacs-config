1da8625f5c71f3af70fbd449a95ed7cd3e11fb53
Tjaart van der Walt <github@tjaart.co.za>
2015-07-24 18:34:57 -0500

    init.d/init-tern-auto-complete.el

--- init.d/init-tern-auto-complete.el
+++ init.d/init-tern-auto-complete.el
@@ -1,8 +1,10 @@
 (req-package tern-auto-complete
   :requires (tern auto-complete)
   :config (progn
-            (setq tern-ac-sync t)
-            (add-hook 'js-mode-hook
+            (setq tern-ac-sync t))
+
+  :init (progn
+         (add-hook 'js-mode-hook
                       (lambda ()
                         (message "in js mode hook")
                         (add-to-list 'ac-sources 'ac-source-tern-completion)))))
