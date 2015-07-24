343992adeee36f81ba966e1ebbfa58df22ae7c59
Tjaart van der Walt <github@tjaart.co.za>
2015-07-24 18:41:34 -0500

    init.d/init-ac-ispell.el

--- init.d/init-ac-ispell.el
+++ init.d/init-ac-ispell.el
@@ -1,5 +1,5 @@
 (req-package ac-ispell
-  :requires (autocomplete)
+  :require (autocomplete)
   :config (progn
             (ac-ispell-setup)
             (add-hook 'text-mode-hook 'ac-ispell-ac-setup)))
