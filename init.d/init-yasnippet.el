941df638d546cb6026aee63b438dfcaee8efe0b1
Tjaart van der Walt <github@tjaart.co.za>
2015-07-24 22:51:22 -0500

    init.d/init-yasnippet.el

--- init.d/init-yasnippet.el
+++ init.d/init-yasnippet.el
@@ -5,6 +5,6 @@
 
   :config
   (yas-global-mode 1)
-  (define-key yas-minor-mode-map (kbd "<tab>") nil)
-  (define-key yas-minor-mode-map (kbd "TAB") nil)
+  ;; (define-key yas-minor-mode-map (kbd "<tab>") nil)
+  ;; (define-key yas-minor-mode-map (kbd "TAB") nil)
   )
