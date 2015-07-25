f250ba260c3b60bfc820105ed890dc973474727f
Tjaart van der Walt <github@tjaart.co.za>
2015-07-24 22:16:41 -0500

    init.d/init-auto-complete.el

--- init.d/init-auto-complete.el
+++ init.d/init-auto-complete.el
@@ -1,6 +1,7 @@
 (req-package auto-complete
   :require (yasnippet)
   :config (progn
-            (setq-default ac-sources '(ac-source-abbrev ac-source-dictionary ac-source-filename ac-source-words-in-same-mode-buffers ac-source-yasnippet))
+            (setq-default ac-sources '(ac-source-abbrev ac-source-dictionary ac-source-filename ac-source-words-in-same-mode-buffers
+                                                        ))
             (global-auto-complete-mode 1)
             (setq ac-auto-start t)))
