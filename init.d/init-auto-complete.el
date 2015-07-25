3033754a90bfb4625d7f1b454dc16cb50cd2ea06
Tjaart van der Walt <github@tjaart.co.za>
2015-07-24 20:09:56 -0500

    init.d/init-auto-complete.el

--- init.d/init-auto-complete.el
+++ init.d/init-auto-complete.el
@@ -1,4 +1,5 @@
 (req-package auto-complete
   :config (progn
             (setq-default ac-sources '(ac-source-abbrev ac-source-dictionary ac-source-filename ac-source-words-in-same-mode-buffers ac-source-yasnippet))
+            (ac-set-trigger-key "TAB")
             (setq ac-auto-start t)))
