7fa0680f217f7ff82d707d705d2e5113f0ca67fb
Tjaart van der Walt <github@tjaart.co.za>
2015-07-24 22:29:33 -0500

    init.d/init-auto-complete.el

--- init.d/init-auto-complete.el
+++ init.d/init-auto-complete.el
@@ -3,6 +3,5 @@
   :config (progn
             (setq ac-use-fuzzy t)
             (setq-default ac-sources '(ac-source-abbrev ac-source-dictionary ac-source-filename ac-source-words-in-same-mode-buffers))
-            
             (global-auto-complete-mode 1)
             (setq ac-auto-start t)))
