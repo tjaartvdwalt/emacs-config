0d00daeb1cf06c7783482394f80e6ae8764121ef
Tjaart van der Walt <github@tjaart.co.za>
2015-07-25 23:10:52 -0500

    init.d/init-js-mode.el

--- init.d/init-js-mode.el
+++ init.d/init-js-mode.el
@@ -1,5 +1,2 @@
 ( :config(progn
-          (setq-default flycheck-disabled-checkers
-                        (append flycheck-disabled-checkers
-                                '(javascript-jshint)))
-          (flycheck-add-mode 'javascript-standard 'js-mode)))
+         )
