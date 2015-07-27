f2f3e4b8a66ed3e2d6f728c85d25f0c8a488a488
Tjaart van der Walt <github@tjaart.co.za>
2015-07-27 13:33:35 -0500

    functions/formatter/formatter.el

--- functions/formatter/formatter.el
+++ functions/formatter/formatter.el
@@ -28,7 +28,7 @@
   (setq total-chars (point-max))
   (setq temp-line (line-number-at-pos))
   (setq temp-point (point))
-  (formatter-format-region (point-min) (point-max) nil)
+  (formatter-format-region (point-min) (point-max) 'f)
 
   (goto-char temp-point))
 
