c3d51b3a76ba2ed3a84e8c81b66e9cfb24c9dba5
Tjaart van der Walt <github@tjaart.co.za>
2015-07-26 19:14:32 -0500

    functions/standard-format.el

--- functions/standard-format.el
+++ functions/standard-format.el
@@ -1,8 +1,7 @@
 (defun format-buffer ()
-"Applies Javascript standard-format to the current buffer.
+  "Applies Javascript standard-format to the current buffer.
 https://www.npmjs.com/package/standard-format"
   (interactive)
-
   (setq total-chars (point-max))
   (setq temp-line (line-number-at-pos))
   (setq temp-point (point))
@@ -11,10 +10,10 @@ https://www.npmjs.com/package/standard-format"
 
 
 (defun standard-format-region (pmin pmax)
-    (shell-command-on-region pmin pmax
-                             "standard-format -"
-                             (current-buffer) t
-                             (get-buffer-create "*standard-format Errors*") t))
+  (shell-command-on-region pmin pmax
+                           "standard-format -"
+                           (current-buffer) t
+                           (get-buffer-create "*standard-format Errors*") t))
 
 (format-buffer)
 (standard-format-region (point-min) (point-max))
