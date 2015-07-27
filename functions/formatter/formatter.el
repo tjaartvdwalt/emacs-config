a225627b1d5bc543b302585f43594246e6161e15
Tjaart van der Walt <github@tjaart.co.za>
2015-07-26 23:32:52 -0500

    functions/formatter/formatter.el

--- functions/formatter/formatter.el
+++ functions/formatter/formatter.el
@@ -4,13 +4,20 @@
 
 (cl-defstruct formatter command modes)
 
+(setq formatters '())
+
 (setq javascript-standard-format (make-formatter
                                   :command "standard-format -"
                                   :modes '(js-mode js2-mode js3-mode)))
 
+(add-to-list 'formatters 'javascript-standard-format)
+
+(print formatters)
 (setq c-astyle-format (make-formatter
                        :command "astyle"
-                       :modes '(c-mode c++-mode))
+                       :modes '(c-mode c++-mode)))
+
+(add-to-list 'formatters 'javascript-standard-format)
 
 ;;;###autoload
 (defun formatter-format-buffer ()
@@ -25,7 +32,7 @@
 
 (defun formatter-format-region (pmin pmax)
 
-       (shell-command-on-region pmin pmax
-                                "standard-format -"
-                                (current-buffer) t
-                                (get-buffer-create "*standard-format Errors*") t))
+  (shell-command-on-region pmin pmax
+                           "standard-format -"
+                           (current-buffer) t
+                           (get-buffer-create "*standard-format Errors*") t))
