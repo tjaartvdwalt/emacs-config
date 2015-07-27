47cb40e3dcfff576b69a5e80d336a23498cb988c
Tjaart van der Walt <github@tjaart.co.za>
2015-07-27 14:40:38 -0500

    functions/formatter/formatter.el

--- functions/formatter/formatter.el
+++ functions/formatter/formatter.el
@@ -31,6 +31,7 @@
   (print  "error")
 
   (print "na")
+  (string-prefix-p "stdin: Error" (formatter-test-region (point-min) (point-max)))
   (print(length (formatter-test-region (point-min) (point-max))))
   ;; (formatter-test-region (point-min) (point-max))
   (if (=(length (formatter-test-region (point-min) (point-max))) 0)
