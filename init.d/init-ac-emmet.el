b59e4cab12039e75496ccb242f8dff9622331125
Tjaart van der Walt <github@tjaart.co.za>
2015-07-29 08:42:44 -0500

    init.d/init-ac-emmet.el

--- init.d/init-ac-emmet.el
+++ init.d/init-ac-emmet.el
@@ -1,7 +1,6 @@
 (req-package ac-emmet
   :require (auto-complete emmet-mode)
 
-:config (progn
- (add-hook 'sgml-mode-hook 'ac-emmet-html-setup)
- (add-hook 'css-mode-hook 'ac-emmet-css-setup)))
-
+  :config (progn
+            (add-hook 'sgml-mode-hook 'ac-emmet-html-setup)
+            (add-hook 'css-mode-hook 'ac-emmet-css-setup)))
