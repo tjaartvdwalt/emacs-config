0cb8e06c664a1ab47c7cdfe9ea9d0e37bd44cb44
Tjaart van der Walt <github@tjaart.co.za>
2015-07-29 09:55:02 -0500

    init.d/init-smartparens.el

--- init.d/init-smartparens.el
+++ init.d/init-smartparens.el
@@ -1,4 +1,18 @@
 (req-package smartparens-config
   :ensure smartparens
-  :config (smartparens-global-mode))
+  :config (progn
+            (define-key smartparens-mode-map (kbd "C-M-d") 'sp-down-sexp)
+            (define-key smartparens-mode-map (kbd "C-M-a") 'sp-backward-down-sexp)
+            (define-key smartparens-mode-map (kbd "C-S-d") 'sp-beginning-of-sexp)
+            (define-key smartparens-mode-map (kbd "C-S-a") 'sp-end-of-sexp)
 
+            (define-key smartparens-mode-map (kbd "C-M-e") 'sp-up-sexp)
+            (define-key smartparens-mode-map (kbd "C-M-u") 'sp-backward-up-sexp)
+            (define-key smartparens-mode-map (kbd "C-M-t") 'sp-transpose-sexp)
+
+            (define-key smartparens-mode-map (kbd "C-M-n") 'sp-next-sexp)
+            (define-key smartparens-mode-map (kbd "C-M-p") 'sp-previous-sexp)
+
+            (define-key smartparens-mode-map (kbd "C-M-k") 'sp-kill-sexp)
+            (define-key smartparens-mode-map (kbd "C-M-w") 'sp-copy-sexp)
+            ))
