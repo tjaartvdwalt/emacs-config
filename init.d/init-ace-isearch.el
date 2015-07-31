d918511e9a2c1b0657107b639acbc14aca9f1f76
Tjaart van der Walt <github@tjaart.co.za>
2015-07-31 09:19:05 -0500

    init.d/init-ace-isearch.el

--- init.d/init-ace-isearch.el
+++ init.d/init-ace-isearch.el
@@ -1,8 +1,8 @@
 (req-package ace-isearch
   :require helm-swoop
   :config(progn (global-ace-isearch-mode +1)
-              (setq ace-isearch-input-idle-delay 1.5)
+              (setq ace-isearch-jump-delay 1.5)
               ;; I don't like ace-isearch to use helm-swoop... easiest
               ;; way for me to avoid this is to set the threshold to a
               ;; ridiculously large number
-              (setq ace-isearch-input-length 100)))
+              (setq ace-isearch-input-length 6)))
