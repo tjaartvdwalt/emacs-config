aea8c1e8adb7680b8b15e20cbb2acf206481aa62
Tjaart van der Walt <github@tjaart.co.za>
2015-07-31 22:54:52 -0500

    init.d/init-web-mode.el

--- init.d/init-web-mode.el
+++ init.d/init-web-mode.el
@@ -35,6 +35,22 @@
                      (setq emmet-use-css-transform nil)))))
 
 
+    ;; (add-hook 'web-mode-before-auto-complete-hooks
+    ;;           '(lambda ()
+    ;;              (let ((web-mode-cur-language
+    ;;                     (web-mode-language-at-pos)))
+    ;;                (if (string= web-mode-cur-language "php")
+    ;;                    (yas-activate-extra-mode 'php-mode)
+    ;;                  (yas-deactivate-extra-mode 'php-mode))
+    ;;                (if (string= web-mode-cur-language "css")
+    ;;                    (setq emmet-use-css-transform t)
+    ;;                  (setq emmet-use-css-transform nil))
+    ;;                )))
+
+    ;; (setq web-mode-ac-sources-alist
+    ;;       '(("php" . (ac-source-php-auto-yasnippets ac-source-yasnippet))
+    ;;         ("html" . (ac-source-emmet-html-aliases ac-source-emmet-html-snippets))
+    ;;         ("css" . (ac-source-css-property))))
 
     
     (add-hook 'web-mode-hook
