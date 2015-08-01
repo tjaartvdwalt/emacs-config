2a8a841f7e1e2769833c9f1ed47d2e1320b0dd20
Tjaart van der Walt <github@tjaart.co.za>
2015-07-31 23:01:30 -0500

    init.d/init-emmet-mode.el

--- init.d/init-emmet-mode.el
+++ init.d/init-emmet-mode.el
@@ -1,6 +1,5 @@
 (req-package emmet-mode
   :config((progn
             (add-hook 'sgml-mode-hook 'emmet-mode) 
-            (add-hook 'css-mode-hook  'emmet-mode) 
-            )))
+            (add-hook 'css-mode-hook  'emmet-mode))))
 
