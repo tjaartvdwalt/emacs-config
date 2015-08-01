4c7a38f104f45816274686aa673c596f68629c69
Tjaart van der Walt <github@tjaart.co.za>
2015-07-31 22:50:48 -0500

    init.d/init-web-mode.el

--- init.d/init-web-mode.el
+++ init.d/init-web-mode.el
@@ -22,17 +22,22 @@
     (add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
 
 
-    (setq web-mode-ac-sources-alist
-          '(("html" . (ac-source-emmet-html-aliases ac-source-emmet-html-snippets))
-            ("css" . (ac-source-css-property ac-source-emmet-css-snippets))))
-
     (add-hook 'web-mode-before-auto-complete-hooks
               '(lambda ()
                  (let ((web-mode-cur-language
                         (web-mode-language-at-pos)))
+                   (if (string= web-mode-cur-language "php")
+                       (yas-activate-extra-mode 'php-mode)
+                     (yas-deactivate-extra-mode 'php-mode))
                    (if (string= web-mode-cur-language "css")
                        (setq emmet-use-css-transform t)
-                     (setq emmet-use-css-transform nil)))))
+                     (setq emmet-use-css-transform nil))
+                   )))
+
+    (setq web-mode-ac-sources-alist
+          '(("php" . (ac-source-php-auto-yasnippets ac-source-yasnippet))
+            ("html" . (ac-source-emmet-html-aliases ac-source-emmet-html-snippets))
+            ("css" . (ac-source-css-property))))
 
     
     (add-hook 'web-mode-hook
