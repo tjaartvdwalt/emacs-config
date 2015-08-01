b44b75b2de6c62285e85fdfdc120ea750de6ebdc
Tjaart van der Walt <github@tjaart.co.za>
2015-07-31 19:09:22 -0500

    init.d/init-smartparens.el

--- init.d/init-smartparens.el
+++ init.d/init-smartparens.el
@@ -1,15 +1,15 @@
 (req-package smartparens-config
   :ensure smartparens
   :config (progn
-            (smartparens-global-mode t)
-            (define-key smartparens-mode-map (kbd "C-M-d") 'sp-down-sexp)
-            (define-key smartparens-mode-map (kbd "C-M-a") 'sp-backward-down-sexp)
-            (define-key smartparens-mode-map (kbd "C-M-e") 'sp-up-sexp)
-            (define-key smartparens-mode-map (kbd "C-M-u") 'sp-backward-up-sexp)
+            ;; (smartparens-global-mode t)
+            ;; (define-key smartparens-mode-map (kbd "C-M-d") 'sp-down-sexp)
+            ;; (define-key smartparens-mode-map (kbd "C-M-a") 'sp-backward-down-sexp)
+            ;; (define-key smartparens-mode-map (kbd "C-M-e") 'sp-up-sexp)
+            ;; (define-key smartparens-mode-map (kbd "C-M-u") 'sp-backward-up-sexp)
 
-            (define-key smartparens-mode-map (kbd "C-S-d") 'sp-beginning-of-sexp)
-            (define-key smartparens-mode-map (kbd "C-S-a") 'sp-end-of-sexp)
-
+            
+            (define-key smartparens-mode-map (kbd "C-S-a") 'sp-beginning-of-sexp)
+            (define-key smartparens-mode-map (kbd "C-S-e") 'sp-end-of-sexp)
             
             (define-key smartparens-mode-map (kbd "C-M-n") 'sp-next-sexp)
             (define-key smartparens-mode-map (kbd "C-M-p") 'sp-previous-sexp)
