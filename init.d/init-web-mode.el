ee6bce2bec421b7003ccf20ffe8e02689f84a9fa
Tjaart van der Walt <github@tjaart.co.za>
2015-08-01 09:58:27 -0500

    init.d/init-web-mode.el

--- init.d/init-web-mode.el
+++ init.d/init-web-mode.el
@@ -17,8 +17,8 @@
     (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
     (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
     (add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
-    ;; (add-to-list 'auto-mode-alist '("\\.sass\\'" . web-mode))
-    ;; (add-to-list 'auto-mode-alist '("\\.scss\\'" . web-mode))
+    (add-to-list 'auto-mode-alist '("\\.sass\\'" . web-mode))
+    (add-to-list 'auto-mode-alist '("\\.scss\\'" . web-mode))
     (add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
 
 
