5a73946f612573c35d746ac6ea14073b36822361
Tjaart van der Walt <github@tjaart.co.za>
2015-08-02 12:43:40 -0500

    init.d/init-column-enforce.el

--- init.d/init-column-enforce.el
+++ init.d/init-column-enforce.el
@@ -3,9 +3,10 @@
 ;;; Code:
 
 (req-package column-enforce-mode
+  :diminish column-enforce-mode
   :defer 1
   :config (progn (add-hook 'prog-mode-hook 'column-enforce-mode)
-               ;; (global-set-key "\C-ce" 'column-enforce-mode)
-               ))
+                 ;; (global-set-key "\C-ce" 'column-enforce-mode)
+                 ))
 (provide 'init-column-enforce)
 ;;; init-column-enforce ends here
