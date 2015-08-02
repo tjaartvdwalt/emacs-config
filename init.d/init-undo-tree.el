d5c6a1435f6153fe6b5aa12c893f564c6af533ed
Tjaart van der Walt <github@tjaart.co.za>
2015-08-02 08:24:26 -0500

    init.d/init-undo-tree.el

--- init.d/init-undo-tree.el
+++ init.d/init-undo-tree.el
@@ -1,8 +1,9 @@
 (req-package undo-tree
   ;; :bind (("M-/" . undo-tree-redo)
   ;;        ("C-x u" . undo-tree-visualize))
- :config (progn
-        (global-unset-key (kbd "M-/"))
-          (global-set-key "\M-/" 'undo-tree-redo)
-          (global-set-key "\C-xu" 'undo-tree-visualize)
-          (global-undo-tree-mode 1)))
+  :diminish undo-tree-mode
+  :config (progn
+            (global-unset-key (kbd "M-/"))
+            (global-set-key "\M-/" 'undo-tree-redo)
+            (global-set-key "\C-xu" 'undo-tree-visualize)
+            (global-undo-tree-mode 1)))
