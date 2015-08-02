2fc8e27f8f73e34a06364d358b6e521271a29d1c
Tjaart van der Walt <github@tjaart.co.za>
2015-08-02 12:37:20 -0500

    init.d/init-diredx.el

--- init.d/init-diredx.el
+++ init.d/init-diredx.el
@@ -3,6 +3,7 @@
   :require dired
   :config
   (progn
+    (setq dired-omit-mode t)
     (setq-default dired-omit-files-p t)
     ;; 1) Hide files that start with # (emacs tmp files)
     ;; 2) Hide files that start with . then not a dot then anything
