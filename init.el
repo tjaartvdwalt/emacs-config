eded64b4245a1028d88cf96f59abe39bbd1b6f85
Tjaart van der Walt <github@tjaart.co.za>
2015-08-08 16:44:05 -0500

    init.el

--- init.el
+++ init.el
@@ -69,5 +69,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Cousine" :foundry "monotype" :slant normal :weight normal :height 128 :width normal)))))
+ ;; '(default ((t (:family "Cousine" :foundry "monotype" :slant normal :weight normal :height 128 :width normal)))))
 (put 'upcase-region 'disabled nil)
