a554b4766c8dd1a349360cd319ade251e546140c
Tjaart van der Walt <github@tjaart.co.za>
2015-08-22 20:26:11 -0500

    init.d/init-ac-emoji.el

--- init.d/init-ac-emoji.el
+++ init.d/init-ac-emoji.el
@@ -1,4 +1,5 @@
 (req-package ac-emoji
+  :require (auto-complete)
   :config(progn
            (add-hook 'markdown-mode-hook 'ac-emoji-setup)
            (add-hook 'git-commit-mode-hook 'ac-emoji-setup)))
