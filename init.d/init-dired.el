217dc0986c52f0795e541eebe5b8b015ed7e7985
Tjaart van der Walt <github@tjaart.co.za>
2015-08-22 20:12:24 -0500

    init.d/init-dired.el

--- init.d/init-dired.el
+++ init.d/init-dired.el
@@ -10,6 +10,7 @@
     ;; used to attach files in mu4e
     (add-hook 'dired-mode-hook 'turn-on-gnus-dired-mode)
     (setq diredp-hide-details-initially-flag nil)
+    (setq dired-auto-revert-buffer t)
     (define-key dired-mode-map (kbd "\\") 'dired-get-size)))
 
 (provide 'init-dired)
