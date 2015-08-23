4bba885dcafa74d7943ed8abb099014097f5e3da
Tjaart van der Walt <github@tjaart.co.za>
2015-08-22 20:42:15 -0500

    init.d/init-auto-complete.el

--- init.d/init-auto-complete.el
+++ init.d/init-auto-complete.el
@@ -6,4 +6,5 @@
             (setq-default ac-sources '(ac-source-yasnippet ac-source-abbrev ac-source-dictionary ac-source-filename ac-source-words-in-same-mode-buffers))
             (global-auto-complete-mode 1)
             (setq ac-auto-start t)
+            (ac-flyspell-workaround)
             (define-key ac-mode-map (kbd "TAB") 'auto-complete)))
