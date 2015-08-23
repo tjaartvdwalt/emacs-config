121ba81c3972452552affc4252fc4000f121f5bf
Tjaart van der Walt <github@tjaart.co.za>
2015-08-23 18:52:45 -0500

    init.d/init-go.el

--- init.d/init-go.el
+++ init.d/init-go.el
@@ -1,16 +1,17 @@
 (req-package go-mode
-:require (go-eldoc yasnippet go-snippets gotest)
-;; :bind
-;;        (("M-." . godef-jump)
-;;         ("C-c C-r" . go-remove-unused-imports)
-;;         ("C-c C-p" . go-test-current-project)
-;;         ("C-c C-f" . go-test-current-file)
-;;         ("C-c C-t" . go-test-current-test))
+  :defer t
+  :require (go-eldoc yasnippet go-snippets gotest)
+  ;; :bind
+  ;;        (("M-." . godef-jump)
+  ;;         ("C-c C-r" . go-remove-unused-imports)
+  ;;         ("C-c C-p" . go-test-current-project)
+  ;;         ("C-c C-f" . go-test-current-file)
+  ;;         ("C-c C-t" . go-test-current-test))
 
-:config
-(progn
-        (add-hook 'before-save-hook 'gofmt-before-save)
-        (add-hook 'go-mode-hook 'go-eldoc-setup)
-        ;;(add-to-list 'company-backends 'company-go)
-        ))
+  :config
+  (progn
+    (add-hook 'before-save-hook 'gofmt-before-save)
+    (add-hook 'go-mode-hook 'go-eldoc-setup)
+    ;;(add-to-list 'company-backends 'company-go)
+    ))
 
