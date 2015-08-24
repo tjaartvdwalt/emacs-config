37bf0373d022548b3155862c6e33e52591cde3aa
Tjaart van der Walt <github@tjaart.co.za>
2015-08-23 19:08:53 -0500

    init.d/init-powerline.el

--- init.d/init-powerline.el
+++ init.d/init-powerline.el
@@ -1,3 +1,4 @@
 (req-package powerline
   :config(progn
-           (add-hook 'after-init-hook (powerline-default-theme))))
+           ;; (add-hook 'after-init-hook (powerline-default-theme))
+           ))
