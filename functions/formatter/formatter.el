13e1c2c535ef2e91608e72f41e83479dcd2bacc9
Tjaart van der Walt <github@tjaart.co.za>
2015-08-25 10:47:30 -0500

    functions/formatter/formatter.el

--- functions/formatter/formatter.el
+++ functions/formatter/formatter.el
@@ -77,8 +77,8 @@ from https://github.com/bradfitz/goimports."
               (message "Applied gofmt")))
         (message "Could not apply gofmt"))
 
-      ;; (kill-buffer patchbuf)
-      ;; (delete-file tmpfile)
+      (kill-buffer patchbuf)
+      (delete-file tmpfile)
       )))
 
 (defun go--apply-rcs-patch (patch-buffer)
