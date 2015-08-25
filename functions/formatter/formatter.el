30bada656c9f6c896c41b41d09dd755f81cc4320
Tjaart van der Walt <github@tjaart.co.za>
2015-08-25 10:43:28 -0500

    functions/formatter/formatter.el

--- functions/formatter/formatter.el
+++ functions/formatter/formatter.el
@@ -1,46 +1,46 @@
-(defgroup formatter ()
-  "Free keys."
-  :group 'convenience)
+;; (defgroup formatter ()
+;;   "Free keys."
+;;   :group 'convenience)
 
-(cl-defstruct formatter command modes)
+;; (cl-defstruct formatter command modes)
 
-(setq formatter-formatters
-      (list
-       (make-formatter
-        :command "astyle"
-        :modes '(c-mode c++-mode))
+;; (setq formatter-formatters
+;;       (list
+;;        (make-formatter
+;;         :command "astyle"
+;;         :modes '(c-mode c++-mode))
 
-       (make-formatter
-        :command "snazzy --format --stdin"
-        :modes '(js-mode js2-mode js3-mode))
-       ))
+;;        (make-formatter
+;;         :command "snazzy --format --stdin"
+;;         :modes '(js-mode js2-mode js3-mode))
+;;        ))
 
-(defun get-formatter-command ()
-  (dolist (f formatter-formatters)
-    (if (member major-mode (formatter-modes f))
-        (setq command (formatter-command f))))
-  command)
+;; (defun get-formatter-command ()
+;;   (dolist (f formatter-formatters)
+;;     (if (member major-mode (formatter-modes f))
+;;         (setq command (formatter-command f))))
+;;   command)
 
- ;;;###autoload
-(defun formatter-format-buffer ()
-  "Format the current buffer with the apropriate formatter."
-  (interactive)
-  (setq total-chars (point-max))
-  (setq temp-line (line-number-at-pos))
-  (setq temp-point (point))
+;;  ;;;###autoload
+;; (defun formatter-format-buffer ()
+;;   "Format the current buffer with the apropriate formatter."
+;;   (interactive)
+;;   (setq total-chars (point-max))
+;;   (setq temp-line (line-number-at-pos))
+;;   (setq temp-point (point))
 
-  (formatter-format-region (point-min) (point-max))
-  (goto-char temp-point))
+;;   (formatter-format-region (point-min) (point-max))
+;;   (goto-char temp-point))
 
-(defun formatter-format-region (pmin pmax)
-  (shell-command-on-region pmin pmax
-                           (get-formatter-command)
-                           t t
-                           (get-buffer-create "*Formatter Errors*") t))
+;; (defun formatter-format-region (pmin pmax)
+;;   (shell-command-on-region pmin pmax
+;;                            (get-formatter-command)
+;;                            t t
+;;                            (get-buffer-create "*Formatter Errors*") t))
 
 
-(provide 'formatter)
-;;; formatter ends here
+;; (provide 'formatter)
+;; ;;; formatter ends here
 (defcustom standardfmt-command "standard"
   "The 'gofmt' command.
 Some users may replace this with 'goimports'
