1c1b04538c7e6c036fa8b33ddc479570591c6e6c
Tjaart van der Walt <github@tjaart.co.za>
2015-09-01 10:37:18 -0500

    init.d/init-magit.el

--- init.d/init-magit.el
+++ init.d/init-magit.el
@@ -6,9 +6,10 @@
   :bind ("C-c v m" . magit-status)
   :config
   (progn
+    (setq magit-revert-buffers "ask")
     (setq magit-last-seen-setup-instructions "1.4.0")
     ;; (when (not window-system)
-      ;; (set-face-background 'magit-item-highlight "black"))
+    ;; (set-face-background 'magit-item-highlight "black"))
     (add-hook 'magit-status-mode-hook (lambda () (magit-filenotify-mode t)))))
 (provide 'init-magit)
 ;;; init-magit ends here
