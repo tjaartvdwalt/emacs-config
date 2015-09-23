be8a626360907fa2433c5b9506b22b8adffd256f
Tjaart van der Walt <github@tjaart.co.za>
2015-09-23 13:59:55 -0500

    init.d/init-speedbar.el

--- init.d/init-speedbar.el
+++ init.d/init-speedbar.el
@@ -1,5 +1,7 @@
 (req-package speedbar
 
   :config((progn
-            (global-unset-key (kbd "M-\\\\")))))
+            (global-unset-key (kbd "C-\\\\"))
+            (global-set-key "\C-\\" 'speedbar))))
+
 
