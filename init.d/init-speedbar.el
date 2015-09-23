f1df26d04ed622378c296ebb5fbaeca949b6207e
Tjaart van der Walt <github@tjaart.co.za>
2015-09-23 15:35:48 -0500

    init.d/init-speedbar.el

--- init.d/init-speedbar.el
+++ init.d/init-speedbar.el
@@ -1,9 +1,10 @@
 (req-package speedbar
   :bind ("C-c s" . speedbar-disp))
 
-
+;; resize the speedbar window after we open it
 (defun speedbar-disp()
   "Format the current buffer according to the standardfmt tool."
+  (interactive)
   (speedbar)
   (call-process "i3-msg" nil nil nil "resize shrink left")
   (call-process "i3-msg" nil nil nil "resize shrink left"))
