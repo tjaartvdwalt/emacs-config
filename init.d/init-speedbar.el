508de94fc6ea583e1d8217fa9142a747c236e733
Tjaart van der Walt <github@tjaart.co.za>
2015-09-23 15:22:20 -0500

    init.d/init-speedbar.el

--- init.d/init-speedbar.el
+++ init.d/init-speedbar.el
@@ -4,4 +4,4 @@
 
 (defun speedbar-disp()
   "Format the current buffer according to the standardfmt tool."
-  (call-process "i3-msg 'workspace 4:emacs; resize shrink left'"))
+  (call-process "i3-msg 'workspace 4:emacs; resize shrink left'" nil errbuf nil "" nil))
