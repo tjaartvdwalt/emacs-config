f196ee48d0d957551e76fcd5b6559149e28f74da
Tjaart van der Walt <github@tjaart.co.za>
2015-09-23 10:01:56 -0500

    init.d/init-json-snatcher.el

---
+++ init.d/init-json-snatcher.el
@@ -0,0 +1,8 @@
+(req-package json-snatcher
+  :config((progn
+            defun js-mode-bindings ()
+            "Sets a hotkey for using the json-snatcher plugin"
+            (when (string-match  "\\.json$" (buffer-name))
+              (local-set-key (kbd "C-c M-w") 'jsons-print-path)))
+          (add-hook 'js-mode-hook 'js-mode-bindings)
+          (add-hook 'js2-mode-hook 'js-mode-bindings))))
