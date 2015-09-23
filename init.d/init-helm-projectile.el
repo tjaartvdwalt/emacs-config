32a29acd36dddb95e49e577599ec1c34fe99a224
Tjaart van der Walt <github@tjaart.co.za>
2015-09-23 10:02:09 -0500

    init.d/init-helm-projectile.el

--- init.d/init-helm-projectile.el
+++ init.d/init-helm-projectile.el
@@ -4,4 +4,6 @@
   :config (progn
             (setq projectile-completion-system 'helm)
             (helm-projectile-on)
-            (setq projectile-switch-project-action 'helm-projectile)))
+            ;; (setq projectile-switch-project-action 'helm-projectile)
+            (setq projectile-switch-project-action 'projectile-find-dir)
+            (setq projectile-find-dir-includes-top-level t)))
