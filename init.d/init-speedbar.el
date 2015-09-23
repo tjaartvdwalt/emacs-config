743bcb71b2abe143acb74253a8a39414d71421cd
Tjaart van der Walt <github@tjaart.co.za>
2015-09-23 14:17:40 -0500

    init.d/init-speedbar.el

--- init.d/init-speedbar.el
+++ init.d/init-speedbar.el
@@ -1,6 +1,7 @@
 (req-package speedbar
+  :bind ("C-c s" . speedbar))
 
-  :config((progn
-            ;; (global-set-key (kbd "C-cs\\\") nil))))
-            (global-set-key (kbd "C-c s") 'speedbar))))
+;; :config((progn
+;;           ;; (global-set-key (kbd "C-cs\\\") nil))))
+;;           (global-set-key (kbd "C-c s") 'speedbar))))
 
