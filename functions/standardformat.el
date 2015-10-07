efbc0f35cbac4c8ca68ee5c8452c973c990725d5
Tjaart van der Walt <github@tjaart.co.za>
2015-10-07 08:24:18 -0500

    functions/standardformat.el

--- functions/standardformat.el
+++ functions/standardformat.el
@@ -7,4 +7,12 @@
 
     (call-process "standard-format" nil errbuf nil "-w" (buffer-file-name))
     (revert-buffer t t)
-    (display-buffer errbuf)))
+    (display-buffer errbuf)
+
+    (kill-error-buffer errbuf)))
+
+(defun kill-error-buffer (errbuf)
+  (let ((win (get-buffer-window errbuf)))
+    (if win
+        (quit-window t win)
+      (kill-buffer errbuf))))
