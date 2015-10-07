96afa8a6e5302370076e10e6e4c157a7dcc3bc46
Tjaart van der Walt <github@tjaart.co.za>
2015-10-06 23:00:39 -0500

    functions/standardformat.el

--- functions/standardformat.el
+++ functions/standardformat.el
@@ -1,4 +1,4 @@
-(revert-buffer true true nil)
-        (errbuf (get-buffer-create "*Standardformat Errors*"))
+;; (revert-buffer true true nil)
+;;         (errbuf (get-buffer-create "*Standardformat Errors*"))
 
 (call-process "standard-format" nil nil nil "-w" (buffer-file-name))
