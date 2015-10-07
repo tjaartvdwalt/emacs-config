7e4fa2df4cdf953f49e4d9ebd06fee263e4eb1e4
Tjaart van der Walt <github@tjaart.co.za>
2015-10-07 08:11:52 -0500

    functions/standardformat.el

--- functions/standardformat.el
+++ functions/standardformat.el
@@ -1,7 +1,7 @@
 (defun standardfmt ()
   "Format the current buffer using standard-format."
   (interactive)
-  (setq errbuf (get-buffer-create "*StandardFormat Errors*"))
   (kill-buffer errbuf)
+  (setq errbuf (get-buffer-create "*StandardFormat Errors*"))
   (call-process "standard-format" nil errbuf nil "-w" (buffer-file-name))
   (revert-buffer t t))
