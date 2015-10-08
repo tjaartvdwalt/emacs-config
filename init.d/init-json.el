323016f30059fd79f4ebce9b3415205f25473c7e
Tjaart van der Walt <github@tjaart.co.za>
2015-10-08 16:29:57 -0500

    init.d/init-json.el

--- init.d/init-json.el
+++ init.d/init-json.el
@@ -1,4 +1,9 @@
 (req-package json-mode
   :require (flycheck)
   :config(progn
-           (flycheck-add-mode 'json-jsonlint 'json-mode)))
+           (flycheck-add-mode 'json-jsonlint 'json-mode)
+           (add-hook 'json-mode-hook
+                     (lambda ()
+                       (if (derived-mode-p 'json-mode)
+                         ;; (local-unset-key (kbd "\C-c u i"))
+                         (add-hook 'after-save-hook 'indent-whole-buffer t t))))))
