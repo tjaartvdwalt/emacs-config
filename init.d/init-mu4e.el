c6cb6156528397ddeadb16a26f7131b90cfd98d0
Tjaart van der Walt <github@tjaart.co.za>
2015-10-15 16:14:46 -0500

    init.d/init-mu4e.el

--- init.d/init-mu4e.el
+++ init.d/init-mu4e.el
@@ -179,6 +179,7 @@
     ;; sending message since message-send-mail-hook is processed right
     ;; before sending message.
     (defun choose-msmtp-account ()
+      (message "choose-msmtp-account")
       (if (message-mail-p)
           (save-excursion
             (let*
