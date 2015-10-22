9ca6c59bb5797c8fa73bb754057745c227e6dbec
Tjaart van der Walt <github@tjaart.co.za>
2015-10-22 12:02:37 -0500

    functions/ctags.el

--- functions/ctags.el
+++ functions/ctags.el
@@ -1,7 +1,7 @@
 (defun build-ctags ()
 (interactive)
 (message "building project tags")
-(let ((root '("/home/tjaart/solmates/mvp_webclient/www/js/view/")))
+(let ((root "/home/tjaart/solmates/mvp_webclient/www/js/view/"))
   (shell-command (concat "ctags -e -R --extra=+fq --exclude=db --exclude=test --exclude=.git --exclude=public -f " root "TAGS " root))
 (visit-project-tags)
 (message "tags built successfully")))
