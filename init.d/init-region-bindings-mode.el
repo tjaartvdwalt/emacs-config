fe3180d01d92c1f06595e8070304ca28019a384c
Tjaart van der Walt <github@tjaart.co.za>
2015-10-23 14:43:06 -0500

    init.d/init-region-bindings-mode.el

--- init.d/init-region-bindings-mode.el
+++ init.d/init-region-bindings-mode.el
@@ -4,4 +4,4 @@
             (define-key region-bindings-mode-map "r" 'er/expand-region)
             (define-key region-bindings-mode-map "q" 'region-bindings-mode-off)
             (define-key region-bindings-mode-map "w" 'mark-whole-buffer)
-            )))
+
