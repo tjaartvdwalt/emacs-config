d8701e7f6dc632023af4632d56a812cd083dabb0
Tjaart van der Walt <github@tjaart.co.za>
2015-10-23 14:08:28 -0500

    init.d/init-region-bindings-mode.el

--- init.d/init-region-bindings-mode.el
+++ init.d/init-region-bindings-mode.el
@@ -3,4 +3,6 @@
             (region-bindings-mode-enable)
             (define-key region-bindings-mode-map "r" 'er/expand-region)
             (define-key region-bindings-mode-map "q" 'region-bindings-mode-off)
-            (define-key region-bindings-mode-map "w" 'mark-whole-buffer))))
+            (define-key region-bindings-mode-map "w" 'mark-whole-buffer)
+(add-hook 'activate-mark-hook '(message "activate mark"))
+            )))
