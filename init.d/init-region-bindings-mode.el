ec72b434a8a15f3bd7156f973ce5d3885712e915
Tjaart van der Walt <github@tjaart.co.za>
2015-10-23 12:06:42 -0500

    init.d/init-region-bindings-mode.el

--- init.d/init-region-bindings-mode.el
+++ init.d/init-region-bindings-mode.el
@@ -1,5 +1,5 @@
 (req-package region-bindings-mode
-  :init((progn
+  :config((progn
             (region-bindings-mode-enable)
             (define-key region-bindings-mode-map "r" 'er/expand-region)
             (define-key region-bindings-mode-map "w" 'mark-whole-buffer))))
