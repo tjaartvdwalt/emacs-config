381cbfd87e9dd5ac855a588adb9017fcb9e8a2ab
Tjaart van der Walt <github@tjaart.co.za>
2015-10-27 09:02:57 -0500

    init.d/init-js-doc.el

--- init.d/init-js-doc.el
+++ init.d/init-js-doc.el
@@ -1,17 +1,11 @@
 (req-package js-doc
   :config(
 (setq js-doc-mail-address "tjaart@tjaart.co.za"
-      js-doc-author (format "your name <%s>" js-doc-mail-address)
-      js-doc-url "url of your website"
-      js-doc-license "license name")
-;;
-;; (add-hook 'js-mode-hook
-;;           #'(lambda ()
-;;               (define-key js-mode-map "\C-ci" 'js-doc-insert-function-doc)
-;;               (define-key js2-mode-map "@" 'js-doc-insert-tag)))
-;;
-;; If you want to see the tag description, just input the next command
-;;   M-x js-doc-describe-tag
+      js-doc-author (format "Tjaart van der Walt <%s>" js-doc-mail-address)
+      js-doc-url "http://www.tjaart.co.za"
+      js-doc-license "MIT")
 
-;;; Code:
-          ))
+(add-hook 'js-mode-hook
+          #'(lambda ()
+              (define-key js-mode-map "\C-cdi" 'js-doc-insert-function-doc)
+              (define-key js-mode-map "@" 'js-doc-insert-tag)))))
