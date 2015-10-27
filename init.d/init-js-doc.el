7bb3e4a3649326a8824c3707a935e60f39ef3abd
Tjaart van der Walt <github@tjaart.co.za>
2015-10-27 09:15:17 -0500

    init.d/init-js-doc.el

--- init.d/init-js-doc.el
+++ init.d/init-js-doc.el
@@ -1,11 +1,11 @@
 (req-package js-doc
   :config(progn
-          (setq js-doc-mail-address "tjaart@tjaart.co.za"
-                js-doc-author (format "Tjaart van der Walt <%s>" js-doc-mail-address)
-                js-doc-url "http://www.tjaart.co.za"
-                js-doc-license "MIT")
+           (setq js-doc-mail-address "tjaart@tjaart.co.za"
+                 js-doc-author (format "Tjaart van der Walt <%s>" js-doc-mail-address)
+                 js-doc-url "http://www.tjaart.co.za"
+                 js-doc-license "MIT")
 
-          (define-key js-mode-map (kbd "C-cdi") 'js-doc-insert-function-doc)
-          (define-key js-mode-map (kbd "@") 'js-doc-insert-tag)))
+           (define-key js-mode-map (kbd "C-cdi") 'js-doc-insert-function-doc)
+           (define-key js-mode-map (kbd "@") 'js-doc-insert-tag)))
 
 ;; (define-key js-mode-map (kbd "M-.") nil)3
