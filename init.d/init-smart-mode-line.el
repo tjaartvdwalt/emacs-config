c124a30e3944b30af648026c318ac7a0011edb3d
Tjaart van der Walt <github@tjaart.co.za>
2015-11-16 12:30:31 -0600

    init.d/init-smart-mode-line.el

--- init.d/init-smart-mode-line.el
+++ init.d/init-smart-mode-line.el
@@ -1,8 +1,10 @@
 (req-package smart-mode-line
   :config (progn
 	    ;; (setq sml/no-confirm-load-theme t)
-            (sml/setup)
+            ;; (sml/setup)
             ;; (setq sml/name-width 8)
             (sml/apply-theme 'dark) 
             ;; (setq sml/mode-width "full")
-            ))
+            )
+:init (sml/setup)
+  )
