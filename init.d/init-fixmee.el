6c1d470202cff9d8c53b8dbdf6dc748b31412219
Tjaart van der Walt <github@tjaart.co.za>
2016-01-29 13:14:58 -0600

    init.d/init-fixmee.el

--- init.d/init-fixmee.el
+++ init.d/init-fixmee.el
@@ -1,7 +1,6 @@
-(req-package fixmee
-  :config (progn
-  (global-fixmee-mode t)
-  (setq fixmee-smartrep-prefix "C-cf")
-  (setq fixmee-goto-nextmost-urgent-keystrokes '("C-cff"))
-  (setq fixmee-goto-prevmost-urgent-keystrokes '("C-cfF"))
-  (setq fixmee-view-listing-keystrokes '("C-cfv"))))
+;; (req-package fixmee
+;;   :config (progn
+;;   (setq fixmee-smartrep-prefix "C-cf")
+;;   (setq fixmee-goto-nextmost-urgent-keystrokes '("C-cff"))
+;;   (setq fixmee-goto-prevmost-urgent-keystrokes '("C-cfF"))
+;;   (setq fixmee-view-listing-keystrokes '("C-cfv"))))
