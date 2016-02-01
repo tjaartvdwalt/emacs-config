abd4e48bd8a6580adeffc478aa6308747f34529f
Tjaart van der Walt <github@tjaart.co.za>
2016-02-01 11:53:24 -0600

    init.d/init-projectile.el

--- init.d/init-projectile.el
+++ init.d/init-projectile.el
@@ -5,6 +5,7 @@
     (projectile-global-mode +1)
     ;; (setq projectile-indexing-method 'native)
     (setq projectile-enable-caching t)
+     (add-to-list 'projectile-other-file-alist '("js" "hbs"))
      (add-to-list 'projectile-other-file-alist '("hbs" "js"))
     ;; (setq projectile-switch-project-action 'projectile-dired)
     ))
