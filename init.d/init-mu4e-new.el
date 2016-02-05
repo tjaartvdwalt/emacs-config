5188fb29e6a6b535e62b36ddc654a5cffe30f8de
Tjaart van der Walt <github@tjaart.co.za>
2016-02-05 17:39:29 -0600

    init.d/init-mu4e-new.el

--- init.d/init-mu4e-new.el
+++ init.d/init-mu4e-new.el
@@ -4,6 +4,17 @@
   (progn
     (add-to-list 'load-path "/usr/share/emacs/site-lisp/mu4e")
 
+    (add-to-list 'mu4e-headers-actions
+                 '("sMark as Spam" . mu4e-mark-for-spam) t)
+
+        ;; mark a message as spam ind header view
+    (defun mu4e-mark-for-spam (msg)
+      "Move the message to the spam folder."
+      (interactive)
+      (mu4e-mark-set 'move (my-mu4e-find-folder (my-mu4e-get-message-account msg) "my-mu4e-spam-folder")))
+
+    
+
     (setq mu4e-contexts
           `( ,(make-mu4e-context
                :name "atjaart@tjaart.co.za"
