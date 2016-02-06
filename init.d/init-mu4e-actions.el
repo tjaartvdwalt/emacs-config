7dac5a9751a085b3558a7b23931dacd8f93082a9
Tjaart van der Walt <github@tjaart.co.za>
2016-02-05 22:15:53 -0600

    init.d/init-mu4e-actions.el

---
+++ init.d/init-mu4e-actions.el
@@ -0,0 +1,14 @@
+(with-eval-after-load 'mu4e
+(defun mu4e-msgv-action-view-in-browser (msg)
+      "View the body of the message in a web browser."
+      (interactive)
+      (let ((html (mu4e-msg-field (mu4e-message-at-point t) :body-html))
+            (tmpfile (format "%s/%d.html" temporary-file-directory (random))))
+        (unless html (error "No html part for this message"))
+        (with-temp-file tmpfile
+          (insert
+           "<html>"
+           "<head><meta http-equiv=\"content-type\""
+           "content=\"text/html;charset=UTF-8\">"
+           html))
+        (browse-url (concat "file://" tmpfile))))
