1a715c03c07afb23385700a8e34144ca642b7daa
Tjaart van der Walt <github@tjaart.co.za>
2016-02-05 22:16:10 -0600

    init.d/init-mu4e.el

--- init.d/init-mu4e.el
+++ init.d/init-mu4e.el
@@ -122,19 +122,6 @@
     ;;Set a shortcut for mu4e
     (global-set-key "\C-ce" 'mu4e)
 
-    (defun mu4e-msgv-action-view-in-browser (msg)
-      "View the body of the message in a web browser."
-      (interactive)
-      (let ((html (mu4e-msg-field (mu4e-message-at-point t) :body-html))
-            (tmpfile (format "%s/%d.html" temporary-file-directory (random))))
-        (unless html (error "No html part for this message"))
-        (with-temp-file tmpfile
-          (insert
-           "<html>"
-           "<head><meta http-equiv=\"content-type\""
-           "content=\"text/html;charset=UTF-8\">"
-           html))
-        (browse-url (concat "file://" tmpfile))))
 
     (add-to-list 'mu4e-view-actions
                  '("bView in browser" . mu4e-msgv-action-view-in-browser) t)
@@ -311,7 +298,8 @@
 
 
 (with-eval-after-load 'mu4e
-(setq mu4e-contexts
+
+  (setq mu4e-contexts
 `( ,(make-mu4e-context
 :name "atjaart@tjaart.co.za"
 :enter-func (lambda () (mu4e-message "Switch to: tjaart@tjaart.co.za"))
