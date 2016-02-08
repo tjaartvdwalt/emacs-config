97b657edbbac06178f5c35564da54dea23839d9c
Tjaart van der Walt <github@tjaart.co.za>
2016-02-07 19:13:32 -0600

    init.d/init-mu4e-new.el

--- init.d/init-mu4e-new.el
+++ init.d/init-mu4e-new.el
@@ -29,7 +29,7 @@
 
     (setq   mu4e-maildir-shortcuts
     '(("/archive"     . ?a)
-      ("/inbox"       . ?i)
+      (mu4e-inbox-folder . ?i)
       ("/work"        . ?w)
       ("/sent"        . ?s)))
     
