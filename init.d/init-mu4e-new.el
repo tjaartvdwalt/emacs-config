b74ddc21082fdf21568b8ff45b35694cbf5981f8
Tjaart van der Walt <github@tjaart.co.za>
2016-02-07 21:34:06 -0600

    init.d/init-mu4e-new.el

--- init.d/init-mu4e-new.el
+++ init.d/init-mu4e-new.el
@@ -39,7 +39,7 @@
     (add-to-list 'mu4e-view-actions
                  '("iMove to Inbox" . mu4e-view-mark-for-inbox) t)
     (let*((draft "test")))
-    (setq   mu4e-maildir-shortcuts
+    (defvar   mu4e-maildir-shortcuts
             '((draft)     . ?a))
     ;;           ("/tjaart@tjaart.co.za/INBOX" . ?i)
     ;;           ("/work"        . ?w)
