83096bfb3eb3cf8f3f174e53eb58be38c60c6ab7
Tjaart van der Walt <github@tjaart.co.za>
2016-02-09 17:17:34 -0600

    init.d/init-mu4e-new.el

--- init.d/init-mu4e-new.el
+++ init.d/init-mu4e-new.el
@@ -54,6 +54,14 @@
     
     (setq mu4e-maildir-shortcuts '(((message mu4e-inbox-folder) . ?i)))
 
+(setq mu4e-bookmarks
+          `(
+            (,"flag:inbox"  "Messages in Inbox"    ?i)
+            (,"flag:unread"        "Unread messages"      ?u)
+            (,"flag:sent"          "Sent messages"        ?S)
+            (,"flag:trashed"       "Trashed messages"     ?T)
+            (,"flag:draft"         "Draft messages"       ?d)))
+
     (setq message-send-mail-function 'mu4e-refile-folder
           sendmail-program "/usr/bin/msmtp"
           user-full-name "Tjaart van der Walt")
