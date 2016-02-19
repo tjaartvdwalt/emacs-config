3eb51a4b28bc219c7a40b960c028439aafd36acf
Tjaart van der Walt <github@tjaart.co.za>
2016-02-19 08:34:21 -0600

    init.d/init-npm-shortcuts.el

--- init.d/init-npm-shortcuts.el
+++ init.d/init-npm-shortcuts.el
@@ -5,24 +5,3 @@
 (defun npm-publish-patch ()  (interactive)(shell-command "npm version patch && npm publish"))
 (defun npm-publish-minor ()  (interactive)(shell-command "npm version minor && npm publish"))
 (defun npm-publish-major ()  (interactive)(shell-command "npm version major && npm publish"))
-(defun standardfmt ()
-  "Format the current buffer using standard-format."
-  (interactive)
-  (let ((errbuf (get-buffer-create "*StandardFormat Errors*")))
-    (with-current-buffer errbuf
-      (erase-buffer))
-
-    (if (zerop (call-process "standard-format" nil errbuf nil "-w" (buffer-file-name)))
-        (progn
-          (revert-buffer t t)
-          (if errbuf (kill-error-buffer errbuf)))
-      (progn
-        (revert-buffer t t)
-        (display-buffer errbuf)))))
-
-
-(defun kill-error-buffer (errbuf)
-  (let ((win (get-buffer-window errbuf)))
-    (if win
-        (quit-window t win)
-      (kill-buffer errbuf))))
