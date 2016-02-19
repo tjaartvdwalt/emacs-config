11ca0d3f10322eb09a46726fe0e3c7486695c463
Tjaart van der Walt <github@tjaart.co.za>
2016-02-19 08:34:16 -0600

    init.d/init-npm-shortcuts.el

--- init.d/init-npm-shortcuts.el
+++ init.d/init-npm-shortcuts.el
@@ -1,11 +1,10 @@
-(global-set-key (kbd "C-c n") 'npm-version)
+(global-set-key (kbd "C-c n p p") 'npm-publish-patch)
+(global-set-key (kbd "C-c n p m") 'npm-publish-minor)
+(global-set-key (kbd "C-c n p M") 'npm-publish-major)
 
-(defun npm-command (command)
-  (interactive)
-  (progn
-  (shell-command command))
-
-(defun npm-version   (interactive)(shell-command "npm version"))
+(defun npm-publish-patch ()  (interactive)(shell-command "npm version patch && npm publish"))
+(defun npm-publish-minor ()  (interactive)(shell-command "npm version minor && npm publish"))
+(defun npm-publish-major ()  (interactive)(shell-command "npm version major && npm publish"))
 (defun standardfmt ()
   "Format the current buffer using standard-format."
   (interactive)
