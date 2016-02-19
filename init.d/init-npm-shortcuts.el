39658bfbb2a72c7b25501dad29e4d8c2779ce7a2
Tjaart van der Walt <github@tjaart.co.za>
2016-02-19 09:26:37 -0600

    init.d/init-npm-shortcuts.el

--- init.d/init-npm-shortcuts.el
+++ init.d/init-npm-shortcuts.el
@@ -14,7 +14,14 @@
 (defun npm-publish-minor ()  (interactive)(shell-command "npm version minor && npm publish &"))
 (defun npm-publish-major ()  (interactive)(shell-command "npm version major && npm publish &"))
 (defun npm-fixpack ()  (interactive)(shell-command "fixpack"))
-(defun npm-check ()  (interactive)(npm-shell-execute "npm-check"))
+(defun npm-check ()  (interactive)(shell-command "npm-check"))
 
 (setq system-uses-terminfo nil)
-(set-terminal-coding-system 'utf-8-unix)
+
+(defadvice display-message-or-buffer (before ansi-color activate)
+  "Process ANSI color codes in shell output."
+  (let ((buf (ad-get-arg 0)))
+    (and (bufferp buf)
+         (string= (buffer-name buf) "*Shell Command Output*")
+         (with-current-buffer buf
+           (ansi-color-apply-on-region (point-min) (point-max))))))
