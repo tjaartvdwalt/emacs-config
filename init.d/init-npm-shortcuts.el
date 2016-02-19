bdc2c7b2e37e80e60e01145a8bb601f85771e155
Tjaart van der Walt <github@tjaart.co.za>
2016-02-19 09:19:52 -0600

    init.d/init-npm-shortcuts.el

--- init.d/init-npm-shortcuts.el
+++ init.d/init-npm-shortcuts.el
@@ -2,8 +2,16 @@
 (global-set-key (kbd "C-c n p m") 'npm-publish-minor)
 (global-set-key (kbd "C-c n p M") 'npm-publish-major)
 (global-set-key (kbd "C-c n f") 'npm-fixpack)
+(global-set-key (kbd "C-c n c") 'npm-check)
+
+;; define this in order to get colorized output
+(defun npm-shell-execute(cmd)
+   (interactive "sShell command: ")
+   (shell (get-buffer-create "my-shell-buf"))
+   (process-send-string (get-buffer-process "my-shell-buf") (concat cmd "\n")))
 
 (defun npm-publish-patch ()  (interactive)(shell-command "npm version patch && npm publish &"))
-(defun npm-publish-minor ()  (interactive)(shell-command "npm version minor && npm publish"))
-(defun npm-publish-major ()  (interactive)(shell-command "npm version major && npm publish"))
+(defun npm-publish-minor ()  (interactive)(shell-command "npm version minor && npm publish &"))
+(defun npm-publish-major ()  (interactive)(shell-command "npm version major && npm publish &"))
 (defun npm-fixpack ()  (interactive)(shell-command "fixpack"))
+(defun npm-check ()  (interactive)(npm-shell-execute "npm-check"))
