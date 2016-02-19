24228e2103022378f8f794c0a5bab9ad478e57db
Tjaart van der Walt <github@tjaart.co.za>
2016-02-19 09:06:54 -0600

    init.d/init-npm-shortcuts.el

--- init.d/init-npm-shortcuts.el
+++ init.d/init-npm-shortcuts.el
@@ -1,7 +1,9 @@
 (global-set-key (kbd "C-c n p p") 'npm-publish-patch)
 (global-set-key (kbd "C-c n p m") 'npm-publish-minor)
 (global-set-key (kbd "C-c n p M") 'npm-publish-major)
+(global-set-key (kbd "C-c n p p") 'npm-fixpack)
 
-(defun npm-publish-patch ()  (interactive)(shell-command "npm version patch && npm publish"))
+(defun npm-publish-patch ()  (interactive)(shell-command "npm version patch && npm publish &"))
 (defun npm-publish-minor ()  (interactive)(shell-command "npm version minor && npm publish"))
 (defun npm-publish-major ()  (interactive)(shell-command "npm version major && npm publish"))
+(defun npm-fixpack ()  (interactive)(shell-command "fixpack"))
