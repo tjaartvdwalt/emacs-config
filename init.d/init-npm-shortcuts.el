67bf9ca338dfc1817277b6fe472e713076e42433
Tjaart van der Walt <github@tjaart.co.za>
2016-02-19 08:19:24 -0600

    init.d/init-npm-shortcuts.el

--- init.d/init-npm-shortcuts.el
+++ init.d/init-npm-shortcuts.el
@@ -1,1 +1,25 @@
-(global-set-key (kbd "C-c u i") (shell-command "npm version" ))
+(global-set-key (kbd "C-c n") (npm-version))
+(defun npm-version()
+  (interactive)
+  ((shell-command "npm version")))
+(defun standardfmt ()
+  "Format the current buffer using standard-format."
+  (interactive)
+  (let ((errbuf (get-buffer-create "*StandardFormat Errors*")))
+    (with-current-buffer errbuf
+      (erase-buffer))
+
+    (if (zerop (call-process "standard-format" nil errbuf nil "-w" (buffer-file-name)))
+        (progn
+          (revert-buffer t t)
+          (if errbuf (kill-error-buffer errbuf)))
+      (progn
+        (revert-buffer t t)
+        (display-buffer errbuf)))))
+
+
+(defun kill-error-buffer (errbuf)
+  (let ((win (get-buffer-window errbuf)))
+    (if win
+        (quit-window t win)
+      (kill-buffer errbuf))))
