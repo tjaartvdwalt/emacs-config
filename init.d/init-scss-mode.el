0aed48c234fa2855e49ad05e1033aacfaa27703c
Tjaart van der Walt <github@tjaart.co.za>
2016-02-19 20:30:39 -0600

    init.d/init-scss-mode.el

--- init.d/init-scss-mode.el
+++ init.d/init-scss-mode.el
@@ -1,9 +1,10 @@
 (req-package scss-mode
   :config(progn
            (setq css-indent-offset 2)
-(add-hook 'scss-mode-hook
+           ;; (flycheck-add-mode 'scss-lint 'scss-mode)
+
+           (add-hook 'scss-mode-hook
               (add-hook 'after-save-hook 'standardfmt t t))
 
-           ;; (flycheck-add-mode 'scss-lint 'scss-mode)
            ))
 
