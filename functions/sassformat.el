ced32a7744b21a207df4d0646d2dbf1bd9f3571f
Tjaart van der Walt <github@tjaart.co.za>
2016-02-19 19:44:59 -0600

    functions/sassformat.el

---
+++ functions/sassformat.el
@@ -0,0 +1,21 @@
+(defun sassfmt ()
+  "Format the current buffer using sass-convert."
+  (interactive)
+  (let ((errbuf (get-buffer-create "*Sass Convert Errors*")))
+    (with-current-buffer errbuf
+      (erase-buffer))
+
+    (if (zerop (call-process "sass-convert" nil errbuf nil "-i" (buffer-file-name)))
+        (progn
+          (revert-buffer t t)
+          (if errbuf (kill-error-buffer errbuf)))
+      (progn
+        (revert-buffer t t)
+        (display-buffer errbuf)))))
+
+
+(defun kill-error-buffer (errbuf)
+  (let ((win (get-buffer-window errbuf)))
+    (if win
+        (quit-window t win)
+      (kill-buffer errbuf))))
