cb618fbc753a51e857534eae720d1c07ce5063bb
Tjaart van der Walt <github@tjaart.co.za>
2016-04-13 12:22:31 -0500

    init.d/init-npm.el

--- init.d/init-npm.el
+++ init.d/init-npm.el
@@ -1,2 +1,10 @@
 (add-to-list 'load-path "~/.emacs.d/manual/npm.el")
 (require 'npm)
+
+(global-set-key (kbd "M-n i") 'npm-install)
+(global-set-key (kbd "M-n n") 'npm-new)
+(global-set-key (kbd "M-n d") 'npm-new-dependency)
+(global-set-key (kbd "M-n e") 'npm-nodemon-exec)
+(global-set-key (kbd "M-n p") 'npm-publish)
+(global-set-key (kbd "M-n t") 'npm-test)
+(global-set-key (kbd "M-n v") 'npm-version)
