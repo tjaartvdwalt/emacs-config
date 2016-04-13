605d85b361838a37872a95c9d5ccb3f7c67c3470
Tjaart van der Walt <github@tjaart.co.za>
2016-04-13 15:39:38 -0500

    init.d/init-js2-refactor.el

--- init.d/init-js2-refactor.el
+++ init.d/init-js2-refactor.el
@@ -1,9 +1,9 @@
 (req-package js2-refactor
   :require (js-mode)
-  :config ((progn
+  :config (
             ;; (add-hook 'js-mode-hook #'js2-refactor-mode)
                 (add-hook 'js-mode-hook
               (lambda ()
-                (add-hook 'js2-refactor-mode)))
+                (add-hook 'js2-refactor-mode))
 
 (js2r-add-keybindings-with-prefix "C-c j"))))
