5d88810b0a9ad31056416f5c8255ee020a6ed568
Tjaart van der Walt <github@tjaart.co.za>
2016-04-13 14:47:11 -0500

    init.d/init-flycheck.el

--- init.d/init-flycheck.el
+++ init.d/init-flycheck.el
@@ -8,7 +8,8 @@
            (setq-default flycheck-disabled-checkers
                          (append flycheck-disabled-checkers
                                  '(javascript-jshint)))
-           (flycheck-add-mode 'javascript-standard 'js-mode)))
+           (flycheck-add-mode 'javascript-standard 'js-mode)
+           (flycheck-add-mode 'javascript-standard 'js2-mode)))
 
 
 
