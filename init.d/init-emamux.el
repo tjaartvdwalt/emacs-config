aa14739c8c33681ad60e7d07af17d5a3a11c36c4
Tjaart van der Walt <github@tjaart.co.za>
2016-04-13 13:58:34 -0500

    init.d/init-emamux.el

--- init.d/init-emamux.el
+++ init.d/init-emamux.el
@@ -1,4 +1,4 @@
 (req-package emamux
   :config(
-          (setq emamux:completing-read-type 'helm))
-  (setq emamux:use-nearest-pane t))
+          (setq emamux:completing-read-type 'helm)
+          (setq emamux:use-nearest-pane t)))
