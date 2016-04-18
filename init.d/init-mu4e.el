87570fb87ec20d9805a2e4ba4bdddaed57b6f23a
Tjaart van der Walt <github@tjaart.co.za>
2016-04-18 12:40:56 -0500

    init.d/init-mu4e.el

--- init.d/init-mu4e.el
+++ init.d/init-mu4e.el
@@ -41,7 +41,7 @@
     (add-to-list 'mu4e-headers-actions
                  '("lMark as Spam" . mu4e-mark-for-spam) t)
     (add-to-list 'mu4e-view-actions
-                 '("lMark as Spam" . mu4e-view-mark-for-spam) t)
+                 '("lMark as Spam" . mu4e-mark-for-spam) t)
     (add-to-list 'mu4e-headers-actions
                  '("iMove to Inbox" . mu4e-mark-for-inbox) t)
     (add-to-list 'mu4e-view-actions
