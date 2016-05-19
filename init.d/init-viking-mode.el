b1f491e86f22be27fa660c164616e5750e6c3457
Tjaart van der Walt <github@tjaart.co.za>
2016-05-19 11:20:01 -0500

    init.d/init-viking-mode.el

--- init.d/init-viking-mode.el
+++ init.d/init-viking-mode.el
@@ -2,4 +2,4 @@
   :config(progn
            (viking-global-mode)
                         (global-unset-key (kbd "C-k"))
- (global-set-key (kbd "C- ")   'viking-kill-thing-at-point)))
+ (global-set-key (kbd "C-k")   'viking-kill-thing-at-point)))
