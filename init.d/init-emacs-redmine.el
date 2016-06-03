3710a4086bd030e8b5aeb14297439d535eed8b10
Tjaart van der Walt <github@tjaart.co.za>
2016-06-02 23:51:51 -0500

    init.d/init-emacs-redmine.el

--- init.d/init-emacs-redmine.el
+++ init.d/init-emacs-redmine.el
@@ -7,5 +7,4 @@
   (setq redmine-project-name "solmates")
   (setq redmine-login-key "f450688fcd92fd4ae47099e11008e6ce8bdff0ad")
   (setq redmine-url "http://jenkins.getsolmates.com:3000")
-  ;; (redmine-show-sprints)
-  )
+  (redmine-show-sprints))
