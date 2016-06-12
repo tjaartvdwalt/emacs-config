2c96653898e06f767722741f73097ca6c60d4b73
Tjaart van der Walt <github@tjaart.co.za>
2016-06-12 14:53:47 -0500

    home/tjaart/.emacs.d/init.el

--- init.el
+++ init.el
@@ -73,7 +73,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "monospace" :slant normal :weight normal :height 190 :width normal)))))
+ '(default ((t (:family "monospace" :slant normal :weight lighter :height 190 :width normal)))))
 
 
 (put 'upcase-region 'disabled nil)
