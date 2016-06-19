03a728e6d64837767eb564ef44b8d79941a3c9cf
Tjaart van der Walt <github@tjaart.co.za>
2016-06-18 23:09:05 -0500

    init.d/mu4e/init-mu4e.el

--- init.d/mu4e/init-mu4e.el
+++ init.d/mu4e/init-mu4e.el
@@ -54,12 +54,12 @@
     (add-to-list 'mu4e-view-actions
                  '("bViewInBrowser" . mu4e-action-view-in-browser) t)
     
-    (add-to-list 'mu4e-view-actions
-                 '("lMark as Spam" . mu4e-mark-for-spam) t)
+    ;; (add-to-list 'mu4e-view-actions
+    ;;              '("lMark as Spam" . mu4e-mark-for-spam) t)
     (add-to-list 'mu4e-headers-actions
                  '("iMove to Inbox" . mu4e-mark-for-inbox) t)
-    (add-to-list 'mu4e-view-actions
-                 '("iMove to Inbox" . mu4e-mark-for-inbox) t)
+    ;; (add-to-list 'mu4e-view-actions
+    ;;              '("iMove to Inbox" . mu4e-mark-for-inbox) t)
 
     (setq mu4e-bookmarks
           `(
