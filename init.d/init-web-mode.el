918304fb508d2fc8d961fc4a48afccea8d4344ac
Tjaart van der Walt <github@tjaart.co.za>
2016-06-18 23:01:17 -0500

    init.d/init-web-mode.el

--- init.d/init-web-mode.el
+++ init.d/init-web-mode.el
@@ -52,9 +52,9 @@
             ("html" . (ac-source-emmet-html-aliases ac-source-emmet-html-snippets))
             ("css" . (ac-source-css-property))))
 
-    (add-hook 'web-mode-hook
-              '(lambda ()
-                (add-hook 'after-save-hook (message "after save hook") t t)))
+    ;; (add-hook 'web-mode-hook
+    ;;           '(lambda ()
+    ;;             (add-hook 'after-save-hook (message "after save hook") t t)))
     ;; (add-hook 'web-mode-hook
     ;;           '(lambda ()
     ;;              (yas-activate-extra-mode 'html-mode)))
