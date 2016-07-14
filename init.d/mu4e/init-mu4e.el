66a6e8da409f5281cbd2b499ceb0f8a7054fdb61
Tjaart van der Walt <github@tjaart.co.za>
2016-07-14 09:04:37 -0500

    init.d/mu4e/init-mu4e.el

--- init.d/mu4e/init-mu4e.el
+++ init.d/mu4e/init-mu4e.el
@@ -54,11 +54,11 @@
       (mu4e-mark-for-spam msg)
       (mu4e-headers-next))
 
-    (add-to-list 'mu4e-headers-actions
-                 '("lMark as Spam" . mu4e-spam-and-next) t)
     (add-to-list 'mu4e-view-actions
                  '("bViewInBrowser" . mu4e-action-view-in-browser) t)
 
+    (add-to-list 'mu4e-headers-actions
+                 '("lMark as Spam" . mu4e-spam-and-next) t)
     (add-to-list 'mu4e-view-actions
                  '("lMark as Spam" . mu4e-mark-for-spam) t)
 
