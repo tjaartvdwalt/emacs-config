525d3ac422cd0b78eb8af7f7a2d272c002a277fd
Tjaart van der Walt <git@tjaart.co.za>
2016-09-28 08:35:10 -0500

    init.d/mu4e/init-mu4e.el

--- init.d/mu4e/init-mu4e.el
+++ init.d/mu4e/init-mu4e.el
@@ -52,7 +52,6 @@
 
     (defun mu4e-view-spam (msg)
       (interactive)
-      (mu4e-mark-at-point 'move my-mu4e-spam-folder)
       (mu4e-view-mark-custom)
       (mu4e-view-headers-next)
       )
