34f790c64ff2e143ad405291a43db095ff1b0749
Tjaart van der Walt <git@tjaart.co.za>
2016-10-06 17:47:48 -0500

    functions/standardformat.el

--- functions/standardformat.el
+++ functions/standardformat.el
@@ -1,21 +1,21 @@
-(defun standardfmt ()
-  "Format the current buffer using standard-format."
-  (interactive)
-  (let ((errbuf (get-buffer-create "*StandardFormat Errors*")))
-    (with-current-buffer errbuf
-      (erase-buffer))
+;; (defun standardfmt ()
+;;   "Format the current buffer using standard-format."
+;;   (interactive)
+;;   (let ((errbuf (get-buffer-create "*StandardFormat Errors*")))
+;;     (with-current-buffer errbuf
+;;       (erase-buffer))
 
-    (if (zerop (call-process "standard-format" nil errbuf nil "-w" (buffer-file-name)))
-        (progn
-          (revert-buffer t t)
-          (if errbuf (kill-error-buffer errbuf)))
-      (progn
-        (revert-buffer t t)
-        (display-buffer errbuf)))))
+;;     (if (zerop (call-process "standard-format" nil errbuf nil "-w" (buffer-file-name)))
+;;         (progn
+;;           (revert-buffer t t)
+;;           (if errbuf (kill-error-buffer errbuf)))
+;;       (progn
+;;         (revert-buffer t t)
+;;         (display-buffer errbuf)))))
 
 
-(defun kill-error-buffer (errbuf)
-  (let ((win (get-buffer-window errbuf)))
-    (if win
-        (quit-window t win)
-      (kill-buffer errbuf))))
+;; (defun kill-error-buffer (errbuf)
+;;   (let ((win (get-buffer-window errbuf)))
+;;     (if win
+;;         (quit-window t win)
+;;       (kill-buffer errbuf))))
