5d2ff72d2093a861951dcb3563bcb502f19b0b6e
Tjaart van der Walt <git@tjaart.co.za>
2016-10-06 17:46:30 -0500

    init.d/init-standard-formatter.el

--- init.d/init-standard-formatter.el
+++ init.d/init-standard-formatter.el
@@ -1,12 +1,26 @@
 (add-hook 'js-mode-hook
           (lambda ()
             (unless (derived-mode-p 'json-mode)
-              ;; (local-unset-key (kbd "\C-c u i"))
               (add-hook 'after-save-hook 'standardfmt t t))))
-              ;; (global-set-key (kbd "C-c u i") 'standardfmt))))
+
+(defun standardfmt ()
+  "Format the current buffer using standard-format."
+  (interactive)
+  (let ((errbuf (get-buffer-create "*StandardFormat Errors*")))
+    (with-current-buffer errbuf
+      (erase-buffer))
+
+    (if (zerop (call-process "standard-format" nil errbuf nil "-w" (buffer-file-name)))
+        (progn
+          (revert-buffer t t)
+          (if errbuf (kill-error-buffer errbuf)))
+      (progn
+        (revert-buffer t t)
+        (display-buffer errbuf)))))
 
 
-;; (add-hook 'js-mode-hook
-;;           (lambda ()
-;;             (unless (derived-mode-p 'json-mode)
-;;               (add-hook 'before-save-hook 'standardfmt t t))))
+(defun kill-error-buffer (errbuf)
+  (let ((win (get-buffer-window errbuf)))
+    (if win
+        (quit-window t win)
+      (kill-buffer errbuf))))
