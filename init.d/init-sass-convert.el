8bed4be0b2c29104c66611166ae8e5f35c4c3325
Tjaart van der Walt <git@tjaart.co.za>
2016-10-06 17:54:35 -0500

    init.d/init-sass-convert.el

--- init.d/init-sass-convert.el
+++ init.d/init-sass-convert.el
@@ -5,7 +5,6 @@
 (defun scssfmt ()
   "Format the current buffer using sass-convert."
   (interactive)
-  (message "fomat scss")
   (let ((errbuf (get-buffer-create "*Sass Convert Errors*")))
     (with-current-buffer errbuf
       (erase-buffer))
