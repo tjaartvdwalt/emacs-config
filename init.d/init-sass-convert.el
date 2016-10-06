8ef3b110fa24b2692c7dd6809629733c6f5785d4
Tjaart van der Walt <git@tjaart.co.za>
2016-10-06 17:50:17 -0500

    init.d/init-sass-convert.el

--- init.d/init-sass-convert.el
+++ init.d/init-sass-convert.el
@@ -1,2 +1,29 @@
 ;;(add-hook 'js				;
-          
+
+(add-hook 'scss-mode-hook
+          (lambda ()
+              (add-hook 'after-save-hook 'scssfmt t t)))
+
+
+
+(defun scssfmt ()
+  "Format the current buffer using sass-convert."
+  (interactive)
+  (let ((errbuf (get-buffer-create "*Sass Convert Errors*")))
+    (with-current-buffer errbuf
+      (erase-buffer))
+
+    (if (zerop (call-process "sass-convert" nil errbuf nil "-i" (buffer-file-name)))
+        (progn
+          (revert-buffer t t)
+          (if errbuf (kill-error-buffer errbuf)))
+      (progn
+        (revert-buffer t t)
+        (display-buffer errbuf)))))
+
+
+(defun kill-error-buffer (errbuf)
+  (let ((win (get-buffer-window errbuf)))
+    (if win
+        (quit-window t win)
+      (kill-buffer errbuf))))
