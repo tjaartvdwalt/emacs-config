3b24e7ac92cae5cc9e930210ca7c6f390f6f9a1a
Tjaart van der Walt <git@tjaart.co.za>
2016-11-24 20:32:16 -0600

    init.d/init-web-mode.el

--- init.d/init-web-mode.el
+++ init.d/init-web-mode.el
@@ -15,6 +15,7 @@
     (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
     (add-to-list 'auto-mode-alist '("\\.hbs\\'" . web-mode))
     (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+    (add-to-list 'auto-mode-alist '("\\.jsx\\'" . web-mode))
     (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
     (add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
     ;; (add-to-list 'auto-mode-alist '("\\.sass\\'" . web-mode))
