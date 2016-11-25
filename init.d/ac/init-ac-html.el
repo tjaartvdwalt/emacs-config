09fc13d35332d7ac1a8c6ec42e70ca5167d53581
Tjaart van der Walt <git@tjaart.co.za>
2016-11-24 20:00:58 -0600

    init.d/ac/init-ac-html.el

--- init.d/ac/init-ac-html.el
+++ init.d/ac/init-ac-html.el
@@ -1,9 +1,6 @@
 (req-package ac-html
   :config(
-(add-to-list 'web-mode-ac-sources-alist
-             '("html" . (ac-source-html-tag
-                         ac-source-html-attr
-                         ac-source-html-attrv)))
-          )
-  
-  )
+          (add-to-list 'web-mode-ac-sources-alist
+                       '("html" . (ac-source-html-tag
+                                   ac-source-html-attr
+                                   ac-source-html-attrv)))))
