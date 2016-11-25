af5609418ad54f02a319cecaa6efd9a8fe66baf7
Tjaart van der Walt <git@tjaart.co.za>
2016-11-24 20:10:51 -0600

    init.d/init-jsx-mode.el

--- init.d/init-jsx-mode.el
+++ init.d/init-jsx-mode.el
@@ -1,5 +1,4 @@
 (req-package jsx-mode
   :config(
           (progn
-            (add-to-list 'auto-mode-alist '("\\.jsx\\'" . jsx-mode))
-            (autoload 'jsx-mode "jsx-mode" "JSX mode" t))))
+            (add-to-list 'auto-mode-alist '("\\.jsx\\'" . jsx-mode)))))
