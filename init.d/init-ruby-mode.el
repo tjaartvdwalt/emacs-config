8a845ce1bfd64835ee880b770f4792050acf1020
Tjaart van der Walt <git@tjaart.co.za>
2016-12-10 16:19:35 -0600

    init.d/init-ruby-mode.el

--- init.d/init-ruby-mode.el
+++ init.d/init-ruby-mode.el
@@ -1,5 +1,5 @@
 (req-package enh-ruby-mode
-  :require (ruby-end rspec-mode inf-ruby robe autocomplete)
+  :require (ruby-end rspec-mode inf-ruby robe auto-complete)
   :config(progn
            (add-hook 'enh-ruby-mode-hook 'robe-mode)
            (add-to-list 'ac-modes 'enh-ruby-mode)
