8f1dbbc4ce69d5150ad939e6bab593043c421bd4
Tjaart van der Walt <git@tjaart.co.za>
2016-12-10 15:57:44 -0600

    init.d/init-enh-ruby-mode.el

--- init.d/init-enh-ruby-mode.el
+++ init.d/init-enh-ruby-mode.el
@@ -1,3 +1,4 @@
 (req-package enh-ruby-mode
-  :config((add-to-list 'auto-mode-alist
-                         '("\\(?:\\.rb\\|ru\\|rake\\|thor\\|jbuilder\\|gemspec\\|podspec\\|/\\(?:Gem\\|Rake\\|Cap\\|Thor\\|Vagrant\\|Guard\\|Pod\\)file\\)\\'" . enh-ruby-mode))))
+  :config(progn
+           (add-to-list 'auto-mode-alist
+                        '("\\(?:\\.rb\\|ru\\|rake\\|thor\\|jbuilder\\|gemspec\\|podspec\\|/\\(?:Gem\\|Rake\\|Cap\\|Thor\\|Vagrant\\|Guard\\|Pod\\)file\\)\\'" . enh-ruby-mode))))
