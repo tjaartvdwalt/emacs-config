31f30afb66b6dc99cd1138aa86fd392046d8854f
Tjaart van der Walt <git@tjaart.co.za>
2016-12-10 16:35:16 -0600

    init.d/init-smartparens.el

--- init.d/init-smartparens.el
+++ init.d/init-smartparens.el
@@ -3,7 +3,7 @@
   :config(progn
  (smartparens-global-mode)
  (show-smartparens-global-mode t)
- (sp-with-modes '(rhtml-mode)
-   (sp-local-pair "<" ">")
- (sp-local-pair "")
- )))
+ ;; (sp-with-modes '(rhtml-mode)
+ ;;   (sp-local-pair "<" ">")
+ ;; (sp-local-pair "")
+ ))
