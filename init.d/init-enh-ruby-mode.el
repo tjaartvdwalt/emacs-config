4d117f36c3dcc0f7ac228e946f84de4bebc7ed01
Tjaart van der Walt <git@tjaart.co.za>
2016-12-10 15:59:58 -0600

    init.d/init-enh-ruby-mode.el

--- init.d/init-enh-ruby-mode.el
+++ init.d/init-enh-ruby-mode.el
@@ -1,4 +1,6 @@
 (req-package enh-ruby-mode
+  :require (ruby-end rspec-mode inf-ruby robe)
   :config(progn
+           (add-hook 'enh-ruby-mode-hook 'robe-mode)
            (add-to-list 'auto-mode-alist
                         '("\\(?:\\.rb\\|ru\\|rake\\|thor\\|jbuilder\\|gemspec\\|podspec\\|/\\(?:Gem\\|Rake\\|Cap\\|Thor\\|Vagrant\\|Guard\\|Pod\\)file\\)\\'" . enh-ruby-mode))))
