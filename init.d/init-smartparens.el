35e634bbce415dc4d0a6940db479a07d567cf9d2
Tjaart van der Walt <git@tjaart.co.za>
2016-12-10 16:30:06 -0600

    init.d/init-smartparens.el

---
+++ init.d/init-smartparens.el
@@ -0,0 +1,8 @@
+(req-package smartparens
+  :require (smartparens-config smartparens-ruby)
+  :config(progn
+ (smartparens-global-mode)
+ (show-smartparens-global-mode t)
+ (sp-with-modes '(rhtml-mode)
+   (sp-local-pair "<" ">")
+   (sp-local-pair ""))))
