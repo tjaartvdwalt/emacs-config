2f77009fd7eb489d58d0b69a231c0ceece9cc5fc
Tjaart van der Walt <git@tjaart.co.za>
2016-12-20 16:15:56 -0600

    init.d/init-flycheck.el

--- init.d/init-flycheck.el
+++ init.d/init-flycheck.el
@@ -2,15 +2,12 @@
   :require (flycheck-flow)
   :diminish flycheck-mode
   :config(progn
-           
+
            (add-hook 'after-init-hook 'global-flycheck-mode)
-(add-hook 'javascript-mode-hook 'flycheck-mode)
+           (add-hook 'javascript-mode-hook 'flycheck-mode)
 
            ;; set javascript standard checekr as default
            (setq-default flycheck-disabled-checkers
                          (append flycheck-disabled-checkers
                                  '(javascript-jshint)))
-  ))
-
-
-
+           ))
