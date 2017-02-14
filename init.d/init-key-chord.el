44a9e9441f150b45dcd11e05c87467ce4626c4ca
Tjaart van der Walt <git@tjaart.co.za>
2017-02-14 11:15:20 -0600

    init.d/init-key-chord.el

--- init.d/init-key-chord.el
+++ init.d/init-key-chord.el
@@ -1,10 +1,10 @@
 (req-package key-chord
- :config ( progn
-           (key-chord-mode 1)
-           ;; Max time delay between two key presses to be considered a key chord
-    (setq key-chord-two-keys-delay 0.2)
-    
-    ;; Max time delay between two presses of the same key to be considered a key chord.
-    ;; Should normally be a little longer than `key-chord-two-keys-delay'.
-    (setq key-chord-one-key-delay 0.4)
-(key-chord-define-global "ll" 'er/expand-region)))
+  :config ( progn
+            (key-chord-mode 1)
+            ;; Max time delay between two key presses to be considered a key chord
+            (setq key-chord-two-keys-delay 0.2)
+
+            ;; Max time delay between two presses of the same key to be considered a key chord.
+            ;; Should normally be a little longer than `key-chord-two-keys-delay'.
+            (setq key-chord-one-key-delay 0.4)
+            (key-chord-define-global "rr" 'er/expand-region)))
