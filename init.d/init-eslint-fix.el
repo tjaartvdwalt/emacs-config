698f8a49ba6dffd50ed60dfdfcc2e1f6a9cbcfe6
Tjaart van der Walt <git@tjaart.co.za>
2017-02-14 14:59:35 -0600

    init.d/init-eslint-fix.el

--- init.d/init-eslint-fix.el
+++ init.d/init-eslint-fix.el
@@ -1,4 +1,3 @@
 (req-package eslint-fix
-  :config(
-          (eval-after-load 'js-mode
-            '(add-hook 'js-mode-hook (lambda () (add-hook 'after-save-hook 'eslint-fix nil t))))))
+  :config(progn
+             (add-hook 'js-mode-hook (lambda () (add-hook 'after-save-hook 'eslint-fix nil t)))))
