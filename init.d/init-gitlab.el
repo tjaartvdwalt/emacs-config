21f5ca24089724a505c428fa8d0c453c4641035e
Tjaart van der Walt <git@tjaart.co.za>
2017-02-17 12:31:32 -0600

    init.d/init-gitlab.el

--- init.d/init-gitlab.el
+++ init.d/init-gitlab.el
@@ -2,5 +2,5 @@
   :require (helm-gitlab)
   :config(progn
            (setq gitlab-host "https://gitlab.com"
-                 gitlab-username "tjaart"
+                 gitlab-username "tjaart@tjaart.co.za"
                  gitlab-password "aazU4zgerVdWkYEta6H8")))
