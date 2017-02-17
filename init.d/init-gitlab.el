42791bdbcdb6540e362195393879b5699ae05920
Tjaart van der Walt <git@tjaart.co.za>
2017-02-17 12:41:40 -0600

    init.d/init-gitlab.el

--- init.d/init-gitlab.el
+++ init.d/init-gitlab.el
@@ -2,5 +2,4 @@
   :require (helm-gitlab)
   :config(progn
            (setq gitlab-host "https://gitlab.com"
-                 gitlab-username "gitlab@tjaart.co.za"
                  gitlab-token-id "W4zVaUd3DVoPyfDRbAkn")))
