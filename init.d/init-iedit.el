f8d3d252e1aaf456c7ac861da36d0be9cc1ef861
Tjaart van der Walt <git@tjaart.co.za>
2017-02-23 15:33:23 -0600

    init.d/init-iedit.el

--- init.d/init-iedit.el
+++ init.d/init-iedit.el
@@ -2,4 +2,4 @@
   :config(progn
            (global-unset-key (kbd "C-;"))
            (global-set-key (kbd "C-,") 'iedit-mode)
-                      (define-key iedit-lib-keymap (kbd ",") 'iedit-toggle-unmatched-lines-visible)))
+           (define-key iedit-lib-keymap (kbd ",") 'iedit-toggle-unmatched-lines-visible)))
