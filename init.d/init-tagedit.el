0c6635358cbf48b5f4412692faafd288a0b16de6
Tjaart van der Walt <git@tjaart.co.za>
2017-02-25 16:33:12 -0600

    init.d/init-tagedit.el

--- init.d/init-tagedit.el
+++ init.d/init-tagedit.el
@@ -1,4 +1,5 @@
 (req-package tagedit
   :config(progn
-     (tagedit-add-paredit-like-keybindings)
+           (tagedit-add-paredit-like-keybindings)
+           (tagedit-add-experimental-features)
      (add-hook 'html-mode-hook (lambda () (tagedit-mode 1)))))
