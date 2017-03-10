0bc9bf0ba0fdd22f560f37a3a1cf00bc5c509373
Tjaart van der Walt <git@tjaart.co.za>
2017-03-10 10:39:10 -0600

    init.d/init-tdd.el

--- init.d/init-tdd.el
+++ init.d/init-tdd.el
@@ -5,8 +5,8 @@
 
 (defun runjest ()
   "Run jest with tdd"
-  (let (errbuf (get-buffer-create "*TDD Errors*"))
+  (let ((errbuf (get-buffer-create "*TDD Errors*")))
     (tdd-waiting)
-    (let (res (call-process "jest" nil errbuf nil nil nil)))
+    (let ((res (call-process "jest" nil errbuf nil nil nil))))
     (message res)
     ))
