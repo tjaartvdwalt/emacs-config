9ea1e608ba288e0690d5f52a11c6927c28bd38c6
Tjaart van der Walt <git@tjaart.co.za>
2017-03-10 15:10:50 -0600

    init.d/init-back-button.el

--- init.d/init-back-button.el
+++ init.d/init-back-button.el
@@ -1,12 +1,12 @@
-(use-package back-button
-  :diminish back-button-mode
-  :require smartrep
-  ;; :bind (("C-c bh" . back-button-local-backward)
-  ;;        ("C-c bn" . back-button-local-forward))
-  :config (progn (back-button-mode 1)
-                 (smartrep-define-key
-                     global-map "C-c b"
-                   '(("h" . 'back-button-local-backward)
-                     ("n" . 'back-button-local-forward)
-                     ("C-h" . 'back-button-global-backward)
-                     ("C-n" . 'back-button-global-forward)))))
+;; (use-package back-button
+;;   :diminish back-button-mode
+;;   :require smartrep
+;;   ;; :bind (("C-c bh" . back-button-local-backward)
+;;   ;;        ("C-c bn" . back-button-local-forward))
+;;   :config (progn (back-button-mode 1)
+;;                  (smartrep-define-key
+;;                      global-map "C-c b"
+;;                    '(("h" . 'back-button-local-backward)
+;;                      ("n" . 'back-button-local-forward)
+;;                      ("C-h" . 'back-button-global-backward)
+;;                      ("C-n" . 'back-button-global-forward)))))
