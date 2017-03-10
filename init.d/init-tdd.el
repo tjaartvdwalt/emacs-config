ebf15e9afd00540dbea73c287e577e0f44d2f90a
Tjaart van der Walt <git@tjaart.co.za>
2017-03-10 16:28:55 -0600

    init.d/init-tdd.el

--- init.d/init-tdd.el
+++ init.d/init-tdd.el
@@ -5,8 +5,23 @@
     (add-hook 'js-mode-hook
               (lambda ()
                 (set (make-local-variable 'compile-command)
-                     ("jest")))
+                     (format "jest")))
               )))
+
+
+(set (make-local-variable 'compile-command)
+                    ;; emulate make's .c.o implicit pattern rule, but with
+                    ;; different defaults for the CC, CPPFLAGS, and CFLAGS
+                    ;; variables:
+                    ;; $(CC) -c -o $@ $(CPPFLAGS) $(CFLAGS) $<
+		    (let ((file (file-name-nondirectory buffer-file-name)))
+                      (format "%s -c -o %s.o %s %s %s"
+                              (or (getenv "CC") "gcc")
+                              (file-name-sans-extension file)
+                              (or (getenv "CPPFLAGS") "-DDEBUG=9")
+                              (or (getenv "CFLAGS") "-ansi -pedantic -Wall -g")
+			      file))))))
+
 
 ;; (defun runjest ()
 ;;   "Run jest with tdd"
