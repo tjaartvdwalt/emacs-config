eebba71ddac43b785b0d61a75e3c89c412085c3f
Tjaart van der Walt <git@tjaart.co.za>
2017-03-10 16:47:40 -0600

    init.d/init-ansi-color.el

---
+++ init.d/init-ansi-color.el
@@ -0,0 +1,5 @@
+(require 'ansi-color)
+(defun colorize-compilation-buffer ()
+  (let ((inhibit-read-only t))
+    (ansi-color-apply-on-region (point-min) (point-max))))
+(add-hook 'compilation-filter-hook 'colorize-compilation-buffer)
