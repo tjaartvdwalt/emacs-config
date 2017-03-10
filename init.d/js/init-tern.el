7083068b17b5a41afff1ca249d0fff27c16f499c
Tjaart van der Walt <git@tjaart.co.za>
2017-03-10 14:48:27 -0600

    init.d/js/init-tern.el

--- init.d/js/init-tern.el
+++ init.d/js/init-tern.el
@@ -1,7 +1,7 @@
 ;; tern must be installed seperately
 ;; on arch linux `yaourt -S nodejs-tern`
 
-(req-package tern
+(use-package tern
   :diminish tern-mode
   :config (progn
             (add-hook 'js-mode-hook (lambda () (tern-mode t)))))
