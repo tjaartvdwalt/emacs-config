18173037bac88119f13df9feeabc3be3d2ee1974
Tjaart van der Walt <git@tjaart.co.za>
2017-03-10 14:48:50 -0600

    init.d/helm/init-helm-open-github.el

--- init.d/helm/init-helm-open-github.el
+++ init.d/helm/init-helm-open-github.el
@@ -1,4 +1,4 @@
-(req-package helm-open-github
+(use-package helm-open-github
       :bind (("C-c h h f" . helm-open-github-from-file)
              ("C-c h h c" . helm-open-github-from-commit)
              ("C-c h h i" . helm-open-github-from-issues)
