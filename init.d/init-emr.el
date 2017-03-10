5340bc1558aebfca7a492da7be131f02b23673a9
Tjaart van der Walt <git@tjaart.co.za>
2017-03-10 14:49:06 -0600

    init.d/init-emr.el

--- init.d/init-emr.el
+++ init.d/init-emr.el
@@ -1,4 +1,4 @@
-(req-package emr
+(use-package emr
   :config(progn
            (define-key prog-mode-map (kbd "M-RET") 'emr-show-refactor-menu)
            (add-hook 'prog-mode-hook 'emr-initialize)))
