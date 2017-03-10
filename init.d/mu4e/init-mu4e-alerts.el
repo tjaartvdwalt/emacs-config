a7f183f587d792b97f529c2248d768e87af56bb5
Tjaart van der Walt <git@tjaart.co.za>
2017-03-10 14:47:30 -0600

    init.d/mu4e/init-mu4e-alerts.el

--- init.d/mu4e/init-mu4e-alerts.el
+++ init.d/mu4e/init-mu4e-alerts.el
@@ -1,4 +1,4 @@
-;; (req-package mu4e-alert
+;; (use-package mu4e-alert
 ;;   :config(progn
 ;;            (mu4e-alert-set-default-style 'libnotify)
 ;;            (add-hook 'after-init-hook #'mu4e-alert-enable-notifications)))
