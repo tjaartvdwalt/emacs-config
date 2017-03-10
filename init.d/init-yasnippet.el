045a0eb1850a23eed88225f24cc27c2c103472a1
Tjaart van der Walt <git@tjaart.co.za>
2017-03-10 16:04:03 -0600

    init.d/init-yasnippet.el

--- init.d/init-yasnippet.el
+++ init.d/init-yasnippet.el
@@ -1,7 +1,7 @@
 ;; custom snippets go in ~/.emac.d/snippets
 
 (use-package yasnippet
-  :require (auto-yasnippet java-snippets react-snippets)
+  ;; :require (auto-yasnippet java-snippets react-snippets)
 
   :config
   (yas-global-mode 1)
