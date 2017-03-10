cf5c602bc0338c9d8fec9417ba76393655db7536
Tjaart van der Walt <git@tjaart.co.za>
2017-03-10 10:32:16 -0600

    init.d/init-tdd.el

--- init.d/init-tdd.el
+++ init.d/init-tdd.el
@@ -1,1 +1,11 @@
-(req-package tdd)
+(req-package tdd
+  :config (progn 
+(add-hook 'js-mode-hook (lambda () (add-hook 'after-save-hook 'runjest nil t))))
+
+(defun runjest ()
+  "Run jest with tdd"
+  (let ((errbuf (get-buffer-create "*TDD Errors*")))
+    (call-process "jest" nil errbuf nil nil nil)
+    ))
+
+
