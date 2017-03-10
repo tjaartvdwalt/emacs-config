07c1d64c097f57b3964050e826f3a989308fb7e0
Tjaart van der Walt <git@tjaart.co.za>
2017-03-10 15:18:11 -0600

    init.d/init-gitlab.el

--- init.d/init-gitlab.el
+++ init.d/init-gitlab.el
@@ -1,5 +1,5 @@
 (use-package gitlab
-  :require (helm-gitlab)
+  ;; :require (helm-gitlab)
   :config(progn
            (setq gitlab-host "https://gitlab.com"
                  gitlab-token-id "W4zVaUd3DVoPyfDRbAkn")))
