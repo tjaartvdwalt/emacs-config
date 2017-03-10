4a322f133d0ba1ff38fc4dbf5653d94c95cc2403
Tjaart van der Walt <git@tjaart.co.za>
2017-03-10 15:54:47 -0600

    init.d/init-readline-complete.el

--- init.d/init-readline-complete.el
+++ init.d/init-readline-complete.el
@@ -1,10 +1,10 @@
-;;; package --- summary
-;;; Commentary:
-;;; Code:
-(use-package readline-complete
-  :defines ac-modes
-  ;; :require (auto-complete)
-  :config
-  (progn
-    (add-to-list 'ac-modes 'shell-mode)
-    (add-hook 'shell-mode-hook 'ac-rlc-setup-sources)))
+;; ;;; package --- summary
+;; ;;; Commentary:
+;; ;;; Code:
+;; (use-package readline-complete
+;;   :defines ac-modes
+;;   ;; :require (auto-complete)
+;;   :config
+;;   (progn
+;;     (add-to-list 'ac-modes 'shell-mode)
+;;     (add-hook 'shell-mode-hook 'ac-rlc-setup-sources)))
