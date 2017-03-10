ebfa72847e892fd58c605c7155fc717a9a07097c
Tjaart van der Walt <git@tjaart.co.za>
2017-03-10 14:49:20 -0600

    init.d/init-rainbow-delimeters.el

--- init.d/init-rainbow-delimeters.el
+++ init.d/init-rainbow-delimeters.el
@@ -1,7 +1,7 @@
 ;;; package --- summary
 ;;; Commentary:
 ;;; Code:
-(req-package rainbow-delimiters
+(use-package rainbow-delimiters
   :config
   (progn
     (add-hook 'prog-mode-hook #'rainbow-delimiters-mode)))
