03318af449f5157071e81bde08fa4c7f8f2245db
Tjaart van der Walt <git@tjaart.co.za>
2017-03-10 14:45:57 -0600

    init.d/init-base16.el

--- init.d/init-base16.el
+++ init.d/init-base16.el
@@ -1,6 +1,6 @@
 (elpa-install 'base16-theme)
 (load-theme 'base16-google-dark t)
-;; (req-package base16-theme
+;; (use-package base16-theme
 ;;   ;; :ensure (base16-google-dark)
 ;;   :config (progn
 ;;             ))
