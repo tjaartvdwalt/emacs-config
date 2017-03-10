cc66b48b79c08f2dff5ac783ab168ae20a5df9e1
Tjaart van der Walt <git@tjaart.co.za>
2017-03-10 16:33:32 -0600

    init.d/init-smart-compile.el

--- init.d/init-smart-compile.el
+++ init.d/init-smart-compile.el
@@ -1,20 +1,20 @@
-;; (use-package smart-compile
-;;   :config (progn
-;;           ;; Set a shortcut for compile
-;;           (global-set-key "\C-xc" 'smart-compile)
-;;           (setq smart-compile-alist '(
-;;                                     (emacs-lisp-mode    . (emacs-lisp-byte-compile))
-;;                                     (html-mode          . (browse-url-of-buffer))
-;;                                     (nxhtml-mode        . (browse-url-of-buffer))
-;;                                     (html-helper-mode   . (browse-url-of-buffer))
-;;                                     (octave-mode        . (run-octave))
-;;                                     ("\\.c\\'"          . "gcc -O2 %f -lm -o %n")
-;;                                     ("\\.[Cc]+[Pp]*\\'" . "g++ -O2 %f -lm -o %n")
-;;                                     ("\\.go\\'"         . "go build")
-;;                                     ("\\.m\\'"          . "gcc -O2 %f -lobjc -lpthread -o %n")
-;;                                     ("\\.java\\'"       . "javac %f")
-;;                                     ("\\.php\\'"        . "php -l %f")
-;;                                     ("\\.pl\\'"         . "perl -cw %f")
-;;                                     ("\\.rb\\'"         . "ruby -cw %f")
-;;                                     )
-;;           )))
+(use-package smart-compile
+  :config (progn
+          ;; Set a shortcut for compile
+          (global-set-key "\C-xc" 'smart-compile)
+          (setq smart-compile-alist '(
+                                    (emacs-lisp-mode    . (emacs-lisp-byte-compile))
+                                    (html-mode          . (browse-url-of-buffer))
+                                    (nxhtml-mode        . (browse-url-of-buffer))
+                                    (html-helper-mode   . (browse-url-of-buffer))
+                                    (octave-mode        . (run-octave))
+                                    ("\\.c\\'"          . "gcc -O2 %f -lm -o %n")
+                                    ("\\.[Cc]+[Pp]*\\'" . "g++ -O2 %f -lm -o %n")
+                                    ("\\.go\\'"         . "go build")
+                                    ("\\.m\\'"          . "gcc -O2 %f -lobjc -lpthread -o %n")
+                                    ("\\.java\\'"       . "javac %f")
+                                    ("\\.php\\'"        . "php -l %f")
+                                    ("\\.pl\\'"         . "perl -cw %f")
+                                    ("\\.rb\\'"         . "ruby -cw %f")
+                                    )
+          )))
