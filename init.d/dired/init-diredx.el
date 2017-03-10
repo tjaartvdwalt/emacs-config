271a2209fc81d2c811095d75547de75def440f2a
Tjaart van der Walt <git@tjaart.co.za>
2017-03-10 16:13:25 -0600

    init.d/dired/init-diredx.el

--- init.d/dired/init-diredx.el
+++ init.d/dired/init-diredx.el
@@ -1,11 +1,7 @@
-;; use dired-x for hiding hidden files
-(use-package dired-x
-  ;; :require dired
-  :config
-  (progn
-    (setq dired-omit-mode t)
-    (setq-default dired-omit-files-p t)
-    ;; 1) Hide files that start with # (emacs tmp files)
-    ;; 2) Hide files that start with . then not a dot then anything
-    ;;    This will hide any hidden files, but not . and .. that I use to navigate
-    (setq dired-omit-files "^#\\|^\\.[^.].*$")))
+(progn
+  (setq dired-omit-mode t)
+  (setq-default dired-omit-files-p t)
+  ;; 1) Hide files that start with # (emacs tmp files)
+  ;; 2) Hide files that start with . then not a dot then anything
+  ;;    This will hide any hidden files, but not . and .. that I use to navigate
+  (setq dired-omit-files "^#\\|^\\.[^.].*$"))
