83c509a528291ad954133ea140648e6b55fd2067
Tjaart van der Walt <git@tjaart.co.za>
2017-03-14 22:26:35 -0500

    init.d/js/init-js-mode.el

--- init.d/js/init-js-mode.el
+++ init.d/js/init-js-mode.el
@@ -1,5 +1,6 @@
 (progn
   (setq js-indent-level 2)
+  (add-to-list 'auto-mode-alist '("\\.js\\'" . js-jsx-mode))
   (add-to-list 'auto-mode-alist '("\\.jsx\\'" . js-jsx-mode))
   (add-hook 'js-mode-hook
             (lambda ()
