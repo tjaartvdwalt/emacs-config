f1a6073b8e0e0abf6ff484bc3754f92db4123000
Tjaart van der Walt <git@tjaart.co.za>
2017-03-29 13:18:20 -0500

    init.d/ac/init-auto-complete.el

--- init.d/ac/init-auto-complete.el
+++ init.d/ac/init-auto-complete.el
@@ -3,7 +3,7 @@
   ;; :require (yasnippet auto-complete-config)
   :config (progn
             (setq ac-use-fuzzy t)
-            (setq-default ac-sources '(ac-source-yasnippet ac-source-abbrev ac-source-dictionary ac-source-filename ac-source-words-in-same-mode-buffers))
+            (setq-default ac-sources '(ac-source-yasnippet ac-source-abbrev ac-source-dictionary ac-source-filename ac-source-words-in-same-mode-buffers ac-source-ispell-fuzzy))
             (global-auto-complete-mode 1)
             (ac-flyspell-workaround)))
 
