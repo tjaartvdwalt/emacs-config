aa9cdb63ec13f8034d7bd751a8cef1778c76f9f2
Tjaart van der Walt <git@tjaart.co.za>
2017-03-31 12:38:40 -0500

    init.d/init-smart-compile.el

--- init.d/init-smart-compile.el
+++ init.d/init-smart-compile.el
@@ -1,8 +1,6 @@
 (use-package smart-compile
   :bind (("C-x c" . smart-compile)
   :config (progn
-            ;; Set a shortcut for compile
-            (global-set-key "\C-xc" 'smart-compile)
             (setq smart-compile-alist '(
                                         ("\\.js[x]*\\'"      . "npm test")
                                         ))))
