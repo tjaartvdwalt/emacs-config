2e4da7513537fa223afcb4883aec953efe7f2c9e
Tjaart van der Walt <git@tjaart.co.za>
2017-03-31 12:53:17 -0500

    init.d/init-tdd.el

--- init.d/init-tdd.el
+++ init.d/init-tdd.el
@@ -4,8 +4,3 @@
     (add-hook 'js-mode-hook 'tdd-mode)
     (custom-set-variables
      '(tdd-test-function (smart-compile)))))
-
-    ;; (defun runjest ()
-    ;;   "Run jest with tdd"
-    ;;   (let ((errbuf (get-buffer-create "*TDD Errors*")))
-    ;;     (call-process "jest" nil nil)))
