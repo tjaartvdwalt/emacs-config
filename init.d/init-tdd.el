ad57975b1d3e52a5f27ff08ea461547ad6bf9984
Tjaart van der Walt <git@tjaart.co.za>
2017-03-31 12:32:39 -0500

    init.d/init-tdd.el

--- init.d/init-tdd.el
+++ init.d/init-tdd.el
@@ -1,7 +1,7 @@
 (use-package tdd
   :config
   (progn
-    (add-hook 'js-mode-hook #'tdd-mode)
+    (add-hook 'js-mode-hook 'tdd-mode)
     (custom-set-variables
      '(tdd-test-function (smart-compile)))))
 
