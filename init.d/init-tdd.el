7b6f43660365662fbebc4c5e7d608119376d9664
Tjaart van der Walt <git@tjaart.co.za>
2017-03-31 16:37:39 -0500

    init.d/init-tdd.el

--- init.d/init-tdd.el
+++ init.d/init-tdd.el
@@ -1,6 +1,6 @@
 (use-package tdd
   :config
   (progn
-    (add-hook 'prog-mode-hook #'tdd-mode t)
+    (add-hook 'prog-mode-hook (lambda () (tdd-mode t)))
     (custom-set-variables
      '(tdd-test-function (smart-compile))))) 
