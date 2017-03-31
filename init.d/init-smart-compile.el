e2af684607c9bb503a44b0b3b93b86941066cf74
Tjaart van der Walt <git@tjaart.co.za>
2017-03-31 12:54:45 -0500

    init.d/init-smart-compile.el

--- init.d/init-smart-compile.el
+++ init.d/init-smart-compile.el
@@ -1,5 +1,4 @@
 (use-package smart-compile
-  ;; :bind (("C-x c" . smart-compile)
          :config (progn
                    (add-to-list 'smart-compile-alist
                                 '("\\.js[x]*\\'"      . "npm test"))))
