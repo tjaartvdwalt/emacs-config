63cc34c6ef1b54580527794c96ed312509bc510f
Tjaart van der Walt <git@tjaart.co.za>
2017-03-31 10:32:57 -0500

    init.d/init-git-commit-insert-issue.el

--- init.d/init-git-commit-insert-issue.el
+++ init.d/init-git-commit-insert-issue.el
@@ -1,5 +1,3 @@
 (use-package git-commit-insert-issue
   :config(progn
-(add-hook 'git-commit-mode-hook 'git-commit-insert-issue-mode)
-           )
-  )
+           (add-hook 'git-commit-mode-hook 'git-commit-insert-issue-mode)))
