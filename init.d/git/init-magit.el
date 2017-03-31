ef4c3a120edf4a59d91fc934f3c1b288cb74be69
Tjaart van der Walt <git@tjaart.co.za>
2017-03-31 13:36:57 -0500

    init.d/git/init-magit.el

--- init.d/git/init-magit.el
+++ init.d/git/init-magit.el
@@ -8,9 +8,5 @@
  (progn
    (setq magit-revert-buffers "ask")
    (setq magit-last-seen-setup-instructions "1.4.0")    ))
-
-   ;; (when (not window-system)
-   ;; (set-face-background 'magit-item-highlight "black"))
-   ;; (add-hook 'magit-status-mode-hook (lambda () (magit-filenotify-mode t)))
 (provide 'init-magit)
 ;; init-magit ends here
