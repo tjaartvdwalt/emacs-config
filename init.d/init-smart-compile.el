c0fa3817b5794a6798816131b995d177c8bcafc1
Tjaart van der Walt <git@tjaart.co.za>
2017-03-31 13:21:26 -0500

    init.d/init-smart-compile.el

--- init.d/init-smart-compile.el
+++ init.d/init-smart-compile.el
@@ -1,6 +1,9 @@
 (use-package smart-compile
          :config (progn
                    (add-to-list 'smart-compile-alist
-                                ;; assumes a properly configured npm project
+                                ;; npm install -g jsonlint
+                                '("\\.json\\'"      . "js-yaml %f")
+                                ;;  npm install -g js-yaml
                                 '("\\.y[a]*ml\\'"      . "js-yaml %f")
+                                ;; assumes a properly configured npm project, with test script
                                 '("\\.js[x]*\\'"      . "npm test"))))
