df466103deeb0cb3c3695db15755493e3f3e306d
Tjaart van der Walt <git@tjaart.co.za>
2017-03-31 13:05:41 -0500

    init.d/init-smart-compile.el

--- init.d/init-smart-compile.el
+++ init.d/init-smart-compile.el
@@ -1,4 +1,6 @@
 (use-package smart-compile
          :config (progn
                    (add-to-list 'smart-compile-alist
+                                ;; assumes a properly configured npm project
+                                '("\\.yml\\'"      . "js-yml")
                                 '("\\.js[x]*\\'"      . "npm test"))))
