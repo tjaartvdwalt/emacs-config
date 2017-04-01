c9e1cc956a3f200b801021a5b37a81510b08526e
Tjaart van der Walt <git@tjaart.co.za>
2017-04-01 11:33:24 -0500

    init.el

--- init.el
+++ init.el
@@ -69,7 +69,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "monospace" :slant normal :weight normal :height 130 :width normal)))))
+ '(default ((t (:family "monospace" :slant normal :weight normal :height 200 :width normal)))))
 
 
 (put 'upcase-region 'disabled nil)
