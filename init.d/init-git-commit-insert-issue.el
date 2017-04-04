0796d4e7693458ea422ebcc669d5ea7934d00656
Tjaart van der Walt <git@tjaart.co.za>
2017-04-04 15:49:26 -0500

    init.d/init-git-commit-insert-issue.el

--- init.d/init-git-commit-insert-issue.el
+++ init.d/init-git-commit-insert-issue.el
@@ -1,6 +1,6 @@
 ;; for development, use my local repos
 
-(load-dir-one "/home/tjaart/code/bitbucket-issues.el")
+(load-dir-one "/home/tjaart/code/bitbucket.el")
 (load-dir-one "/home/tjaart/code/git-commit-insert-issue")
 
 (add-hook 'git-commit-mode-hook 'git-commit-insert-issue-mode)
