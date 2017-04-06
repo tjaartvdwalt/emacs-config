db4097ab4c308df6094cfd13d4c9d5e0e1481b28
Tjaart van der Walt <git@tjaart.co.za>
2017-04-06 11:22:45 -0500

    init.d/init-base16.el

--- init.d/init-base16.el
+++ init.d/init-base16.el
@@ -1,5 +1,5 @@
 (elpa-install 'base16-theme)
-(load-theme 'base16-google-dark t)
+(load-theme 'base16-solarized-light t)
 ;; (use-package base16-theme
 ;;   ;; :ensure (base16-google-dark)
 ;;   :config (progn
