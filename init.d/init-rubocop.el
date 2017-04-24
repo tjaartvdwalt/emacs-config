e3916329f9b6c7cd23c65b5abecc74e24e4a1cd1
Tjaart van der Walt <git@tjaart.co.za>
2017-04-24 09:26:27 -0500

    init.d/init-rubocop.el

--- init.d/init-rubocop.el
+++ init.d/init-rubocop.el
@@ -3,7 +3,5 @@
             (add-hook 'enh-ruby-mode-hook (lambda () (add-hook 'after-save-hook 'rubocop-save-buffer t t)))))
 
 (defun rubocop-save-buffer ()
-  "docstring"
-  (interactive "P")
   (message "saved ruby file")
   )
