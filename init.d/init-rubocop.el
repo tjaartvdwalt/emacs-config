1e7ca075c8218c81b93545ba0beb2186e2f2d839
Tjaart van der Walt <git@tjaart.co.za>
2017-04-24 09:30:24 -0500

    init.d/init-rubocop.el

--- init.d/init-rubocop.el
+++ init.d/init-rubocop.el
@@ -1,6 +1,6 @@
 (use-package rubocop
   :config (progn
-            (add-hook 'enh-ruby-mode-hook (lambda () (add-hook 'after-save-hook 'rubocop-save-buffer t t)))))
+            (add-hook 'enh-ruby-mode-hook (lambda () (add-hook 'after-save-hook 'rubocop-autocorrect-current-file t t)))))
 
 (defun rubocop-save-buffer ()
   (rubocop-autocorrect-current-file))
