a227f4bb7376158fdfa117bd6124f2e823bec68d
Tjaart van der Walt <git@tjaart.co.za>
2017-04-27 10:47:30 -0500

    init.d/init-yagist.el

--- init.d/init-yagist.el
+++ init.d/init-yagist.el
@@ -1,3 +1,2 @@
 (use-package yagist
-  :config (progn
-(setq yagist-github-token "20e15986d39df6b8ee6e82fcd89318663c2f86ae")))
+  :config (setq yagist-github-token "20e15986d39df6b8ee6e82fcd89318663c2f86ae"))
