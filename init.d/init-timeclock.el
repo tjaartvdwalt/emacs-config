3a122029d75937bc0a41d8a9e921f29efde8e3ec
Tjaart van der Walt <git@tjaart.co.za>
2017-05-29 12:02:49 -0500

    init.d/init-timeclock.el

--- init.d/init-timeclock.el
+++ init.d/init-timeclock.el
@@ -1,8 +1,8 @@
 (use-package timeclock
-  :bind (("C-t i" . timeclock-in)
-          ("C-t o" . timeclock-out)
-           ("C-t r" . timeclock-reread-log)
-           ("C-t c" . timeclock-change)
-           ("C-t t" . timeclock-visit-timelog))
+  :bind (("C-c t i" . timeclock-in)
+          ("C-c t o" . timeclock-out)
+           ("C-c t r" . timeclock-reread-log)
+           ("C-c t c" . timeclock-change)
+           ("C-c t t" . timeclock-visit-timelog))
   :config((timeclock-mode-line-displa t))
 )
