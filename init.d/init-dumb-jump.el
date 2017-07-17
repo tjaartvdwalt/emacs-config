f75092121ecdb27c525ec75ff7185a37a053765d
Tjaart van der Walt <git@tjaart.co.za>
2017-07-17 11:39:33 -0700

    init.d/init-dumb-jump.el

--- init.d/init-dumb-jump.el
+++ init.d/init-dumb-jump.el
@@ -4,6 +4,8 @@
   ;;         ("M-g i" . dumb-jump-go-prompt)
   ;;         ("M-g x" . dumb-jump-go-prefer-external)
   ;;         ("M-g z" . dumb-jump-go-prefer-external-other-window))
-  :config (setq dumb-jump-selector 'helm)
+  :config (progn
+            (add-hook 'prog-mode-hook #'dumb-jump-mode)
+            (setq dumb-jump-selector 'helm))
   :ensure)
 )
