25c5875231f87dfc7a7bdaf3f259e86d25c46bbc
Tjaart van der Walt <git@tjaart.co.za>
2017-07-25 11:07:15 -0700

    init.d/ivy/init-counsel-projectile.el

---
+++ init.d/ivy/init-counsel-projectile.el
@@ -0,0 +1,5 @@
+(use-package counsel-projectile :ensure t
+  :bind* (("C-c p p" . counsel-projectile))
+  :init
+  (setq counsel-projectile-drop-to-switch-project-binding "C-c s p")
+  (counsel-projectile-on))
