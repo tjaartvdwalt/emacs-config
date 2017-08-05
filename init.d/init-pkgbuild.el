daa479978c37b697b9d43462e25ebe16a2164456
Tjaart van der Walt <git@tjaart.co.za>
2017-08-04 18:27:11 -0700

    init.d/init-pkgbuild.el

--- init.d/init-pkgbuild.el
+++ init.d/init-pkgbuild.el
@@ -1,4 +1,4 @@
-(use-package pony-mode
+(use-package pkgbuild-mode
   :config(progn
 (autoload 'pkgbuild-mode "pkgbuild-mode.el" "PKGBUILD mode." t)
  (setq auto-mode-alist (append '(("/PKGBUILD$" . pkgbuild-mode))
