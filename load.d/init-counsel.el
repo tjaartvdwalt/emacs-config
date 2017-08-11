f4cc63f2f37479055853f4c00dd67f1afa9ad21c
Tjaart van der Walt <git@tjaart.co.za>
2017-08-11 11:32:02 -0700

    load.d/init-counsel.el

---
+++ load.d/init-counsel.el
@@ -0,0 +1,61 @@
+(use-package counsel :ensure t
+  :bind*
+  (("M-x"     . counsel-M-x)
+   ("<f1> c"   . counsel-descbinds)
+   ("<f1> f"   . counsel-describe-function)
+   ("<f1> v"   . counsel-describe-variable)
+   ("<f1> l"   . counsel-find-library)
+   ("<f1> s"   . describe-symbol)
+   ("<f2> i"   . counsel-info-lookup-symbol)
+   ("<f2> u"   . counsel-unicode-char)
+   ("M-y"     . counsel-yank-pop)
+   ("C-x C-f" . counsel-find-file)
+   ("C-x r f" . counsel-recentf)
+   ("C-c g" . counsel-git)
+   ("C-c j" . counsel-git-grep)
+   ("C-c k"   . counsel-ag)
+   ("C-c r"   . counsel-rhythmbox)
+   ("C-x l"   . counsel-locate)
+;;   ("C-c m" . counsel-imenu)            
+    )
+
+  :config
+  (defun reloading (cmd)
+    (lambda (x)
+      (funcall cmd x)
+      (ivy--reset-state ivy-last)))
+  (defun given-file (cmd prompt) ; needs lexical-binding
+    (lambda (source)
+      (let ((target
+         (let ((enable-recursive-minibuffers t))
+           (read-file-name
+        (format "%s %s to:" prompt source)))))
+    (funcall cmd source target 1))))
+  (defun confirm-delete-file (x)
+    (dired-delete-file x 'confirm-each-subdirectory))
+
+  (ivy-add-actions
+   'counsel-find-file
+   `(("c" ,(given-file #'copy-file "Copy") "copy")
+     ("d" ,(reloading #'confirm-delete-file) "delete")
+     ("m" ,(reloading (given-file #'rename-file "Move")) "move")))
+  (ivy-add-actions
+   'counsel-projectile-find-file
+   `(("c" ,(given-file #'copy-file "Copy") "copy")
+     ("d" ,(reloading #'confirm-delete-file) "delete")
+     ("m" ,(reloading (given-file #'rename-file "Move")) "move")
+     ("b" counsel-find-file-cd-bookmark-action "cd bookmark")))
+
+  ;; to make counsel-ag search the root projectile directory.
+  (defun counsel-ag-projectile ()
+    (interactive)
+    (counsel-ag nil (projectile-project-root)))
+
+  (setq counsel-find-file-at-point t)
+  ;; ignore . files or temporary files
+  (setq counsel-find-file-ignore-regexp
+    (concat
+     ;; File names beginning with # or .
+     "\\(?:\\`[#.]\\)"
+     ;; File names ending with # or ~
+     "\\|\\(?:\\`.+?[#~]\\'\\)")))
