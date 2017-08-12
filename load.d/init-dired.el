d8b995f16c36108813233d2f3fd67806a35c0aaa
Tjaart van der Walt <git@tjaart.co.za>
2017-08-11 20:40:46 -0700

    load.d/init-dired.el

--- load.d/init-dired.el
+++ load.d/init-dired.el
@@ -1,17 +1,15 @@
-(use-package
-  :config
-  (use-package dired-k
-    ;; use dired-k as alternative to revert buffer. This will refresh git status
-    :bind (:map dired-mode-map
-                ("g" . dired-k))
-    :config
-    (add-hook 'dired-mode-hook 'dired-k))
+;; Configure dired to sort directories first
+(setq dired-listing-switches "-alh")
+;; used to attach files in mu4e
+(add-hook 'dired-mode-hook 'turn-on-gnus-dired-mode)
+(setq diredp-hide-details-initially-flag nil)
+(setq dired-auto-revert-buffer t)
+(define-key dired-mode-map (kbd "\\") 'dired-get-size)
+(define-key dired-mode-map (kbd "/") 'diredp-up-directory-reuse-dir-buffer))
 
-  ;; Configure dired to sort directories first
-  (setq dired-listing-switches "-alh")
-  ;; used to attach files in mu4e
-  (add-hook 'dired-mode-hook 'turn-on-gnus-dired-mode)
-  (setq diredp-hide-details-initially-flag nil)
-  (setq dired-auto-revert-buffer t)
-  (define-key dired-mode-map (kbd "\\") 'dired-get-size)
-  (define-key dired-mode-map (kbd "/") 'diredp-up-directory-reuse-dir-buffer))
+(use-package dired-k
+  ;; use dired-k as alternative to revert buffer. This will refresh git status
+  :bind (:map dired-mode-map
+              ("g" . dired-k))
+  :config
+  (add-hook 'dired-mode-hook 'dired-k))
