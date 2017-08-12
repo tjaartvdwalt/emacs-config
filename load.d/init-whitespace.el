e7669df0d18414b8e50dddb9250ecadffd48f860
Tjaart van der Walt <git@tjaart.co.za>
2017-08-11 20:53:41 -0700

    load.d/init-whitespace.el

--- load.d/init-whitespace.el
+++ load.d/init-whitespace.el
@@ -1,7 +1,3 @@
 ;; make whitespace-mode use just basic coloring
 (use-package whitespace
-  :bind ("C-c w" . whitespace-mode)
-  :config
-  (global-set-key "\C-cw" 'whitespace-mode)
-  )
-;;(global-whitespace-mode 1)
+  :bind ("C-c w" . whitespace-mode))
