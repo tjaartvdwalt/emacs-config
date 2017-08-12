239054a054ecc8a410f9fe44a6a14d54c875da2a
Tjaart van der Walt <git@tjaart.co.za>
2017-08-11 22:52:59 -0700

    load.d/init-web-mode.el

--- load.d/init-web-mode.el
+++ load.d/init-web-mode.el
@@ -1,27 +1,27 @@
 (use-package web-mode
   ;; :require (yasnippet flycheck auto-complete)
-  :mode "\\.html?\\'"
+  :mode "\\.[agj]sp\\|\\.as[cp]x\\|\\.css\\|\\.djhtml\\|\\.erb\\|\\.hbs\\|\\.mustache\\|\\.php\\|\\.phtml\\|||\\.html?\\'"
   :config
   (setq web-mode-enable-css-colorization t)
   (setq web-mode-enable-auto-pairing t)
   (setq web-mode-enable-current-element-highlight t)
   (setq web-mode-enable-current-column-highlight t)
 
-  ;; add extensions to mode list
-  ;; (add-to-list 'ac-modes 'web-mode)
-  (add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
-  (add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
-  (add-to-list 'auto-mode-alist '("\\.css\\'" . web-mode))
-  (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
-  (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
-  (add-to-list 'auto-mode-alist '("\\.hbs\\'" . web-mode))
-  ;; (add-to-list 'auto-mode-alist '("\\.jsx\\'" . web-mode))
-  (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
-  (add-to-list 'auto-mode-alist '("\\.php\\'" . web-mode))
-  (add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
-  ;; (add-to-list 'auto-mode-alist '("\\.sass\\'" . web-mode))
-  ;; (add-to-list 'auto-mode-alist '("\\.scss\\'" . web-mode))
-  (add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+  ;; ;; add extensions to mode list
+  ;; ;; (add-to-list 'ac-modes 'web-mode)
+  ;; (add-to-list 'auto-mode-alist '("'" . web-mode))
+  ;; (add-to-list 'auto-mode-alist '("'" . web-mode))
+  ;; (add-to-list 'auto-mode-alist '("'" . web-mode))
+  ;; (add-to-list 'auto-mode-alist '("'" . web-mode))
+  ;; (add-to-list 'auto-mode-alist '("'" . web-mode))
+  ;; (add-to-list 'auto-mode-alist '("'" . web-mode))
+  ;; ;; (add-to-list 'auto-mode-alist '("\\.jsx\\'" . web-mode))
+  ;; (add-to-list 'auto-mode-alist '("'" . web-mode))
+  ;; (add-to-list 'auto-mode-alist '("'" . web-mode))
+  ;; (add-to-list 'auto-mode-alist '("'" . web-mode))
+  ;; ;; (add-to-list 'auto-mode-alist '("\\.sass\\'" . web-mode))
+  ;; ;; (add-to-list 'auto-mode-alist '("\\.scss\\'" . web-mode))
+  ;; (add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
 
 
   ;; (setq web-mode-ac-sources-alist
