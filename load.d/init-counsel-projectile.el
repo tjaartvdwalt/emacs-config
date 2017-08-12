72b1873102d11e03700c65856aeeca1439d23ace
Tjaart van der Walt <git@tjaart.co.za>
2017-08-11 22:21:15 -0700

    load.d/init-counsel-projectile.el

--- load.d/init-counsel-projectile.el
+++ load.d/init-counsel-projectile.el
@@ -1,5 +1,5 @@
 (use-package counsel-projectile :ensure t
-  :bind (("C-c p p" . counsel-projectile))
+  :bind ("C-c p p" . counsel-projectile)
   :init
   (setq counsel-projectile-drop-to-switch-project-binding "C-c s p")
   (counsel-projectile-on))
