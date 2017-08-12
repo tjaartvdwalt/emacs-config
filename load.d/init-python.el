d1a3f56ecb180e2f23c0021b5c7584a22c604042
Tjaart van der Walt <git@tjaart.co.za>
2017-08-11 18:37:15 -0700

    load.d/init-python.el

--- load.d/init-python.el
+++ load.d/init-python.el
@@ -8,10 +8,15 @@
   (use-package py-isort
     :config(add-hook 'before-save-hook 'py-isort-before-save))
 
+  ;; sudo pip install autopep8
   (use-package py-autopep8
     ;; :require(python-mode)
     :config (add-hook 'python-mode-hook 'py-autopep8-enable-on-save))
 
+  (use-package jedi
+    :config
+    (add-hook 'python-mode-hook 'jedi:setup)
+    (setq jedi:complete-on-dot t))
 
   )
 
