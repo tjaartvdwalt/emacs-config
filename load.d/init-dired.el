ea97cc948da91d21aa85f939dd5c13d5d267bc17
Tjaart van der Walt <git@tjaart.co.za>
2017-08-11 20:30:19 -0700

    load.d/init-dired.el

--- load.d/init-dired.el
+++ load.d/init-dired.el
@@ -1,10 +1,10 @@
 (use-package dired
   :config
-    ;; Configure dired to sort directories first
-    (setq dired-listing-switches "-alh")
-    ;; used to attach files in mu4e
-    (add-hook 'dired-mode-hook 'turn-on-gnus-dired-mode)
-    (setq diredp-hide-details-initially-flag nil)
-    (setq dired-auto-revert-buffer t)
-    (define-key dired-mode-map (kbd "\\") 'dired-get-size)
-    (define-key dired-mode-map (kbd "/") 'diredp-up-directory-reuse-dir-buffer))
+  ;; Configure dired to sort directories first
+  (setq dired-listing-switches "-alh")
+  ;; used to attach files in mu4e
+  (add-hook 'dired-mode-hook 'turn-on-gnus-dired-mode)
+  (setq diredp-hide-details-initially-flag nil)
+  (setq dired-auto-revert-buffer t)
+  (define-key dired-mode-map (kbd "\\") 'dired-get-size)
+  (define-key dired-mode-map (kbd "/") 'diredp-up-directory-reuse-dir-buffer))
