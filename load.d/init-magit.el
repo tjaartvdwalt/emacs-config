29e997609a3e1b1cc99dd300e14bc83cc50d485c
Tjaart van der Walt <git@tjaart.co.za>
2017-08-11 22:26:49 -0700

    load.d/init-magit.el

--- load.d/init-magit.el
+++ load.d/init-magit.el
@@ -5,9 +5,17 @@
   ;; :require (magit-annex magit-rockstar)
   :bind ("C-c v" . magit-status)
   :config
-    (setq magit-revert-buffers "ask")
-    (setq magit-last-seen-setup-instructions "1.4.0")
-    (setq magit-completing-read-function 'ivy-completing-read))
+  (setq magit-revert-buffers "ask")
+  (setq magit-last-seen-setup-instructions "1.4.0")
+  (setq magit-completing-read-function 'ivy-completing-read)
+
+  (use-package magit-svn
+    :config
+    (add-hook 'magit-mode-hook 'magit-svn-mode))
+
+  (use-package magit-annex)
+
+  (use-package gitconfig))
 
 (provide 'init-magit)
 ;; init-magit ends here
