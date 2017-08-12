1ed71847022a12144fcd1d8a8f14bf79305d4084
Tjaart van der Walt <git@tjaart.co.za>
2017-08-12 09:53:57 -0700

    load.d/init-ispell.el

---
+++ load.d/init-ispell.el
@@ -0,0 +1,33 @@
+;;; package --- summary
+;;; Commentary:
+;;; On Arch linux I have `aspell' and `aspell-en' installed.
+;;; According to http://www.emacswiki.org/emacs/InteractiveSpell this
+;;; gets automatically configured in emacs 24.4+
+;;;
+;;; I also have `words-insane' installed as completion for ac-ispell
+;;; Code:
+
+(use-package ispell
+  :diminish ispell-minor-mode
+  ;; :bind
+  ;; instead of having ispell shortcuts, I now use helm-flyspell
+  ;; ("C-c i w" . ispell-word)
+  ;; ("C-c i b" . ispell-buffer))
+
+  :config
+  (progn
+    (add-hook 'text-mode-hook 'ispell-minor-mode)
+    (setq ispell-alternate-dictionary "/usr/share/dict/words-insane")
+    ;; (setq ispell-change-dictionary "british")
+    ;; (global-set-key "\C-cs" 'ispell-buffer)
+    ;; (global-set-key "\C-cn" 'ispell-word)
+    )
+  (use-package flyspell
+    :diminish flyspell-mode
+    :config
+    (add-hook 'text-mode-hook 'flyspell-mode)
+    (use-package flyspell-correct-ivy
+      :bind ("C-c s" 'flyspell-correct-previous-word-generic)))))
+
+(provide 'init-ispell)
+;;; init-ispell ends here
