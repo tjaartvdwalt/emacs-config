a776a78b4ae8564c503872ecd86329117d80c055
Tjaart van der Walt <git@tjaart.co.za>
2017-08-11 19:11:07 -0700

    load.d/init-ruby-mode.el

--- load.d/init-ruby-mode.el
+++ load.d/init-ruby-mode.el
@@ -1,15 +1,15 @@
 (use-package ruby-mode
-  :init (progn
-          (use-package rspec-mode)
-          (use-package inf-ruby)
-          (use-package robe)
-          (use-package yard-mode)
-          (use-package auto-complete))
-  :config (progn
-            (add-hook 'ruby-mode-hook 'robe-mode)
-            (add-hook 'ruby-mode-hook 'yard-mode)
-            (add-hook 'ruby-mode-hook 'eldoc-mode)
-            (add-to-list 'ac-modes 'ruby-mode)
-            (message "add to alist")
-            (add-to-list 'auto-mode-alist
-              '("\\(?:\\.rb\\|ru\\|rake\\|thor\\|jbuilder\\|gemspec\\|podspec\\|/\\(?:Gem\\|Rake\\|Cap\\|Thor\\|Vagrant\\|Guard\\|Pod\\)file\\)\\'" . ruby-mode))))
+  :config
+    (add-hook 'ruby-mode-hook 'eldoc-mode)
+  (add-to-list 'ac-modes 'ruby-mode)
+  (add-to-list 'auto-mode-alist
+               '("\\(?:\\.rb\\|ru\\|rake\\|thor\\|jbuilder\\|gemspec\\|podspec\\|/\\(?:Gem\\|Rake\\|Cap\\|Thor\\|Vagrant\\|Guard\\|Pod\\)file\\)\\'" . ruby-mode))
+  
+  (use-package inf-ruby)
+  (use-package chruby)
+  (use-package rspec-mode)
+  (use-package robe
+    :config (add-hook 'ruby-mode-hook 'robe-mode))
+
+  (use-package yard-mode
+    :config (add-hook 'ruby-mode-hook 'yard-mode)))
