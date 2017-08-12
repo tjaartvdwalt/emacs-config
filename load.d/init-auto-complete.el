90b30cf6c05c61761ed010cde05284395dda6342
Tjaart van der Walt <git@tjaart.co.za>
2017-08-11 19:02:31 -0700

    load.d/init-auto-complete.el

--- load.d/init-auto-complete.el
+++ load.d/init-auto-complete.el
@@ -1,16 +1,33 @@
- (use-package auto-complete
+(use-package auto-complete
   :diminish auto-complete-mode
   :init (require 'auto-complete-config)
-  :config (progn
-            (setq ac-use-fuzzy t)
-            (setq-default ac-sources
-                          '(ac-source-yasnippet
-                            ac-source-abbrev
-                            ac-source-dictionary
-                            ac-source-filename
-                            ac-source-words-in-same-mode-buffers))
-            (global-auto-complete-mode 1)
-             (ac-flyspell-workaround)))
- 
+  :config
+  (setq ac-use-fuzzy t)
+  (setq-default ac-sources
+                '(ac-source-yasnippet
+                  ac-source-abbrev
+                  ac-source-dictionary
+                  ac-source-filename
+                  ac-source-words-in-same-mode-buffers))
+  (global-auto-complete-mode 1)
+  (ac-flyspell-workaround)
+
+  (use-package ac-ispell
+    :config
+    (ac-ispell-setup)
+    (add-hook 'text-mode-hook 'ac-ispell-ac-setup))
+
+  (use-package ac-emoji
+    :config
+    (add-hook 'markdown-mode-hook 'ac-emoji-setup)
+    (add-hook 'git-commit-mode-hook 'ac-emoji-setup))
+
+  (use-package ac-emmet
+    :config
+    (add-hook 'sgml-mode-hook 'ac-emmet-html-setup)
+    (add-hook 'css-mode-hook 'ac-emmet-css-setup))
+
+  )
+
 ;; (setq ac-auto-start nil)
 ;; (define-key ac-mode-map (kbd "TAB") 'auto-complete)
