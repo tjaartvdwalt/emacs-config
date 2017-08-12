738efb5a54f0f9acef1a2aabb49ac0a5d32d81e5
Tjaart van der Walt <git@tjaart.co.za>
2017-08-11 18:32:20 -0700

    load.d/init-python.el

--- load.d/init-python.el
+++ load.d/init-python.el
@@ -3,11 +3,16 @@
 ;;; Code:
 (use-package python-mode
   :interpreter ("python" . python-mode)
-  ;; :config
-  ;; (progn
-  ;;   (add-hook 'python-mode-hook 'anaconda-mode)
-  ;;   (add-hook 'python-mode-hook 'anaconda-eldoc)
-  ;;   (add-to-list 'company-backends 'company-anaconda))
+  :config
+  ;; sudo pip install isort
+  (use-package py-isort
+    :config(add-hook 'before-save-hook 'py-isort-before-save))
+
+  (use-package py-autopep8
+    ;; :require(python-mode)
+    :config (add-hook 'python-mode-hook 'py-autopep8-enable-on-save))
+
+
   )
 
 (provide 'init-python)
