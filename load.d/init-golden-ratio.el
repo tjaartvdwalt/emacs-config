da41b3c3514160b1f777aa8198bfdb5ea223f693
Tjaart van der Walt <git@tjaart.co.za>
2017-08-11 20:57:13 -0700

    load.d/init-golden-ratio.el

--- load.d/init-golden-ratio.el
+++ load.d/init-golden-ratio.el
@@ -1,24 +1,23 @@
 (use-package golden-ratio
   :diminish golden-ratio-mode
-  :init(progn
-         (defcustom golden-ratio-extra-commands
-           '(windmove-left windmove-right windmove-down windmove-up ace-window)
-           "List of extra commands used to jump to other window."
-           :group 'golden-ratio
-           :type '(repeat symbol))
-         )
-  :config(progn
-           (golden-ratio-mode 1)
-           (setq golden-ratio-auto-scale t)
+  :init
+  (defcustom golden-ratio-extra-commands
+    '(windmove-left windmove-right windmove-down windmove-up ace-window)
+    "List of extra commands used to jump to other window."
+    :group 'golden-ratio
+    :type '(repeat symbol))
+  :config
+  (golden-ratio-mode 1)
+  (setq golden-ratio-auto-scale t)
 
 
-           (setq golden-ratio-adjust-factor .8
-                 golden-ratio-wide-adjust-factor .8)
+  (setq golden-ratio-adjust-factor .8
+        golden-ratio-wide-adjust-factor .8)
 
 
-           
-           ;; exclude golden-ratio from helm
-           (defun pl/helm-alive-p ()
-             (if (boundp 'helm-alive-p)
-                 (symbol-value 'helm-alive-p)))
-           (add-to-list 'golden-ratio-inhibit-functions 'pl/helm-alive-p)))
+
+  ;; exclude golden-ratio from helm
+  (defun pl/helm-alive-p ()
+    (if (boundp 'helm-alive-p)
+        (symbol-value 'helm-alive-p)))
+  (add-to-list 'golden-ratio-inhibit-functions 'pl/helm-alive-p))
