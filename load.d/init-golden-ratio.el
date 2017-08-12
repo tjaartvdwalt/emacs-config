f5f562a7ec1ae69a1763b49937c38a094a46a3b2
Tjaart van der Walt <git@tjaart.co.za>
2017-08-11 20:58:30 -0700

    load.d/init-golden-ratio.el

--- load.d/init-golden-ratio.el
+++ load.d/init-golden-ratio.el
@@ -6,6 +6,7 @@
     "List of extra commands used to jump to other window."
     :group 'golden-ratio
     :type '(repeat symbol))
+
   :config
   (golden-ratio-mode 1)
   (setq golden-ratio-auto-scale t)
@@ -14,10 +15,3 @@
   (setq golden-ratio-adjust-factor .8
         golden-ratio-wide-adjust-factor .8)
 
-
-
-  ;; exclude golden-ratio from helm
-  (defun pl/helm-alive-p ()
-    (if (boundp 'helm-alive-p)
-        (symbol-value 'helm-alive-p)))
-  (add-to-list 'golden-ratio-inhibit-functions 'pl/helm-alive-p))
