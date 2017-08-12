ba6c5c61965cdbb933b9da1d4b03cc33dabc2105
Tjaart van der Walt <git@tjaart.co.za>
2017-08-11 22:37:43 -0700

    load.d/init-ibuffer.el

--- load.d/init-ibuffer.el
+++ load.d/init-ibuffer.el
@@ -1,22 +1,7 @@
 (use-package ibuffer
-  ;; :require (ibuffer-vc ibuffer-git ibuffer-tramp)
   :config
-
-  ;; To actually make these columns show up, you need to customize
-  ;; `ibuffer-formats'. The symbol `git-status-mini' can be inserted
-  ;; where you want it, and `git-status' should be inserted with
-  ;; something as something like `(git-status 8 8 :left)', where 8 is
-  ;; the number you picked for `ibuffer-git-column-length'.
-
-  ;; (setq ibuffer-formats '((mark modified read-only " " (name 18 18 :left :elide)
-  ;;                       " " (size 9 -1 :right)
-  ;;                       " " (mode 16 16 :left :elide) " " filename-and-process)
-  ;;                 (mark " " (name 16 -1) " " filename)))
 
   (add-hook 'ibuffer-hook
             (lambda ()
               (setq ibuffer-vc-set-filter-groups-by-vc-root t)
-              (setq ibuffer-tramp-set-filter-groups-by-tramp-connection t)
-              ;; (unless (eq ibuffer-sorting-mode 'alphabetic)
-              ;;   (ibuffer-do-sort-by-alphabetic))
-              )))
+              (setq ibuffer-tramp-set-filter-groups-by-tramp-connection t))))
