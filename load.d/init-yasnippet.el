c8b19e19918a5ea4ea0027ceb7c51dbb087f0e39
Tjaart van der Walt <git@tjaart.co.za>
2017-08-11 20:55:49 -0700

    load.d/init-yasnippet.el

--- load.d/init-yasnippet.el
+++ load.d/init-yasnippet.el
@@ -1,10 +1,5 @@
 ;; custom snippets go in ~/.emac.d/snippets
 
 (use-package yasnippet
-  ;; :require (auto-yasnippet java-snippets react-snippets)
-
-  :config
-  (yas-global-mode 1)
-  (define-key yas-minor-mode-map (kbd "<tab>") nil)
-  (define-key yas-minor-mode-map (kbd "TAB") nil)
-  )
+  :bind(:map yas-minor-mode-map ("TAB" . nil))
+  :config (yas-global-mode 1))
