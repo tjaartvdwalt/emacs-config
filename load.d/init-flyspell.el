78ebe9c76cf3aa9a753b0e4982d0cd4d0703c9c9
Tjaart van der Walt <git@tjaart.co.za>
2017-08-12 09:54:54 -0700

    load.d/init-flyspell.el

--- load.d/init-flyspell.el
+++ load.d/init-flyspell.el
@@ -1,8 +1,3 @@
-(use-package flyspell
-  :diminish flyspell-mode
-  :config
-  (add-hook 'text-mode-hook 'flyspell-mode)
-  (use-package flyspell-correct-ivy))
 
 (provide 'init-flyspell)
 ;;; init-flyspell ends here
