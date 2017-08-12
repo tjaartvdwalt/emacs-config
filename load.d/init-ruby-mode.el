f6cd5606a847ae933c2142205b0a44a62b889d4f
Tjaart van der Walt <git@tjaart.co.za>
2017-08-11 19:11:13 -0700

    load.d/init-ruby-mode.el

--- load.d/init-ruby-mode.el
+++ load.d/init-ruby-mode.el
@@ -1,10 +1,10 @@
 (use-package ruby-mode
   :config
-    (add-hook 'ruby-mode-hook 'eldoc-mode)
+  (add-hook 'ruby-mode-hook 'eldoc-mode)
   (add-to-list 'ac-modes 'ruby-mode)
   (add-to-list 'auto-mode-alist
                '("\\(?:\\.rb\\|ru\\|rake\\|thor\\|jbuilder\\|gemspec\\|podspec\\|/\\(?:Gem\\|Rake\\|Cap\\|Thor\\|Vagrant\\|Guard\\|Pod\\)file\\)\\'" . ruby-mode))
-  
+
   (use-package inf-ruby)
   (use-package chruby)
   (use-package rspec-mode)
