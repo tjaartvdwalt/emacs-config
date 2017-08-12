7c179fd368589cda93571d1961831b7fabce0bbd
Tjaart van der Walt <git@tjaart.co.za>
2017-08-11 22:49:49 -0700

    load.d/init-web-mode.el

--- load.d/init-web-mode.el
+++ load.d/init-web-mode.el
@@ -1,6 +1,6 @@
 (use-package web-mode
   ;; :require (yasnippet flycheck auto-complete)
-  :mode "\\.html?\\"
+  :mode "\\.html?\\'"
   :config
   (setq web-mode-enable-css-colorization t)
   (setq web-mode-enable-auto-pairing t)
