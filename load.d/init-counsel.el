fadd9ab736fa9c3ba6e776da603a862b6a27627e
Tjaart van der Walt <git@tjaart.co.za>
2017-08-13 17:44:31 -0700

    load.d/init-counsel.el

--- load.d/init-counsel.el
+++ load.d/init-counsel.el
@@ -1,22 +1,22 @@
 (use-package counsel :ensure t
   :bind*
   (("M-x"     . counsel-M-x)
-   ("<f1> c"   . counsel-descbinds)
-   ("<f1> f"   . counsel-describe-function)
-   ("<f1> v"   . counsel-describe-variable)
-   ("<f1> l"   . counsel-find-library)
-   ("<f1> s"   . describe-symbol)
-   ("<f2> i"   . counsel-info-lookup-symbol)
-   ("<f2> u"   . counsel-unicode-char)
-   ("M-y"     . counsel-yank-pop)
-   ("C-x C-f" . counsel-find-file)
-   ("C-x r f" . counsel-recentf)
-   ("C-c g" . counsel-git)
-   ("C-c j" . counsel-git-grep)
-   ("C-c k"   . counsel-ag)
-   ("C-c r"   . counsel-rhythmbox)
-   ("C-x l"   . counsel-locate)
-;;   ("C-c m" . counsel-imenu)            
+    ("<f1> c"   . counsel-descbinds)
+    ("<f1> f"   . counsel-describe-function)
+    ("<f1> v"   . counsel-describe-variable)
+    ("<f1> l"   . counsel-find-library)
+    ("<f1> s"   . describe-symbol)
+    ("<f2> i"   . counsel-info-lookup-symbol)
+    ("<f2> u"   . counsel-unicode-char)
+    ("M-y"     . counsel-yank-pop)
+    ("C-x C-f" . counsel-find-file)
+    ("C-x r f" . counsel-recentf)
+    ("C-c g" . counsel-git)
+    ("C-c j" . counsel-git-grep)
+    ("C-c k"   . counsel-ag)
+    ("C-c r"   . counsel-rhythmbox)
+    ("C-x l"   . counsel-locate)
+    ;;   ("C-c m" . counsel-imenu)
     )
 
   :config
@@ -27,24 +27,24 @@
   (defun given-file (cmd prompt) ; needs lexical-binding
     (lambda (source)
       (let ((target
-         (let ((enable-recursive-minibuffers t))
-           (read-file-name
-        (format "%s %s to:" prompt source)))))
-    (funcall cmd source target 1))))
+              (let ((enable-recursive-minibuffers t))
+                (read-file-name
+                  (format "%s %s to:" prompt source)))))
+        (funcall cmd source target 1))))
   (defun confirm-delete-file (x)
     (dired-delete-file x 'confirm-each-subdirectory))
 
   (ivy-add-actions
-   'counsel-find-file
-   `(("c" ,(given-file #'copy-file "Copy") "copy")
-     ("d" ,(reloading #'confirm-delete-file) "delete")
-     ("m" ,(reloading (given-file #'rename-file "Move")) "move")))
+    'counsel-find-file
+    `(("c" ,(given-file #'copy-file "Copy") "copy")
+       ("d" ,(reloading #'confirm-delete-file) "delete")
+       ("m" ,(reloading (given-file #'rename-file "Move")) "move")))
   (ivy-add-actions
-   'counsel-projectile-find-file
-   `(("c" ,(given-file #'copy-file "Copy") "copy")
-     ("d" ,(reloading #'confirm-delete-file) "delete")
-     ("m" ,(reloading (given-file #'rename-file "Move")) "move")
-     ("b" counsel-find-file-cd-bookmark-action "cd bookmark")))
+    'counsel-projectile-find-file
+    `(("c" ,(given-file #'copy-file "Copy") "copy")
+       ("d" ,(reloading #'confirm-delete-file) "delete")
+       ("m" ,(reloading (given-file #'rename-file "Move")) "move")
+       ("b" counsel-find-file-cd-bookmark-action "cd bookmark")))
 
   ;; to make counsel-ag search the root projectile directory.
   (defun counsel-ag-projectile ()
@@ -55,7 +55,14 @@
   ;; ignore . files or temporary files
   (setq counsel-find-file-ignore-regexp
     (concat
-     ;; File names beginning with # or .
-     "\\(?:\\`[#.]\\)"
-     ;; File names ending with # or ~
-     "\\|\\(?:\\`.+?[#~]\\'\\)")))
+      ;; File names beginning with # or .
+      "\\(?:\\`[#.]\\)"
+      ;; File names ending with # or ~
+      "\\|\\(?:\\`.+?[#~]\\'\\)"))
+
+  (use-package counsel-projectile :ensure t
+    :bind (("C-c p p" . counsel-projectile)
+            ("C-c p s" . nil)
+            ("C-c p s" . counsel-projectile-drop-to-switch-project-binding))
+    :init
+    (counsel-projectile-on)))
