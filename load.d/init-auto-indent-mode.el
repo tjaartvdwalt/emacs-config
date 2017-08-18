ec74ab785cb3411592dd308412164ef29c80ad17
Tjaart van der Walt <git@tjaart.co.za>
2017-08-18 09:50:00 -0700

    load.d/init-auto-indent-mode.el

--- load.d/init-auto-indent-mode.el
+++ load.d/init-auto-indent-mode.el
@@ -1,5 +1,5 @@
 (use-package auto-indent-mode
   :config
   (add-hook 'prog-mode-hook
-           (lambda ()
-         (add-hook 'before-save-hook 'indent-whole-buffer))))
+    (lambda ()
+      (add-hook 'after-save-hook 'indent-whole-buffer t t))))
