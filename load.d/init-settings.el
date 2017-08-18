02ec7e298ce48924a21d0a8d889b6ad3b5a170d1
Tjaart van der Walt <git@tjaart.co.za>
2017-08-18 09:12:32 -0700

    load.d/init-settings.el

--- load.d/init-settings.el
+++ load.d/init-settings.el
@@ -8,7 +8,7 @@
 ;; Dont show the splash screen
 (setq inhibit-splash-screen t)
 (setq tab-width 2)
-;; (setq-default indent-tabs-mode nil)
+;;(setq-default indent-tabs-mode nil)
 ;; (global-linum-mode t)
 
 
