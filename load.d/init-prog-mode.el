b1c398ecbc3a522855c824de6975da7e58f64d45
Tjaart van der Walt <git@tjaart.co.za>
2017-08-24 16:22:30 -0700

    load.d/init-prog-mode.el

--- load.d/init-prog-mode.el
+++ load.d/init-prog-mode.el
@@ -1,3 +1,9 @@
-(add-hook 'prog-mode-hook
-  (lambda ()
-    (add-hook 'before-save-hook 'indent-whole-buffer t t)))
+(remove-hook HOOK FUNCTION &optional LOCAL)
+
+
+
+
+(defun auto-indent-on-save ()
+  (add-hook 'prog-mode-hook
+	    (lambda ()
+	      (add-hook 'before-save-hook 'indent-whole-buffer t t))))
