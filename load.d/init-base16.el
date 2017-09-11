6c3df77d6c50dd78082cb424924c8a52cf9f24c4
Tjaart van der Walt <git@tjaart.co.za>
2017-09-11 08:08:36 -0700

    load.d/init-base16.el

--- load.d/init-base16.el
+++ load.d/init-base16.el
@@ -1,8 +1,8 @@
 (elpa-install 'base16-theme)
 
-(if (display-graphic-p)
+;; (if (display-graphic-p)
   (load-theme 'base16-solarized-light t)
-  (load-theme 'wheatgrass t))
+  ;; (load-theme 'wheatgrass t))
 
 ;; (use-package base16-theme
 ;;   ;; :ensure (base16-google-dark)
