0e71b96e1240b70992d784f38079d565dc97ee8a
Tjaart van der Walt <git@tjaart.co.za>
2017-11-09 18:14:10 -0800

    load.d/init-themes.el

--- load.d/init-themes.el
+++ load.d/init-themes.el
@@ -1,7 +1,5 @@
-(use-package base16-theme)
-
 (use-package color-theme-sanityinc-solarized
   :config
   (progn
-    (set-cursor-color "Magenta")
-    (load-theme 'sanityinc-solarized-dark t))
+    (load-theme 'sanityinc-solarized-dark t)
+    (set-cursor-color "magenta")))
