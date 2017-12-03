00fe771eb434c2260a20c161dcefc969f2e10ca0
Tjaart van der Walt <git@tjaart.co.za>
2017-12-02 17:54:36 -0800

    load.d/init-markdown-mode.el

--- load.d/init-markdown-mode.el
+++ load.d/init-markdown-mode.el
@@ -6,3 +6,14 @@
 
 (provide 'init-markdown)
 ;;; init-markdown ends here
+
+(defun orgtbl-to-kramdown (table params)
+  "Convert the Orgtbl mode TABLE to Kramdown."
+  (let* ((alignment (mapconcat (lambda (x) (if x "|--:" "|---"))
+                               org-table-last-alignment ""))
+         (params2
+          (list
+           :splice t
+	   :hline (concat alignment "|")
+           :lstart "| " :lend " |" :sep " | ")))
+    (orgtbl-to-generic table (org-combine-plists params2 params))))
