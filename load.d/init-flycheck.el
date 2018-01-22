954c57e6d490185bb0d363e58ce5778acc8b2c16
Tjaart van der Walt <git@tjaart.co.za>
2018-01-21 22:22:02 -0800

    load.d/init-flycheck.el

--- load.d/init-flycheck.el
+++ load.d/init-flycheck.el
@@ -4,7 +4,7 @@
   (add-hook 'after-init-hook 'global-flycheck-mode)
   (add-hook 'javascript-mode-hook 'flycheck-mode)
 
-  (defcustom 'flycheck-flake8-maximum-line-length 80)
+  (setq flycheck-flake8-maximum-line-length 80)
   ;; set javascript standard checekr as default
   (setq-default flycheck-disabled-checkers
     (append flycheck-disabled-checkers
