f94151a62f7f9a5135574274ed9beff2b6948278
Tjaart van der Walt <git@tjaart.co.za>
2018-01-21 20:49:31 -0800

    load.d/init-python.el

--- load.d/init-python.el
+++ load.d/init-python.el
@@ -5,24 +5,24 @@
   :interpreter ("python" . python-mode)
   :config
   (progn
-  ;; sudo pip install isort
-  (use-package py-isort
-    :config(add-hook 'before-save-hook 'py-isort-before-save))
+    ;; sudo pip install isort
+    (use-package py-isort
+      :config(add-hook 'before-save-hook 'py-isort-before-save))
 
-  ;; sudo pip install autopep8
-  (use-package py-autopep8
-    ;; :require(python-mode)
-    :config (add-hook 'python-mode-hook 'py-autopep8-enable-on-save))
+    ;; sudo pip install autopep8
+    (use-package py-autopep8
+      ;; :require(python-mode)
+      :config (add-hook 'python-mode-hook 'py-autopep8-enable-on-save))
 
-  (use-package jedi
-    :config
-    (add-hook 'python-mode-hook 'jedi:setup)
-    (setq jedi:complete-on-dot t))
+    (use-package jedi
+      :config
+      (add-hook 'python-mode-hook 'jedi:setup)
+      (setq jedi:complete-on-dot t))
 
-  (add-hook 'python-mode-hook
-    (lambda ()
-      (subword-mode 1)))    )
-)
+    (add-hook 'python-mode-hook
+      (lambda ()
+        (subword-mode 1))))
+  )
 
 (provide 'init-python)
 ;;; init-python ends here
