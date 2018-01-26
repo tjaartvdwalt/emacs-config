884c53bc141f86500fb96ef07571668f9ef9dc11
Tjaart van der Walt <git@tjaart.co.za>
2018-01-25 17:13:50 -0800

    load.d/init-python.el

--- load.d/init-python.el
+++ load.d/init-python.el
@@ -17,8 +17,7 @@
 
     (use-package pydoc)
 
-    (use-package python-docstring
-      )
+    (use-package python-docstring)
     (use-package jedi
       :config
       (add-hook 'python-mode-hook 'jedi:setup)
