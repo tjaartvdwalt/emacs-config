7cf0f831faf63f146cc7500205e3c6a3fe63fec8
Tjaart van der Walt <git@tjaart.co.za>
2018-01-26 07:26:11 -0800

    init.el

--- init.el
+++ init.el
@@ -25,10 +25,18 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+  '(ansi-color-faces-vector
+     [default bold shadow italic underline bold bold-italic bold])
+  '(ansi-color-names-vector
+     (vector "#839496" "#dc322f" "#859900" "#b58900" "#268bd2" "#d33682" "#2aa198" "#eee8d5"))
  '(bmkp-last-as-first-bookmark-file "~/.emacs.d/bookmarks")
  '(browse-url-dwim-mode t)
  '(column-number-mode t)
  '(custom-enabled-themes (quote (sanityinc-solarized-dark)))
+  '(custom-safe-themes
+     (quote
+       ("4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" default)))
+ '(fci-rule-color "#073642")
  '(gac-automatically-push-p t)
  '(global-auto-complete-mode t)
  '(keep-end-mode t)
@@ -43,7 +51,29 @@
  '(size-indication-mode t)
  '(tdd-test-function (smart-compile))
  '(tool-bar-mode nil)
- '(uniquify-buffer-name-style (quote forward) nil (uniquify)))
+ '(uniquify-buffer-name-style (quote forward) nil (uniquify))
+ '(vc-annotate-background nil)
+  '(vc-annotate-color-map
+     (quote
+       ((20 . "#dc322f")
+         (40 . "#cb4b16")
+         (60 . "#b58900")
+         (80 . "#859900")
+         (100 . "#2aa198")
+         (120 . "#268bd2")
+         (140 . "#d33682")
+         (160 . "#6c71c4")
+         (180 . "#dc322f")
+         (200 . "#cb4b16")
+         (220 . "#b58900")
+         (240 . "#859900")
+         (260 . "#2aa198")
+         (280 . "#268bd2")
+         (300 . "#d33682")
+         (320 . "#6c71c4")
+         (340 . "#dc322f")
+         (360 . "#cb4b16"))))
+ '(vc-annotate-very-old-color nil))
 
 ;; (set-frame-font "Cousine-13" nil t)
 ;;(set-frame-font "Source Code Pro-13" nil t)
