3562507c81f19a059882091cee4a0d5ca637e853
Tjaart van der Walt <git@tjaart.co.za>
2018-01-26 07:40:06 -0800

    load.d/init-themes.el

--- load.d/init-themes.el
+++ load.d/init-themes.el
@@ -3,6 +3,6 @@
   :init
   (progn
     (add-hook 'window-setup-hook '(lambda () (set-cursor-color "magenta")))
-  (add-hook 'after-make-frame-functions '(lambda (f) (with-selected-frame f (set-cursor-color "magenta"))))
+  ;; (add-hook 'after-make-frame-functions '(lambda (f) (with-selected-frame f (set-cursor-color "magenta"))))
 
   (load-theme 'sanityinc-solarized-dark t)))
