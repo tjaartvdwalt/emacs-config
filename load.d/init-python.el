1ce89b22d20e0ee0a13fc891db735b3bd32497ba
Tjaart van der Walt <git@tjaart.co.za>
2018-01-27 19:04:14 -0800

    load.d/init-python.el

--- load.d/init-python.el
+++ load.d/init-python.el
@@ -15,44 +15,45 @@
 
   ;; sudo pip install autopep8
   (defun pepify ()
-    (py-isort-buffer)
-    (py-autopep8))
+    )
 
   (use-package py-autopep8
     :ensure py-isort
     :ensure-system-package autopep8
     :bind (:map python-mode-map
-            ("C-c i" . pepify))
-    :config
-    (setq py-autopep8-options '("--max-line-length=100")))
+            ("C-c i" . (lambda ()
+                         (py-isort-buffer)
+                         (py-autopep8)))
+            :config
+            (setq py-autopep8-options '("--max-line-length=100")))
 
-  (use-package pydoc
-    :bind (:map python-mode-map
-            ("C-c d" . pydoc-at-point)))
+    (use-package pydoc
+      :bind (:map python-mode-map
+              ("C-c d" . pydoc-at-point)))
 
-  (use-package tox
-    :ensure-system-package tox
-    :bind (:map python-mode-map
-            ("C-c t" . tox-current-test)
-            :map python-mode-map
-            ("C-c c" . tox-current-class)))
+    (use-package tox
+      :ensure-system-package tox
+      :bind (:map python-mode-map
+              ("C-c t" . tox-current-test)
+              :map python-mode-map
+              ("C-c c" . tox-current-class)))
 
-  (use-package sphinx-doc
-    :delight
-    :config
-    (add-hook 'python-mode-hook (lambda ()
-                                  (sphinx-doc-mode t))))
+    (use-package sphinx-doc
+      :delight
+      :config
+      (add-hook 'python-mode-hook (lambda ()
+                                    (sphinx-doc-mode t))))
 
-  (use-package highlight-indentation
-    :config
-    (add-hook 'python-mode-hook (lambda ()
-                                  (highlight-indentation-mode t)
-                                  (highlight-indentation-current-column-mode t))))
+    (use-package highlight-indentation
+      :config
+      (add-hook 'python-mode-hook (lambda ()
+                                    (highlight-indentation-mode t)
+                                    (highlight-indentation-current-column-mode t))))
 
-  (use-package jedi
-    :config
-    (add-hook 'python-mode-hook 'jedi:setup)
-    (setq jedi:complete-on-dot t)))
+    (use-package jedi
+      :config
+      (add-hook 'python-mode-hook 'jedi:setup)
+      (setq jedi:complete-on-dot t)))
 
-(provide 'init-python)
+  (provide 'init-python)
 ;;; init-python ends here
