cafb6cd5e11a52242992ece04eeafcb4f859905b
Tjaart van der Walt <git@tjaart.co.za>
2018-01-27 21:15:21 -0800

    load.d/init-auto-complete.el

--- load.d/init-auto-complete.el
+++ load.d/init-auto-complete.el
@@ -4,11 +4,11 @@
   :config
   (setq ac-use-fuzzy t)
   (setq-default ac-sources
-                '(ac-source-yasnippet
-                  ac-source-abbrev
-                  ac-source-dictionary
-                  ac-source-filename
-                  ac-source-words-in-same-mode-buffers))
+    '(ac-source-yasnippet
+       ac-source-abbrev
+       ac-source-dictionary
+       ac-source-filename
+       ac-source-words-in-same-mode-buffers))
   (global-auto-complete-mode 1)
   (ac-flyspell-workaround)
 
@@ -19,15 +19,11 @@
 
   (use-package ac-emoji
     :hook ((markdown-mode . ac-emoji-setup)
-            (git-commit-mode . ac-emoji-setup))
-    :config
-    (add-hook 'markdown-mode-hook 'ac-emoji-setup)
-    (add-hook 'git-commit-mode-hook 'ac-emoji-setup))
+            (git-commit-mode . ac-emoji-setup)))
 
   (use-package ac-emmet
-    :config
-    (add-hook 'sgml-mode-hook 'ac-emmet-html-setup)
-    (add-hook 'css-mode-hook 'ac-emmet-css-setup)))
+    :hook ((sgml-mode . ac-emmet-html-setup)
+            (css-mode . ac-emmet-css-setup))))
 
 ;; (setq ac-auto-start nil)
 ;; (define-key ac-mode-map (kbd "TAB") 'auto-complete)
