0126bba64dd21f4fbe55b2cdf4c0c03d5b5d3a1a
Tjaart van der Walt <git@tjaart.co.za>
2018-01-27 21:34:53 -0800

    load.d/init-scss-mode.el

--- load.d/init-scss-mode.el
+++ load.d/init-scss-mode.el
@@ -1,9 +1,5 @@
 ;; you need to install scss lint
 ;; gem install scss_lint
 (use-package scss-mode
-  :config(progn
-           ;; (setq css-indent-offset 2)
-           (flycheck-add-mode 'scss 'scss-mode)
-
-           (add-hook 'scss-mode-hook
-                     (add-hook 'after-save-hook 'scssfmt t t))))
+  :hook (scss-mode . (add-hook 'after-save-hook 'scssfmt t t))
+  :config (flycheck-add-mode 'scss 'scss-mode))
