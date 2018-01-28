f44bc911020977bb0d3801e78e8f6356091b2045
Tjaart van der Walt <git@tjaart.co.za>
2018-01-27 18:12:58 -0800

    load.d/init-python.el

--- load.d/init-python.el
+++ load.d/init-python.el
@@ -8,44 +8,44 @@
           :map python-mode-map
           ("C-c d" . pydoc-at-point))
   :config
-    ;; sudo pip install isort
-    (use-package py-isort
-      :config(add-hook 'before-save-hook 'py-isort-before-save))
+  ;; sudo pip install isort
+  (use-package py-isort
+    :config(add-hook 'before-save-hook 'py-isort-before-save))
 
-    ;; sudo pip install autopep8
-    (use-package py-autopep8
-      :ensure-system-package autopep8
-      :config
-      (setq py-autopep8-options '("--max-line-length=100")))
+  ;; sudo pip install autopep8
+  (use-package py-autopep8
+    :ensure-system-package autopep8
+    :config
+    (setq py-autopep8-options '("--max-line-length=100")))
 
-    (use-package pydoc
+  (use-package pydoc
 
-      )
+    )
 
-    (use-package tox
-      :ensure-system-package tox)
+  (use-package tox
+    :ensure-system-package tox)
 
 
-    (use-package sphinx-doc
-      :config
-      (add-hook 'python-mode-hook (lambda ()
-                                    (require 'sphinx-doc)
-                                    (sphinx-doc-mode t))))
+  (use-package sphinx-doc
+    :config
+    (add-hook 'python-mode-hook (lambda ()
+                                  (require 'sphinx-doc)
+                                  (sphinx-doc-mode t))))
 
-    (use-package highlight-indentation
-      :config
-      (add-hook 'python-mode-hook (lambda ()
-                                    (highlight-indentation-mode t)
-                                    (highlight-indentation-current-column-mode t))))
+  (use-package highlight-indentation
+    :config
+    (add-hook 'python-mode-hook (lambda ()
+                                  (highlight-indentation-mode t)
+                                  (highlight-indentation-current-column-mode t))))
 
-    (use-package jedi
-      :config
-      (add-hook 'python-mode-hook 'jedi:setup)
-      (setq jedi:complete-on-dot t))
-    (setq py-smart-indentation t)
-    (add-hook 'python-mode-hook
-      (lambda ()
-        (subword-mode 1))))
+  (use-package jedi
+    :config
+    (add-hook 'python-mode-hook 'jedi:setup)
+    (setq jedi:complete-on-dot t))
+  (setq py-smart-indentation t)
+  (add-hook 'python-mode-hook
+    (lambda ()
+      (subword-mode 1))))
 
 (provide 'init-python)
 ;;; init-python ends here
