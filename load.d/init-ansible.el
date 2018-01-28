81bed30e3fcf6cb092f88c54d33255622d399d52
Tjaart van der Walt <git@tjaart.co.za>
2018-01-27 20:56:52 -0800

    load.d/init-ansible.el

--- load.d/init-ansible.el
+++ load.d/init-ansible.el
@@ -1,13 +1,13 @@
-(use-package ansible
-  :hook (yaml-mode . (lambda ()
-                       (interactive)
-                       (ansible 1))))
-;; :config
-;; (add-hook 'yaml-mode-hook '(lambda () (ansible 1)))
+;; (use-package ansible
+;;   :hook (yaml-mode . (lambda ()
+;;                        (interactive)
+;;                        (ansible 1))))
+;; ;; :config
+;; ;; (add-hook 'yaml-mode-hook '(lambda () (ansible 1)))
 
-(use-package ansible-doc
-  :bind (:map yaml-mode-map
-          ("C-c d" . pydoc-at-point))
-  :hook (yaml-mode . ansible-doc-mode))
-;; :config
-;; (add-hook 'yaml-mode-hook #'ansible-doc-mode)))
+;; (use-package ansible-doc
+;;   :bind (:map yaml-mode-map
+;;           ("C-c d" . pydoc-at-point))
+;;   :hook (yaml-mode . ansible-doc-mode))
+;; ;; :config
+;; ;; (add-hook 'yaml-mode-hook #'ansible-doc-mode)))
