ba35d330eddadc6715c9a74d0d89eb796f984918
Tjaart van der Walt <git@tjaart.co.za>
2018-01-27 19:04:46 -0800

    load.d/init-python.el

--- load.d/init-python.el
+++ load.d/init-python.el
@@ -19,7 +19,7 @@
     :bind (:map python-mode-map
             ("C-c i" . (lambda ()
                          (py-isort-buffer)
-                         (py-autopep8)))
+                         (py-autopep8))))
             :config
             (setq py-autopep8-options '("--max-line-length=100")))
 
