57902d0c3caf56768b8517ca014b102aa8846817
Tjaart van der Walt <git@tjaart.co.za>
2018-01-27 21:29:42 -0800

    load.d/init-rainbow-identifiers.el

--- load.d/init-rainbow-identifiers.el
+++ load.d/init-rainbow-identifiers.el
@@ -2,6 +2,7 @@
 ;;; Commentary:
 ;;; Code:
 (use-package rainbow-identifiers
+  :hook (prog-mode . rainbow-identifiers-mode)
   :config
   (progn
     (add-hook 'prog-mode-hook #'rainbow-identifiers-mode)))
