695fafb664699f20f327c2ad7053234c05806cad
Tjaart van der Walt <git@tjaart.co.za>
2018-01-30 10:22:56 -0800

    load.d/init-multiple-cursors.el

--- load.d/init-multiple-cursors.el
+++ load.d/init-multiple-cursors.el
@@ -1,4 +1,4 @@
 (use-package multiple-cursors
   :bind (("C-c m d" . mc/mark-all-dwim)
           ("C-c m l" . mc/mark-all-like-this)
-          ("C-c m l" . mc/mark-next-like-this)))
+          ("C-c m m" . vr/mc-mark)))
