b9d4459d2098d5fd33c8604242f17601fcf7de0e
Tjaart van der Walt <git@tjaart.co.za>
2018-01-30 16:54:53 -0800

    load.d/init-python.el

--- load.d/init-python.el
+++ load.d/init-python.el
@@ -10,7 +10,7 @@
 
   (use-package py-autopep8
     :ensure py-isort
-    :ensure-system-package (python-isort python-pycodestyle autopep8)
+    :ensure-system-package (python-isort autopep8)
     :bind (:map python-mode-map
             ("C-c i" .   (lambda ()
                            (interactive)
