c2f84f5f406554a062b11f389282fcc01879804f
Tjaart van der Walt <git@tjaart.co.za>
2018-03-23 10:51:53 -0700

    load.d/init-dired-sidebar.el

---
+++ load.d/init-dired-sidebar.el
@@ -0,0 +1,8 @@
+(use-package dired-sidebar
+  :ensure t
+  :commands (dired-sidebar-toggle-sidebar)
+  :config
+  (use-package all-the-icons-dired
+    ;; M-x all-the-icons-install-fonts
+    :ensure t
+    :commands (all-the-icons-dired-mode)))
