407d04e9a23e329a73d7c055d4a144e6346e4540
Tjaart van der Walt <git@tjaart.co.za>
2018-03-23 11:22:31 -0700

    load.d/init-git-commit-insert-issue.el

--- load.d/init-git-commit-insert-issue.el
+++ load.d/init-git-commit-insert-issue.el
@@ -1,9 +1,4 @@
-;; for development, use my local repos
-
-;;(load-dir-one "/home/tjaart/code/git-commit-insert-issue")
-;;
-;;(add-hook 'git-commit-mode-hook 'git-commit-insert-issue-mode)
-
-;; (use-package git-commit-insert-issue
-;;   :config(progn
-;;            (add-hook 'git-commit-mode-hook 'git-commit-insert-issue-mode)))
+;; You need to separately configure Bitbucket Github and Gitlab APIs
+(use-package git-commit-insert-issue
+  :config(progn
+           (add-hook 'git-commit-mode-hook 'git-commit-insert-issue-mode)))
