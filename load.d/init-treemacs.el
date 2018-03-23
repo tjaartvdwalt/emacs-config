90743d2a5d9aacbba49811718cdae2e327bc6454
Tjaart van der Walt <git@tjaart.co.za>
2018-03-23 14:54:43 -0700

    load.d/init-treemacs.el

--- load.d/init-treemacs.el
+++ load.d/init-treemacs.el
@@ -2,6 +2,7 @@
   :bind (("C-." . treemacs-toggle))
   :config
   (treemacs-git-mode 'extended)
+  (treemacs-follow-mode)
   (setq treemacs-show-hidden-files nil))
 
 (use-package treemacs-projectile)
