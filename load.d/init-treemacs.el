e5855de8272453121168863ad7272a5e1f8dbbe7
Tjaart van der Walt <git@tjaart.co.za>
2018-03-29 10:50:22 -0700

    load.d/init-treemacs.el

--- load.d/init-treemacs.el
+++ load.d/init-treemacs.el
@@ -2,10 +2,10 @@
   :bind (("C-." . treemacs-toggle))
   :config
   (treemacs-git-mode 'extended)
-  (treemacs-follow-mode t)
-  ;; (treemacs-tag-follow-mode t)
+  ;; (treemacs-follow-mode t)
+  (treemacs-tag-follow-mode t)
   (treemacs-filewatch-mode t)
-  (setq-local imenu-create-index-function #'ggtags-build-imenu-index)
+  ;; (setq-local imenu-create-index-function #'ggtags-build-imenu-index)
   (setq treemacs-show-hidden-files nil
     treemacs-position 'left))
 
