abbb9793c97d0c559e6946d556f2dd31d118626f
Tjaart van der Walt <git@tjaart.co.za>
2018-04-21 18:35:21 -0700

    load.d/init-smart-mode-line.el

--- load.d/init-smart-mode-line.el
+++ load.d/init-smart-mode-line.el
@@ -1,10 +1,13 @@
 (use-package powerline)
-(use-package airline-themes)
+(use-package airline-themes
+  :config
+  (airline-theme-solarized-gui)
+  )
 ;; (use-package smart-mode-line)
-  ;; :config
-  ;; (setq sml/no-confirm-load-theme t)
-  ;; (setq sml/name-width 8)
-  ;; (sml/apply-theme 'light-powerline)
-  ;; (setq sml/mode-width "full")
-  ;; (use-package smart-mode-line-powerline-theme)
-  ;; (sml/setup))
+;; :config
+;; (setq sml/no-confirm-load-theme t)
+;; (setq sml/name-width 8)
+;; (sml/apply-theme 'light-powerline)
+;; (setq sml/mode-width "full")
+;; (use-package smart-mode-line-powerline-theme)
+;; (sml/setup))
