464c86e0c70eb4f8421e4fa06830e0f459ad7fcc
Tjaart van der Walt <git@tjaart.co.za>
2018-04-21 18:43:25 -0700

    load.d/init-smart-mode-line.el

--- load.d/init-smart-mode-line.el
+++ load.d/init-smart-mode-line.el
@@ -2,6 +2,15 @@
 (use-package airline-themes
   :config
   (load-theme 'airline-simple)
+  (setq powerline-utf-8-separator-left        #xe0b0
+      powerline-utf-8-separator-right       #xe0b2
+      airline-utf-glyph-separator-left      #xe0b0
+      airline-utf-glyph-separator-right     #xe0b2
+      airline-utf-glyph-subseparator-left   #xe0b1
+      airline-utf-glyph-subseparator-right  #xe0b3
+      airline-utf-glyph-branch              #xe0a0
+      airline-utf-glyph-readonly            #xe0a2
+      airline-utf-glyph-linenumber          #xe0a1)
   )
 ;; (use-package smart-mode-line)
 ;; :config
