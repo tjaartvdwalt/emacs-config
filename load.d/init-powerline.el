ed5c3d21cef2e85317a749910cbc8106c4bc1189
Tjaart van der Walt <git@tjaart.co.za>
2018-04-21 18:56:18 -0700

    load.d/init-powerline.el

--- load.d/init-powerline.el
+++ load.d/init-powerline.el
@@ -1,16 +1,20 @@
 (use-package powerline)
 (use-package airline-themes
   :config
+
+  (let ((inhibit-mode-name-delight nil))
+    ad-do-it)
+
   (load-theme 'airline-kolor)
   (setq powerline-utf-8-separator-left        #xe0b0
-      powerline-utf-8-separator-right       #xe0b2
-      airline-utf-glyph-separator-left      #xe0b0
-      airline-utf-glyph-separator-right     #xe0b2
-      airline-utf-glyph-subseparator-left   #xe0b1
-      airline-utf-glyph-subseparator-right  #xe0b3
-      airline-utf-glyph-branch              #xe0a0
-      airline-utf-glyph-readonly            #xe0a2
-      airline-utf-glyph-linenumber          #xe0a1)
+    powerline-utf-8-separator-right       #xe0b2
+    airline-utf-glyph-separator-left      #xe0b0
+    airline-utf-glyph-separator-right     #xe0b2
+    airline-utf-glyph-subseparator-left   #xe0b1
+    airline-utf-glyph-subseparator-right  #xe0b3
+    airline-utf-glyph-branch              #xe0a0
+    airline-utf-glyph-readonly            #xe0a2
+    airline-utf-glyph-linenumber          #xe0a1)
   )
 ;; (use-package smart-mode-line)
 ;; :config
