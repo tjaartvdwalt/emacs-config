1de7a48c48fca5522e4dafadc6d61f16bcf67d47
Tjaart van der Walt <git@tjaart.co.za>
2018-04-21 18:53:40 -0700

    load.d/init-powerline.el

---
+++ load.d/init-powerline.el
@@ -0,0 +1,22 @@
+(use-package powerline)
+(use-package airline-themes
+  :config
+  (load-theme 'airline-kolor)
+  (setq powerline-utf-8-separator-left        #xe0b0
+      powerline-utf-8-separator-right       #xe0b2
+      airline-utf-glyph-separator-left      #xe0b0
+      airline-utf-glyph-separator-right     #xe0b2
+      airline-utf-glyph-subseparator-left   #xe0b1
+      airline-utf-glyph-subseparator-right  #xe0b3
+      airline-utf-glyph-branch              #xe0a0
+      airline-utf-glyph-readonly            #xe0a2
+      airline-utf-glyph-linenumber          #xe0a1)
+  )
+;; (use-package smart-mode-line)
+;; :config
+;; (setq sml/no-confirm-load-theme t)
+;; (setq sml/name-width 8)
+;; (sml/apply-theme 'light-powerline)
+;; (setq sml/mode-width "full")
+;; (use-package smart-mode-line-powerline-theme)
+;; (sml/setup))
