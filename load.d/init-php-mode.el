1eedec17fafffc8000d1f66f9ca9a1a86cc8de8e
Tjaart van der Walt <git@tjaart.co.za>
2018-05-05 10:07:16 -0700

    load.d/init-php-mode.el

--- load.d/init-php-mode.el
+++ load.d/init-php-mode.el
@@ -1,6 +1,15 @@
 (use-package php-mode
   :config
   (use-package ac-php)
-  (use-package ede-php-autoload)
+  (use-package ede-php-autoload
+    :config
+    (add-hook 'php-mode-hook '(lambda ()
+                                (setq ac-sources  '(ac-source-php ) )
+                                (define-key php-mode-map  (kbd "C-]") 'ac-php-find-symbol-at-point)   ;goto define
+                                (define-key php-mode-map  (kbd "C-t") 'ac-php-location-stack-back   ) ;go back
+                                ))
+
+    )
+
   (use-package ede-php-autoload-composer-installers)
   (add-hook 'php-mode-hook #'ede-php-autoload-mode))
