06157ab0ca21f31f6b357f3bd6d546b24dc4cdc2
Tjaart van der Walt <git@tjaart.co.za>
2018-05-04 21:27:45 -0700

    load.d/init-php-mode.el

--- load.d/init-php-mode.el
+++ load.d/init-php-mode.el
@@ -1,5 +1,5 @@
 (use-package php-mode
   :config
-(use-package ede-php-autoload)
-(use-package ede-php-autoload-composer-installers)
+  (use-package ede-php-autoload)
+  (use-package ede-php-autoload-composer-installers)
   (add-hook 'php-mode-hook #'ede-php-autoload-mode))
