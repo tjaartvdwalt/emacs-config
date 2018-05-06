5db9bf624bd8816233478549e5c94a03ba5f541d
Tjaart van der Walt <git@tjaart.co.za>
2018-05-06 15:27:16 -0700

    load.d/init-dumb-jump.el

--- load.d/init-dumb-jump.el
+++ load.d/init-dumb-jump.el
@@ -1,5 +1,8 @@
 (use-package dumb-jump
   :hook (prog-mode . dumb-jump-mode)
-  :bind (("C-M-g" . dumb-jump-go)
-          ("C-M-y" . dumb-jump-back))
+    :bind (:map prog-mode-map
+            ("C-M-g" . dumb-jump-go)
+            ("C-M-p" . dumb-jump-back))
+  ;; :bind (("C-M-g" . dumb-jump-go)
+  ;;         ("C-M-y" . dumb-jump-back))
   :config (setq dumb-jump-selector 'ivy))
