e59d4636f02a9bef6daa520f2bd793ea5b2e6a22
Tjaart van der Walt <git@tjaart.co.za>
2018-05-05 18:48:25 -0700

    load.d/init-yasnippet.el

--- load.d/init-yasnippet.el
+++ load.d/init-yasnippet.el
@@ -3,7 +3,7 @@
 ;; ;; (use-package yasnippet-bundle)
 
 (use-package yasnippet
-  :delight)
-;;   :config
-;;   (define-key yas-minor-mode-map (kbd "<tab>") nil)
-;;   (define-key yas-minor-mode-map (kbd "TAB") nil))
+  :delight
+  :config
+  (define-key yas-minor-mode-map (kbd "<tab>") nil)
+  (define-key yas-minor-mode-map (kbd "TAB") nil))
