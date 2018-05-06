ba00b57f7b7bd601ed620f6aed8bdc841f7e8535
Tjaart van der Walt <git@tjaart.co.za>
2018-05-06 16:24:12 -0700

    load.d/init-dumb-jump.el

--- load.d/init-dumb-jump.el
+++ load.d/init-dumb-jump.el
@@ -1,6 +1,6 @@
 (use-package dumb-jump
   :ensure
-  :hook (prog-mode . dumb-jump-mode)
+  ;; :hook (prog-mode . dumb-jump-mode)
     ;; :bind (:map prog-mode-map
     ;;         ("C-M-g" . dumb-jump-go)
     ;;         ("C-M-p" . dumb-jump-back))
