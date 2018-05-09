f92e2ca0e34781ecdc4eb079dc04afa68cd5bb37
Tjaart van der Walt <git@tjaart.co.za>
2018-05-08 17:30:40 -0700

    load.d/init-projectile.el

--- load.d/init-projectile.el
+++ load.d/init-projectile.el
@@ -10,3 +10,21 @@
     (add-to-list 'projectile-other-file-alist '("hbs" "js"))
     ;; (setq projectile-switch-project-action 'projectile-dired)
     ))
+
+(defhydra hydra-projectile (:color teal
+			    :columns 4)
+  "Projectile"
+  ("f"   projectile-find-file                "Find File")
+  ("r"   projectile-recentf                  "Recent Files")
+  ("z"   projectile-cache-current-file       "Cache Current File")
+  ("x"   projectile-remove-known-project     "Remove Known Project")
+
+  ("d"   projectile-find-dir                 "Find Directory")
+  ("b"   projectile-switch-to-buffer         "Switch to Buffer")
+  ("c"   projectile-invalidate-cache         "Clear Cache")
+  ("X"   projectile-cleanup-known-projects   "Cleanup Known Projects")
+
+  ("o"   projectile-multi-occur              "Multi Occur")
+  ("s"   projectile-switch-project           "Switch Project")
+  ("k"   projectile-kill-buffers             "Kill Buffers")
+  ("q"   nil "Cancel" :color blue))
