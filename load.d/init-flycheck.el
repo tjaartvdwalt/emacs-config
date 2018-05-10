5523d45fcef134a32a7ea9e93a9d047e782db432
Tjaart van der Walt <git@tjaart.co.za>
2018-05-10 09:30:22 -0700

    load.d/init-flycheck.el

--- load.d/init-flycheck.el
+++ load.d/init-flycheck.el
@@ -4,14 +4,14 @@
   ;; :hook (flycheck-mode-hook . flycheck-pycheckers-setup)
   :config
   (use-package flycheck-pycheckers
-    :hook (flycheck-mode-hook . flycheck-pycheckers-setup)
+    :hook (flycheck-mode-hook . flycheck-pycheckers-setup))
 
-    (add-hook 'after-init-hook 'global-flycheck-mode)
-    (add-hook 'javascript-mode-hook 'flycheck-mode)
-    ;; (add-hook 'flycheck-mode-hook 'flycheck-pycheckers-setup)
-    (global-flycheck-mode)
-    ;; (setq flycheck-flake8-maximum-line-length 80)
-    ;; set javascript standard checekr as default
-    (setq-default flycheck-disabled-checkers
-      (append flycheck-disabled-checkers
-        '(javascript-jshint)))))
+  (add-hook 'after-init-hook 'global-flycheck-mode)
+  (add-hook 'javascript-mode-hook 'flycheck-mode)
+  ;; (add-hook 'flycheck-mode-hook 'flycheck-pycheckers-setup)
+  (global-flycheck-mode)
+  ;; (setq flycheck-flake8-maximum-line-length 80)
+  ;; set javascript standard checekr as default
+  (setq-default flycheck-disabled-checkers
+    (append flycheck-disabled-checkers
+      '(javascript-jshint))))
