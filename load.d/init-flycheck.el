3ff893b0da3af6090feb3144ff7f8a358184772d
Tjaart van der Walt <git@tjaart.co.za>
2018-05-10 09:05:11 -0700

    load.d/init-flycheck.el

--- load.d/init-flycheck.el
+++ load.d/init-flycheck.el
@@ -4,14 +4,16 @@
   ;; :hook (flycheck-mode-hook . flycheck-pycheckers-setup)
   :config
   (use-package flycheck-pycheckers
-  :hook (flycheck-mode-hook . flycheck-pycheckers-setup))
+    ;; :hook (flycheck-mode-hook . flycheck-pycheckers-setup))
 
-  (add-hook 'after-init-hook 'global-flycheck-mode)
-  (add-hook 'javascript-mode-hook 'flycheck-mode)
-  ;; (add-hook 'flycheck-mode-hook 'flycheck-pycheckers-setup)
-  (global-flycheck-mode )
-  ;; (setq flycheck-flake8-maximum-line-length 80)
-  ;; set javascript standard checekr as default
-  (setq-default flycheck-disabled-checkers
-    (append flycheck-disabled-checkers
-      '(javascript-jshint))))
+    (with-eval-after-load 'flycheck
+      (add-hook 'flycheck-mode-hook #'flycheck-pycheckers-setup))
+    (add-hook 'after-init-hook 'global-flycheck-mode)
+    (add-hook 'javascript-mode-hook 'flycheck-mode)
+    ;; (add-hook 'flycheck-mode-hook 'flycheck-pycheckers-setup)
+    (global-flycheck-mode )
+    ;; (setq flycheck-flake8-maximum-line-length 80)
+    ;; set javascript standard checekr as default
+    (setq-default flycheck-disabled-checkers
+      (append flycheck-disabled-checkers
+        '(javascript-jshint))))
