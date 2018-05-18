3ccda262b5572507ae32e2320d11d9f0e26c8bb7
Tjaart van der Walt <git@tjaart.co.za>
2018-05-18 16:22:26 -0700

    load.d/init-smart-shift.el

--- load.d/init-smart-shift.el
+++ load.d/init-smart-shift.el
@@ -1,5 +1,10 @@
 (use-package smart-shift
-  :bind (("C-M <left>" . smart-shift-left)
+  :bind (("C-M <left>" . nil)
+          ("C-M <right>" . nil)
+          ("C-M <up>" . nil)
+          ("C-M <down>" . nil)
+
+          ("C-M <left>" . smart-shift-left)
           ("C-M <right>" . smart-shift-right)
           ("C-M <up>" . smart-shift-up)
           ("C-M <down>" . smart-shift-down))
