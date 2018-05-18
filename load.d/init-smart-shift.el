6150cfb306b8e82bf6ddc452dc85bd0114b25a51
Tjaart van der Walt <git@tjaart.co.za>
2018-05-18 16:16:10 -0700

    load.d/init-smart-shift.el

--- load.d/init-smart-shift.el
+++ load.d/init-smart-shift.el
@@ -1,7 +1,7 @@
 (use-package smart-shift
-  :bind (("C-c <left>" . smart-shift-left)
-          ("C-c <right>" . smart-shift-right)
-          ("C-c <up>" . smart-shift-up)
-          ("C-c <down>" . smart-shift-down))
+  :bind (("C-M <left>" . smart-shift-left)
+          ("C-M <right>" . smart-shift-right)
+          ("C-M <up>" . smart-shift-up)
+          ("C-M <down>" . smart-shift-down))
   :config
   (global-smart-shift-mode 1))
