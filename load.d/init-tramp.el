52aa2235e30642ec11159e3038109facf71e993d
Tjaart van der Walt <git@tjaart.co.za>
2018-07-12 14:47:20 -0700

    load.d/init-tramp.el

--- load.d/init-tramp.el
+++ load.d/init-tramp.el
@@ -1,5 +1,5 @@
 (setq tramp-default-method "ssh")
 
-(tramp-set-completion-function "ssh"
- '((tramp-parse-sconfig "/etc/ssh_config")
-   (tramp-parse-sconfig "~/.ssh/config")))
+;; (tramp-set-completion-function "ssh"
+;;  '((tramp-parse-sconfig "/etc/ssh_config")
+;;    (tramp-parse-sconfig "~/.ssh/config")))
