2c2788c55b9b2573642495cb867c78bd337e70cd
Tjaart van der Walt <git@tjaart.co.za>
2018-09-11 16:58:29 -0700

    load.d/init-elm-mode.el

--- load.d/init-elm-mode.el
+++ load.d/init-elm-mode.el
@@ -1,2 +1,9 @@
 (use-package elm-mode
-  :hook (flycheck-mode . flycheck-elm-setup))
+  :bind (:map elm-mode-map
+          ("C-c i" .   (lambda ()
+                         (interactive)
+                         (py-isort-buffer)
+                         (py-autopep8))))
+
+                                        ;:hook (flycheck-mode . flycheck-elm-setup)
+  )
