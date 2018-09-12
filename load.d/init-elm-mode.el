735a1d0b861f9795e59f4c14c7d1d2e930308749
Tjaart van der Walt <git@tjaart.co.za>
2018-09-11 17:16:02 -0700

    load.d/init-elm-mode.el

--- load.d/init-elm-mode.el
+++ load.d/init-elm-mode.el
@@ -2,10 +2,12 @@
   :bind (:map elm-mode-map
           ("C-c i" .   (lambda ()
                          (interactive)
-                         (shell-command-on-region pmin pmax
-                           "astyle"
-                           (current-buffer) t
-                           (get-buffer-create "*Astyle Errors*") t))
+                         (setq filename '(buffer-file-name (window-buffer (minibuffer-selected-window))))
+                         (shell-command "fixpack &")
+                         ;; (shell-command-on-region pmin pmax
+                         ;;   "astyle"
+                         ;;   (current-buffer) t
+                         ;;   (get-buffer-create "*Astyle Errors*") t))
             )))
 
 (use-package flycheck-elm
