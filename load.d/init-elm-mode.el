97819ce776e3b54b58e60d04a5be6b1379acd1ad
Tjaart van der Walt <git@tjaart.co.za>
2018-09-11 17:45:59 -0700

    load.d/init-elm-mode.el

--- load.d/init-elm-mode.el
+++ load.d/init-elm-mode.el
@@ -2,10 +2,9 @@
   :bind (:map elm-mode-map
           ("C-c i" .   (lambda ()
                          (interactive)
-                         (setq filename (buffer-file-name (window-buffer (minibuffer-selected-window))))
-                         (setq filename "/home/tjaart/elm-architecture-tutorial/examples/01-button.elm")
+                         ;; (setq filename (buffer-file-name (window-buffer (minibuffer-selected-window))))
+                         ;; (setq filename "/home/tjaart/elm-architecture-tutorial/examples/01-button.elm")
                          (let ((errbuf (get-buffer-create "*Elm Format Errors*")))
-                           (shell-command (concat "elm-format --yes " filename))
                            (call-process "elm-format" nil errbuf nil "--yes" "/home/tjaart/elm-architecture-tutorial/examples/01-button.elm"))
                          ;; (shell-command-on-region pmin pmax
                          ;;   "astyle"
