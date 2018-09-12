1b0f8e249a0c8f971fb0fe82d36911d0e6bedbc0
Tjaart van der Walt <git@tjaart.co.za>
2018-09-11 17:55:10 -0700

    load.d/init-elm-mode.el

--- load.d/init-elm-mode.el
+++ load.d/init-elm-mode.el
@@ -6,14 +6,14 @@
                          ;; (setq filename "/home/tjaart/elm-architecture-tutorial/examples/01-button.elm")
                          (let ((errbuf (get-buffer-create "*Elm Format Errors*")))
                            (if (zerop (call-process "elm-format" nil errbuf nil "--yes" (buffer-file-name)))
-                           (progn
-                             (revert-buffer t t)
-                             (if errbuf (kill-error-buffer errbuf)))
-                           (progn
-                             (revert-buffer t t)
-                             (display-buffer errbuf)
-                             )))))
-  (use-package flycheck-elm
-    :config
-    (eval-after-load 'flycheck
-      '(add-hook 'flycheck-mode-hook #'flycheck-elm-setup)))
+                             (progn
+                               (revert-buffer t t)
+                               (if errbuf (kill-error-buffer errbuf)))
+                             (progn
+                               (revert-buffer t t)
+                               (display-buffer errbuf)
+                               )))))))
+(use-package flycheck-elm
+  :config
+  (eval-after-load 'flycheck
+    '(add-hook 'flycheck-mode-hook #'flycheck-elm-setup)))
