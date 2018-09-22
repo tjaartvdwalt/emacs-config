46cf260587f4436ac37bc6f6ff5d2ff44ce0c610
Tjaart van der Walt <git@tjaart.co.za>
2018-09-22 15:38:24 -0700

    load.d/init-elm-mode.el

--- load.d/init-elm-mode.el
+++ load.d/init-elm-mode.el
@@ -4,15 +4,16 @@
                          (interactive)
                          (let ((errbuf (get-buffer-create "*Elm Format Errors*")))
                            (elm-compile-clean-imports)
+                           (elm-mode-format-buffer)))))
 
-                           (with-current-buffer errbuf
-                             (erase-buffer))
+                           ;; (with-current-buffer errbuf
+                           ;;   (erase-buffer))
 
-                           (save-buffer)
-                           (if (zerop (call-process "elm-format" nil errbuf nil "--yes" (buffer-file-name)))
-                             (progn
-                               (revert-buffer t t)
-                               (if errbuf (kill-error-buffer errbuf)))
-                             (progn
-                               (revert-buffer t t)
-                               (display-buffer errbuf))))))))
+                           ;; (save-buffer)
+                           ;; (if (zerop (call-process "elm-format" nil errbuf nil "--yes" (buffer-file-name)))
+                           ;;   (progn
+                           ;;     (revert-buffer t t)
+                           ;;     (if errbuf (kill-error-buffer errbuf)))
+                           ;;   (progn
+                           ;;     (revert-buffer t t)
+                           ;;     (display-buffer errbuf))))))))
