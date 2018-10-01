6140e8365a9960b313b973b5014bcef613f208d8
Tjaart van der Walt <git@tjaart.co.za>
2018-10-01 12:15:46 -0700

    load.d/init-elm-mode.el

--- load.d/init-elm-mode.el
+++ load.d/init-elm-mode.el
@@ -1,10 +1,15 @@
 (use-package elm-mode
   :bind (:map elm-mode-map
+          ("C-M-g" . jedi:goto-definition)
+          ("C-M-p" . jedi:goto-definition-pop-marker)
           ("C-c i" .   (lambda ()
                          (interactive)
                          (let ((errbuf (get-buffer-create "*Elm Format Errors*")))
                            (elm-compile-clean-imports)
-                           (elm-mode-format-buffer))))))
+                           (elm-mode-format-buffer)))))
+  :config
+  (setq elm-tags-on-save 1)
+  )
 
                            ;; (with-current-buffer errbuf
                            ;;   (erase-buffer))
