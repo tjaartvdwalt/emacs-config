8647d3c3a79280a20fd39e9ff05ed26a0824a1de
Tjaart van der Walt <git@tjaart.co.za>
2018-10-01 16:18:45 -0700

    load.d/init-company.el

--- load.d/init-company.el
+++ load.d/init-company.el
@@ -1,5 +1,6 @@
 (use-package company
-  :hook
-  (after-init-hook . global-company-mode)
+  ;; :hook
+  ;; (after-init-hook . global-company-mode)
   :config
+  (add-hook 'after-init-hook 'global-company-mode)
   (setq company-idle-delay 0))
