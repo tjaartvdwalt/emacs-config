e9d65f944d184329ddd3e7fc9c7c271d9f26e2c2
Tjaart van der Walt <git@tjaart.co.za>
2018-10-01 12:17:16 -0700

    load.d/init-elm-mode.el

--- load.d/init-elm-mode.el
+++ load.d/init-elm-mode.el
@@ -1,24 +1,22 @@
 (use-package elm-mode
   :bind (:map elm-mode-map
-          ("C-M-g" . jedi:goto-definition)
-          ("C-M-p" . jedi:goto-definition-pop-marker)
+          ("C-M-g" . elm-mode-goto-tag-at-point)
+          ("C-M-p" . pop-tag-mark)
           ("C-c i" .   (lambda ()
                          (interactive)
                          (let ((errbuf (get-buffer-create "*Elm Format Errors*")))
                            (elm-compile-clean-imports)
                            (elm-mode-format-buffer)))))
-  :config
-  (setq elm-tags-on-save 1)
-  )
+  :config (setq elm-tags-on-save 1))
 
-                           ;; (with-current-buffer errbuf
-                           ;;   (erase-buffer))
+;; (with-current-buffer errbuf
+;;   (erase-buffer))
 
-                           ;; (save-buffer)
-                           ;; (if (zerop (call-process "elm-format" nil errbuf nil "--yes" (buffer-file-name)))
-                           ;;   (progn
-                           ;;     (revert-buffer t t)
-                           ;;     (if errbuf (kill-error-buffer errbuf)))
-                           ;;   (progn
-                           ;;     (revert-buffer t t)
-                           ;;     (display-buffer errbuf))))))))
+;; (save-buffer)
+;; (if (zerop (call-process "elm-format" nil errbuf nil "--yes" (buffer-file-name)))
+;;   (progn
+;;     (revert-buffer t t)
+;;     (if errbuf (kill-error-buffer errbuf)))
+;;   (progn
+;;     (revert-buffer t t)
+;;     (display-buffer errbuf))))))))
