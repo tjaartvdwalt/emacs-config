4c3bd6ecf1926846b59a6ca2194c7c19a279f87d
Tjaart van der Walt <git@tjaart.co.za>
2018-10-01 16:34:40 -0700

    load.d/init-company.el

--- load.d/init-company.el
+++ load.d/init-company.el
@@ -3,4 +3,12 @@
   ;; (after-init-hook . global-company-mode)
   :config
   (global-company-mode)
+
+    '(company-backends
+     (quote
+       (company-ispell company-abbrev company-bbdb company-eclim company-semantic company-clang company-xcode company-cmake company-capf company-files
+         (company-dabbrev-code company-gtags company-etags company-keywords)
+         company-oddmuse company-dabbrev)))
+
+  (add-to-list 'company-backends 'company-ispell t)
   (setq company-idle-delay 0))
