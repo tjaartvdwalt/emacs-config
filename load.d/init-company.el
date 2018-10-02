cd6c9a26fd9c5781cd27dd914d891f44648816ca
Tjaart van der Walt <git@tjaart.co.za>
2018-10-01 17:18:01 -0700

    load.d/init-company.el

--- load.d/init-company.el
+++ load.d/init-company.el
@@ -10,10 +10,13 @@
     ;;      (company-dabbrev-code company-gtags company-etags company-keywords)
     ;;      company-oddmuse company-dabbrev)))
 
-  (add-to-list 'company-backends 'company-ispell t)
-  (add-to-list 'company-backends 'company-yasnippet t)
-  (setq company-idle-delay 0))
+  (setq company-idle-delay 0)
+  (add-to-list 'company-backends 'company-ispell))
 
 (use-package company-web
   :config
-    (add-to-list 'company-backends 'company-web-html))
+    (add-to-list 'company-backends '(company-web-html company-yasnippet)))
+
+(use-package company-jedi
+:config
+(add-to-list 'company-backends '(company-jedi company-files company-yasnippet)))
