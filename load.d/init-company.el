28471ce2d1cbd9146202c443e124b5cdf9456ca8
Tjaart van der Walt <git@tjaart.co.za>
2018-10-01 17:29:01 -0700

    load.d/init-company.el

--- load.d/init-company.el
+++ load.d/init-company.el
@@ -11,7 +11,16 @@
     ;;      company-oddmuse company-dabbrev)))
 
   (setq company-idle-delay 0)
-  (add-to-list 'company-backends 'company-ispell))
+  (add-to-list 'company-backends 'company-ispell)
+(require 'color)
+
+  (let ((bg (face-attribute 'default :background)))
+    (custom-set-faces
+     '(company-tooltip ((t (:inherit default :background ,(color-lighten-name bg 2)))))
+     '(company-scrollbar-bg ((t (:background ,(color-lighten-name bg 10)))))
+     '(company-scrollbar-fg ((t (:background ,(color-lighten-name bg 5)))))
+     '(company-tooltip-selection ((t (:inherit font-lock-function-name-face))))
+     '(company-tooltip-common ((t (:inherit font-lock-constant-face)))))))
 
 (use-package company-web
   :config
