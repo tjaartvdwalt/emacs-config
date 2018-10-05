b92deedecd1a135f7f993c79a0e76d719ca4cc4a
Tjaart van der Walt <git@tjaart.co.za>
2018-10-05 12:57:43 -0700

    load.d/init-hl-todo.el

--- load.d/init-hl-todo.el
+++ load.d/init-hl-todo.el
@@ -1,11 +1,4 @@
 (use-package hl-todo
-  ;; :bind (:map hl-todo-mode-map
-  ;;         ("M-n" . hl-todo-previous)
-  ;;         ("M-p" . hl-todo-next))
-  ;; :bind (:map prog-mode-map
-  ;;         ("C-M-g" . dumb-jump-go)
-  ;;         ("C-M-c" . dumb-jump-back))
-
   :config
   (define-key hl-todo-mode-map (kbd "M-p") 'hl-todo-previous)
   (define-key hl-todo-mode-map (kbd "M-n") 'hl-todo-next)
