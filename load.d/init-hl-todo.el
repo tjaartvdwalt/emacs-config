fee4776353297aaabbf76d28a0ccc9064ff40448
Tjaart van der Walt <git@tjaart.co.za>
2018-10-05 12:22:39 -0700

    load.d/init-hl-todo.el

--- load.d/init-hl-todo.el
+++ load.d/init-hl-todo.el
@@ -1,7 +1,7 @@
 (use-package hl-todo
-  (:map hl-todo-mode-map
-    ("M-n" . hl-todo-previous)
-    ("M-p" . hl-todo-next))
+  :map hl-todo-mode-map
+  ("M-n" . hl-todo-previous)
+  ("M-p" . hl-todo-next)
 
   :config
   (global-hl-todo-mode))
