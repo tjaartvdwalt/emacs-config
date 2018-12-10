922f8973624d521d30ed5d2604f22422f775eea3
Tjaart van der Walt <git@tjaart.co.za>
2018-12-09 18:11:34 -0800

    load.d/init-evil-nerd-commenter.el

--- load.d/init-evil-nerd-commenter.el
+++ load.d/init-evil-nerd-commenter.el
@@ -3,17 +3,18 @@
   )
 
 (defhydra hydra-commenter ()
-"Evil Nerd Commenter"
-;;   "
-;; _q_: quick comment or uncomment to the line
-;; _c_: copy and comment lines
-;; _p_: comment or uncomment paragraphs
-;; _r_: comment or uncomment region
-;; _v_: toggle invert comment line by line
-;; "
-  (";" evilnc-comment-or-uncomment-lines "comment or uncomment lines")
-  ("q" evilnc-quick-comment-or-uncomment-to-the-line "quick comment or uncomment to the line")
-  ("c" evilnc-copy-and-comment-lines "copy and comment lines")
-  ("p" evilnc-comment-or-uncomment-paragraphs "comment or uncomment paragraphs")
-  ("r" comment-or-uncomment-region "comment or uncomment region")
-  ("v" evilnc-toggle-invert-comment-line-by-line "toggle invert comment line by line"))
+  "
+Evil Nerd Commenter:
+_;_: comment or uncomment lines
+_q_: quick comment or uncomment to the line
+_c_: copy and comment lines
+_p_: comment or uncomment paragraphs
+_r_: comment or uncomment region
+_v_: toggle invert comment line by line
+"
+  (";" evilnc-comment-or-uncomment-lines)
+  ("q" evilnc-quick-comment-or-uncomment-to-the-line)
+  ("c" evilnc-copy-and-comment-lines)
+  ("p" evilnc-comment-or-uncomment-paragraphs)
+  ("r" comment-or-uncomment-region)
+  ("v" evilnc-toggle-invert-comment-line-by-line))
