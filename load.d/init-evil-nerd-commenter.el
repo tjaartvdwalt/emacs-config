5553d30015f0b0851ec7fcbc0a5f92a47e65466a
Tjaart van der Walt <git@tjaart.co.za>
2018-12-09 17:27:06 -0800

    load.d/init-evil-nerd-commenter.el

--- load.d/init-evil-nerd-commenter.el
+++ load.d/init-evil-nerd-commenter.el
@@ -5,47 +5,12 @@
 (defhydra hydra-commenter (:color teal
                             :hint nil)
   "
-     COMMENTER: %(projectile-project-root)
-
-     Find File            Search/Tags          Buffers                Cache
-------------------------------------------------------------------------------------------
-_s-f_: file            _a_: ag                _i_: Ibuffer           _c_: cache clear
- _ff_: file dwim       _g_: update gtags      _b_: switch to buffer  _x_: remove known project
- _fd_: file curr dir   _o_: multi-occur     _s-k_: Kill all buffers  _X_: cleanup non-existing
-  _r_: recent file                                               ^^^^_z_: cache current
-  _d_: dir
-
-"
-  ("ci" evilnc-comment-or-uncomment-lines)
-  ("cl" evilnc-quick-comment-or-uncomment-to-the-line)
-  ("ll" evilnc-quick-comment-or-uncomment-to-the-line)
-  ("cc" evilnc-copy-and-comment-lines)
-  ("cp" evilnc-comment-or-uncomment-paragraphs)
-  ("cr" comment-or-uncomment-region)
-  ("cv" evilnc-toggle-invert-comment-line-by-line)
-  ("."  evilnc-copy-and-comment-operator)
-  ("\\" evilnc-comment-operator)
-
-  ("a"   projectile-ag)
-  ("b"   projectile-switch-to-buffer)
-  ("c"   projectile-invalidate-cache)
-  ("d"   projectile-find-dir)
-  ("s-f" projectile-find-file)
-  ("ff"  projectile-find-file-dwim)
-  ("fd"  projectile-find-file-in-directory)
-  ("g"   ggtags-update-tags)
-  ("s-g" ggtags-update-tags)
-  ("i"   projectile-ibuffer)
-  ("K"   projectile-kill-buffers)
-  ("s-k" projectile-kill-buffers)
-  ("m"   projectile-multi-occur)
-  ("o"   projectile-multi-occur)
-  ("s-p" projectile-switch-project "switch project")
-  ("p"   projectile-switch-project)
-  ("s"   projectile-switch-project)
-  ("r"   projectile-recentf)
-  ("x"   projectile-remove-known-project)
-  ("X"   projectile-cleanup-known-projects)
-  ("z"   projectile-cache-current-file)
-  ("`"   hydra-projectile-other-window/body "other window")
-  ("q"   nil "cancel" :color blue))
+_l_: comment or uncomment lines
+_q_: quick comment or uncomment to the line
+_c_: copy and comment lines
+_p_: comment or uncomment paragraphs
+_r_: comment or uncomment region
+_v_: toggle invert comment line by line
+_._: copy and comment operator
+_\\_: comment operator
+")
