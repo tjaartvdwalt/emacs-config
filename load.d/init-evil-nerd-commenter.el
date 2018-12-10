d9273d19ada69d23eb5dd234d99bc152219e0ccf
Tjaart van der Walt <git@tjaart.co.za>
2018-12-09 18:03:48 -0800

    load.d/init-evil-nerd-commenter.el

--- load.d/init-evil-nerd-commenter.el
+++ load.d/init-evil-nerd-commenter.el
@@ -2,7 +2,7 @@
   :bind ("M-;" . hydra-commenter/body)
   )
 
-(defhydra hydra-commenter (:pre ("\n"))
+(defhydra hydra-commenter (:pre (message "\n"))
 "Evil Nerd Commenter"
 ;;   "
 ;; _q_: quick comment or uncomment to the line
