ce587dcc256da16d7d57cfbde2e5f00c470b1a54
Tjaart van der Walt <git@tjaart.co.za>
2018-12-09 17:49:33 -0800

    load.d/init-evil-nerd-commenter.el

--- load.d/init-evil-nerd-commenter.el
+++ load.d/init-evil-nerd-commenter.el
@@ -4,9 +4,8 @@
 
 (defhydra hydra-commenter (:color teal
                             :hint nil)
-  ;; "Comment"
+"Evil Nerd Commenter"
 ;;   "
-;; _;_: comment or uncomment lines
 ;; _q_: quick comment or uncomment to the line
 ;; _c_: copy and comment lines
 ;; _p_: comment or uncomment paragraphs
@@ -14,8 +13,8 @@
 ;; _v_: toggle invert comment line by line
 ;; "
   (";" evilnc-comment-or-uncomment-lines "comment or uncomment lines")
-  ("q" evilnc-quick-comment-or-uncomment-to-the-line)
-  ("c" evilnc-copy-and-comment-lines)
-  ("p" evilnc-comment-or-uncomment-paragraphs)
-  ("r" comment-or-uncomment-region)
-  ("v" evilnc-toggle-invert-comment-line-by-line))
+  ("q" evilnc-quick-comment-or-uncomment-to-the-line "quick comment or uncomment to the line")
+  ("c" evilnc-copy-and-comment-lines "copy and comment lines")
+  ("p" evilnc-comment-or-uncomment-paragraphs "comment or uncomment paragraphs")
+  ("r" comment-or-uncomment-region "comment or uncomment region")
+  ("v" evilnc-toggle-invert-comment-line-by-line "toggle invert comment line by line"))
