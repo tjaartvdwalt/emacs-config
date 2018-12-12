361fe62418e04d1c89148fca74986f9d2758fc43
Tjaart van der Walt <git@tjaart.co.za>
2018-12-12 15:14:28 -0800

    load.d/init-origami.el

--- load.d/init-origami.el
+++ load.d/init-origami.el
@@ -2,4 +2,18 @@
   ;; :bind (("C-c o c" . origami-close-node)
   ;;        ("C-c o o" . origami-open-node))
 
-  :config (progn (    (add-hook 'prog-mode-hook 'origami-mode))))
+  :config (progn
+            (add-hook 'prog-mode-hook 'origami-mode)
+            (defhydra hydra-folding (:color red)
+              "
+  _o_pen node    _n_ext fold       toggle _f_orward
+  _c_lose node   _p_revious fold   toggle _a_ll
+  "
+              ("o" origami-open-node)
+              ("c" origami-close-node)
+              ("n" origami-next-fold)
+              ("p" origami-previous-fold)
+              ("f" origami-forward-toggle-node)
+              ("a" origami-toggle-all-nodes))
+
+            ))
