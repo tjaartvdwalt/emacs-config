700bbb846bbeeb2f408d73740699f72ee66b34d9
Tjaart van der Walt <git@tjaart.co.za>
2018-12-25 21:29:43 -0800

    load.d/init-js-mode.el

--- load.d/init-js-mode.el
+++ load.d/init-js-mode.el
@@ -11,7 +11,7 @@
 ;; tern must be installed seperately
 ;; on arch linux `yaourt -S nodejs-tern`
 (use-package tern
-  :diminish tern-mode
+  :delight tern-mode
   :config
   (add-hook 'js-mode-hook (lambda () (tern-mode t))))
 
