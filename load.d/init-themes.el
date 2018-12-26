bcf762beac1483ddd96c8f781a7ae59ed80cb79f
Tjaart van der Walt <git@tjaart.co.za>
2018-12-25 21:11:51 -0800

    load.d/init-themes.el

--- load.d/init-themes.el
+++ load.d/init-themes.el
@@ -5,4 +5,5 @@
 ;;   (add-hook 'after-make-frame-functions '(lambda (f) (with-selected-frame f (set-cursor-color "magenta"))))
 ;;   (load-theme 'sanityinc-solarized-dark t))
 
-(use-package dracula-theme)
+(use-package dracula-theme
+  :defer t)
