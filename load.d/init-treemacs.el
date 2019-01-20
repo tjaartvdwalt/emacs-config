bbad6df6478ae8fa928d46a485618d08680a3cf0
Tjaart van der Walt <git@tjaart.co.za>
2019-01-19 16:02:30 -0800

    load.d/init-treemacs.el

--- load.d/init-treemacs.el
+++ load.d/init-treemacs.el
@@ -1,14 +1,14 @@
-(use-package treemacs
-  :ensure t
-  :defer t
-  :bind (("C-." . treemacs-toggle))
-  :config
-  (treemacs-git-mode 'extended)
-  (treemacs-follow-mode t)
-  ;; (treemacs-tag-follow-mode t)
-  (treemacs-filewatch-mode t)
-  ;; (setq-local imenu-create-index-function #'ggtags-build-imenu-index)
-  (setq treemacs-show-hidden-files nil
-    treemacs-position 'left))
+;; (use-package treemacs
+;;   :ensure t
+;;   :defer t
+;;   :bind (("C-." . treemacs-toggle))
+;;   :config
+;;   (treemacs-git-mode 'extended)
+;;   (treemacs-follow-mode t)
+;;   ;; (treemacs-tag-follow-mode t)
+;;   (treemacs-filewatch-mode t)
+;;   ;; (setq-local imenu-create-index-function #'ggtags-build-imenu-index)
+;;   (setq treemacs-show-hidden-files nil
+;;     treemacs-position 'left))
 
-(use-package treemacs-projectile)
+;; (use-package treemacs-projectile)
