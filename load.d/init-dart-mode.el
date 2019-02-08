18f0d658ba93bb5c3fc767be6da4a5308f9359cf
Tjaart van der Walt <git@tjaart.co.za>
2019-02-07 21:17:53 -0800

    load.d/init-dart-mode.el

--- load.d/init-dart-mode.el
+++ load.d/init-dart-mode.el
@@ -1,4 +1,5 @@
 (use-package dart-mode
-  :bind (:map dart-mode-map
-          ("C-c i" .   (lambda ()
-                         (dart-format)))))
+  ;; :bind (:map dart-mode-map
+  ;;         ("C-c i" .   (lambda ()
+  ;;                        (dart-format))))
+  ;; )
