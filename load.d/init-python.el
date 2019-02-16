91eee16cd14b33d4df3e960dcae9c731dd071407
Tjaart van der Walt <git@tjaart.co.za>
2019-02-15 22:36:09 -0800

    load.d/init-python.el

--- load.d/init-python.el
+++ load.d/init-python.el
@@ -9,6 +9,7 @@
   :config
   (setq-default python-indent-guess-indent-offset nil)
   (setq-default python-indent-guess-indent-offset-verbose nil)
+  (setq-default tab-width 4)
   (setq-default python-indent 4)
   (setq-default python-indent-offset 4)
   ;; (setq-default py-smart-indentation t)
