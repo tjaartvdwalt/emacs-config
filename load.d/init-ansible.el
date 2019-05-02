39f0b3f1c824ad4a640eb0621ba6d0f2b6949a3c
Tjaart van der Walt <tjaart@tjaart.org>
2019-05-02 11:37:49 -0700

    load.d/init-ansible.el

--- load.d/init-ansible.el
+++ load.d/init-ansible.el
@@ -1,6 +1,6 @@
-(use-package ansible
- :config
- (add-hook 'yaml-mode-hook '(lambda () (ansible 1)))
+;; (use-package ansible
+;;  :config
+;;  (add-hook 'yaml-mode-hook '(lambda () (ansible 1)))
 
  (use-package ansible-doc
    :config
