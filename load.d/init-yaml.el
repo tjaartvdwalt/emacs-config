b163db241a2eea2c7e1cec1be89c26fc10a2c3ca
Tjaart van der Walt <tjaart@tjaart.org>
2019-05-03 10:18:43 -0700

    load.d/init-yaml.el

--- load.d/init-yaml.el
+++ load.d/init-yaml.el
@@ -1,7 +1,11 @@
 ;;; package --- summary
 ;;; Commentary:
 ;;; Code:
-(use-package yaml-mode)
+(use-package yaml-mode
+  :bind (:map yaml-mode-map
+          ("C-c i" .   prettier-js)
+
+  )
 
 (provide 'init-yaml)
 ;;; init-yaml.el ends here
