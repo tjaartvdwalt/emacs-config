0d2076246dc12a85d82a904d51788182e31f129a
Tjaart van der Walt <tjaart@tjaart.org>
2019-05-16 14:14:35 -0700

    load.d/init-multiple-cursors.el

--- load.d/init-multiple-cursors.el
+++ load.d/init-multiple-cursors.el
@@ -1,13 +1,9 @@
 (use-package multiple-cursors
   :config
-  (defhydra hydra-origami (:color red)
+  (defhydra hydra-multiple-cursors (:color red)
     "
   _o_pen node    _n_ext fold       toggle _f_orward
   _c_lose node   _p_revious fold   toggle _a_ll
   "
-    ("o" origami-open-node)
-    ("c" origami-close-node)
-    ("n" origami-next-fold)
-    ("p" origami-previous-fold)
-    ("f" origami-forward-toggle-node)
-    ("a" origami-toggle-all-nodes)))
+    ("a" mc/edit-beginnings-of-lines)
+    ("e" mc/edit-ends-of-lines)))
