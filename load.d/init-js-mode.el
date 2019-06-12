e93664e8c20e31283f0953bdfa0fde476f7357d7
Tjaart van der Walt <git@tjaart.org>
2019-06-12 13:51:10 -0700

    load.d/init-js-mode.el

--- load.d/init-js-mode.el
+++ load.d/init-js-mode.el
@@ -1,5 +1,5 @@
 (progn
-  ;; (setq js-indent-level 2)
+  (setq js-indent-level 4)
 
   (add-to-list 'auto-mode-alist '("\\.js\\'" . js-mode))
   (add-to-list 'auto-mode-alist '("\\.mjs\\'" . js-mode))
