5d67c030aa93c79c8eeb06e24a45a002f686d9fb
Tjaart van der Walt <git@tjaart.org>
2019-08-05 02:43:43 -0700

    load.d/init-smartparens.el

--- load.d/init-smartparens.el
+++ load.d/init-smartparens.el
@@ -1,9 +1,9 @@
 (use-package smartparens-config
   :ensure smartparens
   :hook ((prog-mode . turn-on-smartparens-strict-mode)
-          (markdown-mode . turn-on-smartparens-strict-mode)))
-:bind ("C-c k" . sp-kill-sexp)
+          (markdown-mode . turn-on-smartparens-strict-mode))
+  :bind ("C-c k" . sp-kill-sexp)
 
-:config
-(show-smartparens-global-mode t)
-(require 'smartparens-config))
+  :config
+  (show-smartparens-global-mode t)
+  (require 'smartparens-config))
