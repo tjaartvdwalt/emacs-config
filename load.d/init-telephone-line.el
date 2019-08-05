b3aa05dd9ec42d2cd5349ae7fa6d2affa175b852
Tjaart van der Walt <git@tjaart.org>
2019-08-05 08:56:30 -0700

    load.d/init-telephone-line.el

--- load.d/init-telephone-line.el
+++ load.d/init-telephone-line.el
@@ -1,8 +1,7 @@
 (use-package telephone-line
-  :config
-  (progn
-	(telephone-line-mode 1)
-	(setq telephone-line-primary-left-separator 'telephone-line-cubed-left
-		  telephone-line-secondary-left-separator 'telephone-line-cubed-hollow-left
-		  telephone-line-primary-right-separator 'telephone-line-cubed-right
-		  telephone-line-secondary-right-separator 'telephone-line-cubed-hollow-right)))
+  :config (progn
+			(telephone-line-mode 1)
+			(setq telephone-line-primary-left-separator 'telephone-line-cubed-left
+				  telephone-line-secondary-left-separator 'telephone-line-cubed-hollow-left
+				  telephone-line-primary-right-separator 'telephone-line-cubed-right
+				  telephone-line-secondary-right-separator 'telephone-line-cubed-hollow-right)))
