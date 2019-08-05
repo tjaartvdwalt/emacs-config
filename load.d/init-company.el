192850e989633a40c91fdbb29ea0852e4c757ee9
Tjaart van der Walt <git@tjaart.org>
2019-08-05 10:48:16 -0700

    load.d/init-company.el

--- load.d/init-company.el
+++ load.d/init-company.el
@@ -1,8 +1,8 @@
 (use-package company
   :bind (("M-<tab>" . company-other-backend))
-  :hook ((after-init . global-company-mode))
   ;; :delight
   :config
+  (global-company-mode)
   (setq company-idle-delay 0)
   (setq company-minimum-prefix-length 2)
   (setq company-selection-wrap-around t)
