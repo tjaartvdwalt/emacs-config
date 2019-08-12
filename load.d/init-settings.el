7a6f04f4123c47613316b0afc3a5cc7ff1f102fe
Tjaart van der Walt <git@tjaart.org>
2019-08-12 18:35:01 +0200

    load.d/init-settings.el

--- load.d/init-settings.el
+++ load.d/init-settings.el
@@ -7,7 +7,7 @@
 ;; (scroll-bar-mode -1)
 ;; Dont show the splash screen
 (setq inhibit-splash-screen t)
-(setq-default tab-width 4)
+(setq tab-width 4)
 (setq-default standard-indent 4)
 (electric-indent-mode t)
 ;;(setq-default tab-always-indent nil)
