b8b5b9f900afae7c1121af736eb16661f4f9d726
Tjaart van der Walt <git@tjaart.org>
2019-08-12 18:46:23 +0200

    load.d/init-prog-mode.el

--- load.d/init-prog-mode.el
+++ load.d/init-prog-mode.el
@@ -1,3 +1,4 @@
 (use-package prog-mode
   :hook (prog-mode . (lambda ()
-					   (setq tab-width 4))))
+                       (setq-default tab-width 4)
+                       (setq tab-width 4))))
