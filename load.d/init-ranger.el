1bf13d6a4554d9eb59e575d09f7f1a9436cefe4a
Tjaart van der Walt <git@tjaart.org>
2019-08-14 17:22:36 +0200

    load.d/init-ranger.el

--- load.d/init-ranger.el
+++ load.d/init-ranger.el
@@ -1,6 +1,6 @@
 (use-package ranger
   :config
-  ;; (setq ranger-mode 1)
+  (setq ranger-mode 1)
   (setq ranger-modify-header t)
-  (ranger-override-dired-mode t)
+  ;; (ranger-override-dired-mode t)
   )
