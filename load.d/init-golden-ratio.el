76af5eb7d3255da2a0d33ce6fc72e19f978ba087
Tjaart van der Walt <git@tjaart.org>
2019-08-14 18:21:58 +0200

    load.d/init-golden-ratio.el

--- load.d/init-golden-ratio.el
+++ load.d/init-golden-ratio.el
@@ -10,7 +10,7 @@
   :config
   (golden-ratio-mode 1)
   (setq golden-ratio-auto-scale t)
-  (setq golden-ratio-exclude-modes '("dired-mode"))
+  (setq golden-ratio-exclude-modes '("dired-mode", "ranger-mode"))
 
   (setq golden-ratio-adjust-factor .8
         golden-ratio-wide-adjust-factor .8))
