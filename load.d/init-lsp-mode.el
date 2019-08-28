c91d3f0662ca6f7206806f53036b6ae10b4e8f86
Tjaart van der Walt <git@tjaart.org>
2019-08-28 13:15:46 -0700

    load.d/init-lsp-mode.el

--- load.d/init-lsp-mode.el
+++ load.d/init-lsp-mode.el
@@ -2,6 +2,7 @@
   :hook (prog-mode . lsp-deferred)
   :commands lsp)
 
-;; optionally
+(use-package lsp-vue
+  :hook (web-mode . lsp-vue-enable))
 (use-package lsp-ui :commands lsp-ui-mode)
 (use-package company-lsp :commands company-lsp)
