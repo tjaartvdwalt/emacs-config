d28e2431a0fbf2f5b22cd103607036258f060075
Tjaart van der Walt <git@tjaart.org>
2019-08-29 13:07:11 -0700

    load.d/init-lsp-mode.el

--- load.d/init-lsp-mode.el
+++ load.d/init-lsp-mode.el
@@ -7,7 +7,5 @@
   (setq lsp-enable-snippet t)
   )
 
-(use-package lsp-vue
-  )
-(use-package lsp-ui :commands lsp-ui-mode)
-(use-package company-lsp :commands company-lsp)
+;; (use-package lsp-ui :commands lsp-ui-mode)
+;; (use-package company-lsp :commands company-lsp)
