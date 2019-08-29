5df120cbece544ff273a73a4ceffbed0f890b2b7
Tjaart van der Walt <git@tjaart.org>
2019-08-29 13:11:22 -0700

    load.d/init-lsp-mode.el

--- load.d/init-lsp-mode.el
+++ load.d/init-lsp-mode.el
@@ -2,9 +2,9 @@
   :hook ((prog-mode . lsp-deferred)
 		 (web-mode . lsp-deferred))
   :commands lsp
-  :config progn((setq lsp-auto-configure t)
-				(setq lsp-auto-guess-root t)
-				(setq lsp-enable-snippet t)))
+  :config (progn ((setq lsp-auto-configure t)
+				  (setq lsp-auto-guess-root t)
+				  (setq lsp-enable-snippet t))))
 
 ;; (use-package lsp-ui :commands lsp-ui-mode)
 ;; (use-package company-lsp :commands company-lsp)
