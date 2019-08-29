bcafe77068f0b0b1131b3ab4f7a2dbf1c84f3f2c
Tjaart van der Walt <git@tjaart.org>
2019-08-29 13:28:01 -0700

    load.d/init-ruby-mode.el

--- load.d/init-ruby-mode.el
+++ load.d/init-ruby-mode.el
@@ -1,25 +1,25 @@
 (use-package ruby-mode
   :mode "\\(?:\\.rb\\|ru\\|rake\\|thor\\|jbuilder\\|gemspec\\|podspec\\|/\\(?:Gem\\|Rake\\|Cap\\|Thor\\|Vagrant\\|Guard\\|Pod\\)file\\)\\'"
-  :interpreter "ruby"
+  :interpreter "ruby")
 
-  :config
-  (add-hook 'ruby-mode-hook 'eldoc-mode)
-  (add-to-list 'ruby-mode)
+  ;; :config
+  ;; (add-hook 'ruby-mode-hook 'eldoc-mode)
+  ;; (add-to-list 'ruby-mode)
 
-  (use-package chruby)
+  ;; (use-package chruby)
 
-  (use-package inf-ruby)
+  ;; (use-package inf-ruby)
 
-  (use-package rspec-mode)
+  ;; (use-package rspec-mode)
 
-  ;; (use-package robe
-  ;;   :hook ((ruby-mode . robe-mode)(ruby-mode . ac-robe-setup)))
+  ;; ;; (use-package robe
+  ;; ;;   :hook ((ruby-mode . robe-mode)(ruby-mode . ac-robe-setup)))
 
-  ;; (use-package rubocop
-  ;;   :hook (ruby-mode . (lambda ()
-  ;;                        (add-hook
-  ;;                          'after-save-hook '
-  ;;                          rubocop-autocorrect-current-file t t))))
+  ;; ;; (use-package rubocop
+  ;; ;;   :hook (ruby-mode . (lambda ()
+  ;; ;;                        (add-hook
+  ;; ;;                          'after-save-hook '
+  ;; ;;                          rubocop-autocorrect-current-file t t))))
 
-  (use-package yard-mode
-    :hook (ruby-mode . yard-mode)))
+  ;; (use-package yard-mode
+  ;;   :hook (ruby-mode . yard-mode)))
