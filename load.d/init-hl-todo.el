ca8e64aa5aa22ca1625b083fb4531374d0b38d73
Tjaart van der Walt <git@tjaart.org>
2019-10-09 10:40:02 -0700

    load.d/init-hl-todo.el

--- load.d/init-hl-todo.el
+++ load.d/init-hl-todo.el
@@ -1,5 +1,5 @@
 (use-package hl-todo
-  :hook ((prog-mode . hl-todo-mode)
-		 :bind (:map hl-todo-mode-map
-					 ("M-p" . hl-todo-previous)
-					 ("M-n" . hl-todo-next))))
+  :hook ((prog-mode . hl-todo-mode))
+  :bind (:map hl-todo-mode-map
+			  ("M-p" . hl-todo-previous)
+			  ("M-n" . hl-todo-next)))
