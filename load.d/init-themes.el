ae6cc66c16b868c3fa344fad9ab3489d7703386d
Tjaart van der Walt <git@tjaart.org>
2019-10-15 18:50:14 -0700

    load.d/init-themes.el

--- load.d/init-themes.el
+++ load.d/init-themes.el
@@ -7,6 +7,6 @@
 
 ;; (use-package dracula-theme)
 (use-package spacemacs-theme
-  :ensure spacemacs-dark-theme
-  :config
-  (load-theme 'spacemacs-theme-dark t))
+  :ensure spacemacs-dark-theme)
+  ;; :config
+  ;; (load-theme 'spacemacs-theme-dark t)
