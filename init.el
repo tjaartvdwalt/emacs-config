8cd1a8d4a4e6b00eebb39c65fec88dbe40e8f1ca
Tjaart van der Walt <git@tjaart.org>
2019-10-15 18:50:34 -0700

    init.el

--- init.el
+++ init.el
@@ -36,8 +36,6 @@
    (quote
     ("bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" default)))
  '(fci-rule-color "#073642")
- '(font-size--current-size size)
- '(font-size--default-size default-size)
  '(gac-automatically-push-p t)
  '(keep-end-mode t)
  '(magit-push-arguments nil)
