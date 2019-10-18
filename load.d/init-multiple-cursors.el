15f55cc9fde702c6c79f4cd7a34b46cac2fcab64
Tjaart van der Walt <git@tjaart.org>
2019-10-17 21:49:42 -0700

    load.d/init-multiple-cursors.el

--- load.d/init-multiple-cursors.el
+++ load.d/init-multiple-cursors.el
@@ -1,9 +1,15 @@
 (use-package multiple-cursors
   :config
+  :pretty-hydra
+  ((:color teal :quit-key "q")
+   ("Multiple cursors"
+    (("a" mc/edit-beginnings-of-lines)
+     ("e" mc/edit-ends-of-lines)))))
+
   (defhydra hydra-multiple-cursors (:color red)
     "
   _a_ edit beginnings of lines
   _e_ edit ends of lines
   "
-    ("a" mc/edit-beginnings-of-lines)
-    ("e" mc/edit-ends-of-lines)))
+    ("a" )
+    ("e" )))
