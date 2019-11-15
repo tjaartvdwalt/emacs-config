f5ff83ce6d5fbe2c9045a561479d7dce412f144e
Tjaart van der Walt <git@tjaart.org>
2019-11-15 15:25:51 -0800

    load.d/init-web-mode.el

--- load.d/init-web-mode.el
+++ load.d/init-web-mode.el
@@ -12,6 +12,7 @@
   (setq web-mode-enable-auto-pairing t)
   (setq web-mode-enable-current-element-highlight t)
   (setq web-mode-enable-current-column-highlight t)
+  (add-to-list 'web-mode-comment-formats '("javascript" . "//"))
 
   ;; (add-hook 'web-mode-before-auto-complete-hooks
   ;;           '(lambda ()
