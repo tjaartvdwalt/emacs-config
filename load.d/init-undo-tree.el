fa5e1c34ac961d7cf45f7850c6f721330e86ab5e
Tjaart van der Walt <git@tjaart.org>
2019-11-25 12:36:36 -0800

    load.d/init-undo-tree.el

--- load.d/init-undo-tree.el
+++ load.d/init-undo-tree.el
@@ -1,5 +1,5 @@
-(use-package undo-tree
-  :bind (("M-/" . undo-tree-redo)
-          ("C-x u" . undo-tree-visualize))
-  :delight
-  :config (global-undo-tree-mode 1))
+;; (use-package undo-tree
+;;   :bind (("M-/" . undo-tree-redo)
+;;           ("C-x u" . undo-tree-visualize))
+;;   :delight
+;;   :config (global-undo-tree-mode 1))
