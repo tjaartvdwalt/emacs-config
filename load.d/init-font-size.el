be0f66e784eeada3263ed8fca9efdfebeceac2a5
Tjaart van der Walt <git@tjaart.org>
2019-12-30 12:26:51 -0800

    load.d/init-font-size.el

--- load.d/init-font-size.el
+++ load.d/init-font-size.el
@@ -4,10 +4,10 @@
 Use DEFAULT-HEIGHT for default face and VARIABLE-PITCH-HEIGHT
 for variable-pitch face."
   (set-face-attribute 'default nil
-                      :family "Source Code Pro"
+                      :family "Mono"
                       :height default-height)
   (set-face-attribute 'variable-pitch nil
-                      :family "Fira Sans"
+                      :family "Sans"
                       :height variable-pitch-height
                       :weight 'regular))
 
