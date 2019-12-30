4e84b3d91758ba68543a087389512d8f508632de
Tjaart van der Walt <git@tjaart.org>
2019-12-29 16:15:35 -0800

    load.d/init-pkgbuild-mode.el

--- load.d/init-font-size.el
+++ load.d/init-font-size.el
@@ -1,2 +1,17 @@
-(require `font-size)
-(font-size-init 18)
+(defun mu-setup-main-fonts (default-height variable-pitch-height)
+  "Set up default fonts.
+
+Use DEFAULT-HEIGHT for default face and VARIABLE-PITCH-HEIGHT
+for variable-pitch face."
+  (set-face-attribute 'default nil
+                      :family "Source Code Pro"
+                      :height default-height)
+  (set-face-attribute 'variable-pitch nil
+                      :family "Fira Sans"
+                      :height variable-pitch-height
+                      :weight 'regular))
+
+(when window-system
+  (if (> (x-display-pixel-width) 2000)
+      (mu-setup-main-fonts 150 160)
+    (mu-setup-main-fonts 180 160)))
